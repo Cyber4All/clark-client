5e3d9b09edd21c1c68af89031693d5af845497ad
Sean Donnelly <sdonnelly3000@gmail.com>
2017-11-14 01:21:34 -0500

    Add Hosted API URL
    There is no need for the API URL to be secret.

--- src/env.js
+++ src/env.js
@@ -2,5 +2,5 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = 'http://localhost:3000/api/learning-objects';
+  window.__env.apiUrl = 'http://cubeapi-env.m7fcsmupmd.us-east-1.elasticbeanstalk.com/api/learning-objects';
 }(this));
