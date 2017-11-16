210021eda9899459e5296f3d6ecad102b4e05cc3
Gustavus Shaw II <gshaw1997@gmail.com>
2017-11-16 00:10:32 -0500

    Added Cube UI. Contributes to #1.

--- src/app/home/cube-faces/cube-faces.component.ts
+++ src/app/home/cube-faces/cube-faces.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'cube-faces',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CubeFacesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  
 }
