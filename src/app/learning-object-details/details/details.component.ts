fd4d08796bf359a6d8e82b3e4d063150606b09f7
Tyler Howard <thowar4@students.towson.edu>
2018-01-09 13:16:53 -0500

    Create basic template for details component

--- src/app/learning-object-details/details/details.component.ts
+++ src/app/learning-object-details/details/details.component.ts
@@ -1,3 +1,4 @@
+import { LearningObject } from 'clark-entity';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  learningObject = {
+    name: 'Sample Nano',
+    author: 'thowar4',
+    length: 'Nanomodule',
+    date: new Date(1515468567593).toLocaleDateString(),
+    goals: [{ text: 'goal1' }, { text: 'goal2' }],
+    outcomes: [{ bloom: 'Apply and analyze', verb: 'Analyze', text: 'Integer error', 
+                assessments: [{
+                  plan: 'Case', text: '<img src="https://www.w3schools.com/howto/img_fjords.jpg"/>'}],
+                instructions: [{
+                  instruction: 'Quiz', text: '<ol><li>Option</li></ol>'}]
+              },
+            {
+              bloom: 'Apply and analyze', verb: 'Analyze', text: 'Integer error',
+              assessments: [{
+                plan: 'Case', text: '<img src="https://www.w3schools.com/howto/img_fjords.jpg"/>'
+              }],
+              instructions: [{
+                instruction: 'Quiz', text: '<ol><li>Option</li></ol>'
+              }]
+      }],
 
-  constructor() { }
+  };
+
+  constructor() {
+    
+  }
 
   ngOnInit() {
+    
   }
 
 }
