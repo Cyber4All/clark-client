06f8fc8c8eefd126ef38c7874a2ccc40c06dea98
Sean Donnelly <sdonnelly3000@gmail.com>
2018-01-15 13:22:12 -0500

    Add Buttons for Active Cube Faces
    The active "faces" of the cube are now accessed via a button bar at the top of the scrollable content.
    
    Further work is needed to reintegrate these components since breaking data-level changes have been made.
    
    Contributes to story 2 of #12

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,15 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { AcademicsComponent } from './academics/academics.component';
 import { MappingComponent } from './mapping/mapping.component';
 import { ModalityComponent } from './modality/modality.component';
-import { DetailsComponent } from './learning-object-details/details/details.component'
+import { DetailsComponent } from './learning-object-details/details/details.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '', pathMatch: 'full', component: HomeComponent },
-    { path: 'home', component: HomeComponent, data: { breadcrumb: "Home" } },
-    { path: 'academics', component: AcademicsComponent, data: { breadcrumb: "Academics" } },
-    { path: 'mapping', component: MappingComponent, data: { breadcrumb: "Mapping" } },
-    { path: 'modality', component: ModalityComponent, data: { breadcrumb: "Modality" } },
-    { path: "details/:id", component: DetailsComponent, data: { breadcrumb: "Details" } },
+    { path: 'home', component: HomeComponent, data: { breadcrumb: 'Home' } },
+    { path: 'academics', component: AcademicsComponent, data: { breadcrumb: 'Academics' } },
+    { path: 'mapping', component: MappingComponent, data: { breadcrumb: 'Mapping' } },
+    { path: 'modality', component: ModalityComponent, data: { breadcrumb: 'Modality' } },
+    { path: 'details/:id', component: DetailsComponent, data: { breadcrumb: 'Details' } },
 
     // Catch All
     { path: '**', redirectTo: '' }
