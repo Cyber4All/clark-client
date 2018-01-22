81a4fa8364cabfe366cd48a7ca48a86e546275e6
KolbeML <kleona3@students.towson.edu>
2018-01-22 14:28:09 -0500

    Fix dropdown issue Adjusted CSS to fix issues with dropdown menu.

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -12,7 +12,30 @@ export class UserProfileComponent implements OnInit {
   modulesCreated = "9001";
 
   constructor() {
+    /* interface User {
+      String name;
+      String[] affiliations;
+      LearningObject[] objectsCreated;
+      LearningObject[] objectsDownloaded;
+      boolean contributor = false;
+      //BufferedImage profilePic;
+      //String userName; //for url?
+      //String bio;
     
+      public String getName();
+      public String[] getAffils();
+      public LearningObject[] getCreated();
+      public LearningObject[] getDownloaded();
+      public Boolean isContributor();
+      //public String getBio();
+      //public BufferedImage getPic();
+      public void setName(String newName); //at very least should be able to change last name
+      public void addAffils(String[] newAffils);
+      public void remAffils(String[] badAffils);
+      public void setContrib(); //would only ever need to change false to true
+      //public void setBio(String newBio);
+      //public void setPic(BufferedImage newPic);
+    } */
    }
 
   ngOnInit() {
