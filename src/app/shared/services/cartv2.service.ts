6b9b8ce2c2a7c70f12d703c02e53a46a0e86b98d
Gustavus Shaw II <gshaw1997@gmail.com>
2018-01-25 03:28:29 -0500

    Fixed breaking changes in cartv2.service.

--- src/app/shared/services/cartv2.service.ts
+++ src/app/shared/services/cartv2.service.ts
@@ -1,14 +1,19 @@
 import { USER_ROUTES } from "./../../../environments/route";
 import { Injectable, OnInit } from "@angular/core";
 import { LearningObject, User } from "@cyber4all/clark-entity";
-import { HttpClient } from "@angular/common/http";
+import { Http, Headers } from "@angular/http";
 
 @Injectable()
 export class CartV2Service {
-  private username = JSON.parse(localStorage.getItem('currentUser'))._username;
+  private username = JSON.parse(localStorage.getItem('currentUser')) ? JSON.parse(localStorage.getItem('currentUser'))['_username'] : '';
+  private headers: Headers = new Headers();
 
-  constructor(private http: HttpClient) {
+
+  constructor(private http: Http) {
     console.log(this.username);
+    let token = JSON.parse(localStorage.getItem('currentUser')) ? JSON.parse(localStorage.getItem('currentUser')).token : '';
+    this.headers.append('Authorization', 'Bearer ' + token);
+    this.headers.append('Content-Type', 'application/json');
   }
 
   openLearningObject(url: string) {
@@ -16,12 +21,13 @@ export class CartV2Service {
     window.open(url);
   }
 
-   getCart(): Promise<Array<LearningObject>> {
+  getCart(): Promise<Array<LearningObject>> {
     return this.http
-      .get(USER_ROUTES.GET_CART(this.username))
+      .get(USER_ROUTES.GET_CART(this.username), { headers: this.headers })
       .toPromise()
       .then(val => {
-        return <Array<LearningObject>> val;
+        console.log(val)
+        return <Array<LearningObject>>val.json();
       });
   }
 
@@ -32,16 +38,15 @@ export class CartV2Service {
     // tslint:disable-next-line:max-line-length
     return this.http
       .post(
-        USER_ROUTES.ADD_LEARNING_OBJECT_TO_CART(
-          this.username,
-          author,
-          learningObjectName
-        ),
-        {}
+      USER_ROUTES.ADD_LEARNING_OBJECT_TO_CART(
+        this.username,
+        author,
+        learningObjectName
+      ), {}, { headers: this.headers }
       )
       .toPromise()
       .then(val => {
-        return <Array<LearningObject>> val;
+        return <Array<LearningObject>>val.json();
       });
   }
 
@@ -52,21 +57,21 @@ export class CartV2Service {
     // tslint:disable-next-line:max-line-length
     return this.http
       .delete(
-        USER_ROUTES.CLEAR_LEARNING_OBJECT_FROM_CART(
-          this.username,
-          author,
-          learningObjectName
-        )
+      USER_ROUTES.CLEAR_LEARNING_OBJECT_FROM_CART(
+        this.username,
+        author,
+        learningObjectName
+      ), { headers: this.headers }
       )
       .toPromise()
       .then(val => {
-        return <Array<LearningObject>> val;
+        return <Array<LearningObject>>val.json();
       });
   }
 
   clearCart() {
     this.http
-      .delete(USER_ROUTES.CLEAR_CART(this.username))
+      .delete(USER_ROUTES.CLEAR_CART(this.username), { headers: this.headers })
       .toPromise()
       .then(val => {
         console.log(val);
@@ -75,7 +80,7 @@ export class CartV2Service {
 
   checkout() {
     this.http
-      .get(USER_ROUTES.GET_CART(this.username) + '?download=true')
+      .get(USER_ROUTES.GET_CART(this.username) + '?download=true', { headers: this.headers })
       .toPromise()
       .then(val => {
         console.log(val);
