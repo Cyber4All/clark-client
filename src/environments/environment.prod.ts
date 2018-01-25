fd1fef016eb1e11e82eb5d13db4b1c13c49f2cd2
Gustavus Shaw II <gshaw1997@gmail.com>
2018-01-25 01:57:27 -0500

    Fixed typo in enviornments file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: 'http://learningobjectsubmission-env.qwgabnmb7w.us-east-1.elasticbeanstalk.com/api',
+  apiURL: 'http://learningobjectsubmission-env.qwgabnmb7w.us-east-1.elasticbeanstalk.com/api',
   apiVersion: 'v1',
   STATE_STORAGE_LOCATION: 'state'
 };
