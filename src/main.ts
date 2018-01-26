a89767cff8134d58c585e9c6569b02dc2351e8b0
Gustavus Shaw II <gshaw1997@gmail.com>
2018-01-26 14:36:48 -0500

    Add version checking on application load.

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,25 @@ import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
+
+// Application display name and Version information
+const { version: appVersion } = require('../package.json');
+const { displayName: appName } = require('../package.json');
+const VERSION_STORE = 'version';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+//Get the version of the application the user last ran
+let userVersion = localStorage.getItem(VERSION_STORE);
+(() => {
+  //Set current version of the application
+  localStorage.setItem(VERSION_STORE, appVersion);
+  //Check the version of the application the user last ran; If mismatch clear cache via hard reload
+  userVersion !== appVersion ? location.reload(true) :
+    console.log(`${appName} running version: ${appVersion} - Up to date.`);
+})();
+
+//Verify correct version before bootstrapping application
+userVersion === appVersion ? platformBrowserDynamic().bootstrapModule(AppModule) : console.log('Waiting for update...');
