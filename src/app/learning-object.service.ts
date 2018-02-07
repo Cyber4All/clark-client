1723047c098e775f0a1bd6c41d6ee5eeb678f41b
Sean Donnelly <sdonnelly3000@gmail.com>
2018-02-07 12:18:28 -0500

    Fix merge conflict

--- src/app/learning-object.service.ts
+++ src/app/learning-object.service.ts
@@ -6,11 +6,8 @@ import { Observable } from 'rxjs/Observable';
 import * as Fuse from 'fuse.js';
 import { environment } from '../environments/environment';
 import { LearningObject, User } from '@cyber4all/clark-entity';
-<<<<<<< HEAD
-=======
 import { Query, TextQuery, MappingQuery } from './shared/interfaces/query';
 
->>>>>>> 1e1a6100459b4837e1cbf22802f8ac70ea968fe1
 import * as querystring from 'querystring';
 
 @Injectable()
@@ -29,38 +26,7 @@ export class LearningObjectService {
     return this.data;
   }
 
-<<<<<<< HEAD
-  async configureFuse(query:String){
-    let fuseGroups = await this.getLearningObjects();
-    let options = {
-      shouldSort: true,
-      threshold: 0.3,
-      location: 0,
-      distance: 100,
-      maxPatternLength: 32,
-      minMatchCharLength: 1,
-      keys: ['name']
-    };
-    this.fuse = new Fuse(fuseGroups, options)
-
-
-
-  }
-
-  async search(query: String): Promise<LearningObject[]>{
-    if(query.length < 4){
-      this.clearSearch();
-    }else {
-      await this.configureFuse(query);
-      this.filteredResults = this.fuse.search(query);
-    }
-    return this.filteredResults;
-  }
-
-  getFilteredObjects(){
-=======
   getFilteredObjects() {
->>>>>>> 1e1a6100459b4837e1cbf22802f8ac70ea968fe1
     return this.filteredResults;
   }
 
@@ -79,11 +45,7 @@ export class LearningObjectService {
    * @memberof LearningObjectService
    */
   // TODO: Remove limit
-<<<<<<< HEAD
-  getLearningObjects(query?: any, featured?: number): Promise<LearningObject[]> {
-=======
   getLearningObjects(query?: Query): Promise<LearningObject[]> {
->>>>>>> 1e1a6100459b4837e1cbf22802f8ac70ea968fe1
     let route = '';
     if (query) {
       let queryString = querystring.stringify(query);
@@ -96,11 +58,7 @@ export class LearningObjectService {
       .then((response) => {
         let res = response.json();
         let objects = res.objects;
-<<<<<<< HEAD
-        let total = res.total;
-=======
         this.totalLearningObjects = res.total;
->>>>>>> 1e1a6100459b4837e1cbf22802f8ac70ea968fe1
         return objects.map((_learningObject: string) => {
           let learningObject = LearningObject.unserialize(_learningObject);
           return learningObject;
