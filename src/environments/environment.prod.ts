867597a4dc175f74856b150b0f95ab744e136726
Gustavus Shaw II <gshaw1997@gmail.com>
2018-02-21 17:22:04 -0500

    Update API URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: 'https://api.gateway.clark.center/api',
+  apiURL: 'https://api-gateway.clark.center/api',
   apiVersion: 'v1',
   STATE_STORAGE_LOCATION: 'state',
-  suggestionUrl: 'https://api.learning-outcome.clark.center/api'
+  suggestionUrl: 'https://api-learning-outcome.clark.center/api'
 };
