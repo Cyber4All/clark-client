74469f8b19e3a94b27f14197639f1e6979da0c08
Nick Winner <design@winnerdigital.net>
2018-03-05 18:24:33 -0500

    Fix bug where cart wasn't showing the number of items in cart when cart was empty, fixed bug where reset-password canActivate redirected to a blank screen

--- src/app/auth/can-reset-password.guard.ts
+++ src/app/auth/can-reset-password.guard.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class CanResetPasswordGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      return state.root.children[0].queryParams.otaCode ? true : false;
+      if (state.root.children[0].queryParams.otaCode) {
+        return true;
+      } else {
+        this.router.navigate(['/home']);
+      }
+      return false;
   }
 }
