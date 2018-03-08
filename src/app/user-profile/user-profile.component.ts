ed886ea3dc44a57e0a1d3e3cfcf282a315330949
Nicholas Visalli <nvisal1225@gmail.com>
2018-03-08 01:32:28 -0500

    Created new ui for user-profile.component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -15,26 +15,149 @@ export class UserProfileComponent implements OnInit {
   employer: String;
   contributor = false;
   modulesCreated: LearningObject[];
-  modulesDownloaded = 34; 
+  modulesDownloaded = 34;
   title = "Student";
   email: String;
-  password: String; 
+  password: String;
   //Display Variables
   showContent = 0;
-  editContent = true; 
+  editContent = true;
+  myStyle;
+  particleParams;
+
+
 
   constructor(private service: LearningObjectService, private auth: AuthService) {
   }
 
-  changeName() {    this.name = 'Houssein';  }
+  changeName() { this.name = 'Houssein'; }
   ngOnInit() {
     const user = this.auth.user;
     this.email = user['email'];
     this.name = user['name'];
     this.employer = user['organization'];
 
-    
-    //this.modulesCreated 
-  }
+    this.myStyle = {
+      'position': 'absolute',
+      'width': '100%',
+      'height': '100%',
+      'z-index': 0,
+      'top': 0,
+      'left': 0,
+      'right': 0,
+      'bottom': 0,
+    };
 
+    this.particleParams = {
+      'particles': {
+        'number': {
+          'value': 50,
+          'density': {
+            'enable': true,
+            'value_area': 450
+          }
+        },
+        'color': {
+          'value': '#ffffff'
+        },
+        'shape': {
+          'type': 'circle',
+          'stroke': {
+            'width': 0,
+            'color': '#000000'
+          },
+          'polygon': {
+            'nb_sides': 5
+          },
+          'image': {
+            'src': 'img/github.svg',
+            'width': 100,
+            'height': 100
+          }
+        },
+        'opacity': {
+          'value': 0.5,
+          'random': false,
+          'anim': {
+            'enable': false,
+            'speed': 1,
+            'opacity_min': 0.1,
+            'sync': false
+          }
+        },
+        'size': {
+          'value': 3,
+          'random': true,
+          'anim': {
+            'enable': false,
+            'speed': 40,
+            'size_min': 0.1,
+            'sync': false
+          }
+        },
+        'line_linked': {
+          'enable': true,
+          'distance': 150,
+          'color': '#ffffff',
+          'opacity': 0.4,
+          'width': 1
+        },
+        'move': {
+          'enable': true,
+          'speed': 0,
+          'direction': 'none',
+          'random': false,
+          'straight': false,
+          'out_mode': 'out',
+          'bounce': false,
+          'attract': {
+            'enable': false,
+            'rotateX': 600,
+            'rotateY': 1200
+          }
+        }
+      },
+      'interactivity': {
+        'detect_on': 'canvas',
+        'events': {
+          'onhover': {
+            'enable': false,
+            'mode': 'repulse'
+          },
+          'onclick': {
+            'enable': false,
+            'mode': 'push'
+          },
+          'resize': true
+        },
+        'modes': {
+          'grab': {
+            'distance': 400,
+            'line_linked': {
+              'opacity': 1
+            }
+          },
+          'bubble': {
+            'distance': 50,
+            'size': 20,
+            'duration': 2,
+            'opacity': 8,
+            'speed': 3
+          },
+          'repulse': {
+            'distance': 200,
+            'duration': 0.4
+          },
+          'push': {
+            'particles_nb': 4
+          },
+          'remove': {
+            'particles_nb': 2
+          }
+        }
+      },
+      'retina_detect': true
+    };
+  }
 }
+
