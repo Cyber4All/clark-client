3fa6a3a564d2291af303abc23b153188448bd5cc
Nick Winner <design@winnerdigital.net>
2018-03-13 18:17:40 -0400

    Include the services I forgot

---
+++ src/app/cube/core/profile.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { UserService } from '../../core/user.service';
+
+@Injectable()
+export class ProfileGuard implements CanActivate {
+
+  constructor(private router: Router, private user: UserService) { }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    const username = state.url.split('/')[1];
+    return this.user.validateUser(username).then(val => {
+      if (val) {
+        this.router.navigate(['/users/', username]);
+        return true;
+      } else {
+        this.router.navigate(['/home']);
+        return false;
+      }
+    });
+  }
+}
