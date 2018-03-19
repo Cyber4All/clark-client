a6a5c80d1acb062981d27f9f758480a107720388
Sean Donnelly <sdonnelly3000@gmail.com>
2018-03-19 12:33:22 -0400

    Add Tooltip to Outcomes From Other Sources
    Contributes to #62

--- src/app/onion/suggestion/suggestion.module.ts
+++ src/app/onion/suggestion/suggestion.module.ts
@@ -5,11 +5,13 @@ import { SuggestionComponent } from './suggestion.component';
 import { SuggestionFilterComponent } from './suggestion-filter.component';
 import { FormsModule } from '@angular/forms';
 import { MappingsListComponent } from './mappings-list.component';
+import { TooltipModule } from '@cyber4all/clark-tooltip';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    TooltipModule
   ],
   exports: [
     SuggestionComponent,
