afd54999f99a422e49cacba1da3bcef9caf426e0
Gustavus Shaw II <gshaw1997@gmail.com>
2018-03-19 17:23:54 -0400

    Trim User's name. Closes #80

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -18,7 +18,7 @@ export class RegisterComponent implements OnInit {
     username: '',
     email: '',
     organization: '',
-    password: '',
+    password: ''
   };
   regForm = new FormGroup({
     firstname: new FormControl(null, Validators.required),
@@ -28,9 +28,9 @@ export class RegisterComponent implements OnInit {
     organization: new FormControl(null, Validators.required),
     password: new FormControl(null, Validators.required),
     verifypassword: new FormControl(null, Validators.required),
-    captcha: new FormControl(),
+    captcha: new FormControl()
   });
-  
+
   siteKey = '6LfS5kwUAAAAAIN69dqY5eHzFlWsK40jiTV4ULCV';
 
   passwordVerify = '';
@@ -40,10 +40,11 @@ export class RegisterComponent implements OnInit {
   redirectUrl;
 
   constructor(private auth: AuthService, private route: ActivatedRoute) {
-    this.route.parent.data
-    .subscribe((data) => {
+    this.route.parent.data.subscribe(data => {
       if (route.snapshot.queryParams.returnUrl) {
-        this.redirectUrl = this.auth.makeRedirectURL(route.snapshot.queryParams.returnUrl);
+        this.redirectUrl = this.auth.makeRedirectURL(
+          route.snapshot.queryParams.returnUrl
+        );
       } else {
         if (route.snapshot.queryParams.redirectRoute) {
           this.redirectRoute = route.snapshot.queryParams.redirectRoute;
@@ -54,8 +55,7 @@ export class RegisterComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   submit() {
     this.registerFailure = undefined;
@@ -74,41 +74,49 @@ export class RegisterComponent implements OnInit {
 
     let u = new User(
       this.regInfo.username,
-      this.regInfo.firstname + ' ' + this.regInfo.lastname,
+      `${this.regInfo.firstname.trim()} ${this.regInfo.lastname.trim()}`,
       this.regInfo.email,
       this.regInfo.organization,
       this.regInfo.password
     );
 
-    this.auth.register(u).subscribe(val => {
-      if (this.redirectRoute) {
-        window.location.href = window.location.origin + this.redirectRoute;
-      } else {
-        window.location.href = this.redirectUrl;
+    this.auth.register(u).subscribe(
+      val => {
+        if (this.redirectRoute) {
+          window.location.href = window.location.origin + this.redirectRoute;
+        } else {
+          window.location.href = this.redirectUrl;
+        }
+      },
+      error => {
+        this.error(error.error.message);
       }
-    }, error => {
-      this.error(error.error.message);
-    });
+    );
   }
 
   validate(): boolean {
-    let m: boolean[] = Object.values(this.regInfo).map(function(l) { return (l && l !== '' && true) || false; });
-    let email = this.regInfo.email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g) !== null;
-    
+    let m: boolean[] = Object.values(this.regInfo).map(function(l) {
+      return (l && l !== '' && true) || false;
+    });
+    let email =
+      this.regInfo.email.match(
+        /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
+      ) !== null;
+
     if (m.includes(false) && this.verified === true) {
       this.error('Please fill in all fields!');
       this.loading = false;
       return false;
-    } else if (this.verified === false){
+    } else if (this.verified === false) {
       this.error('Please complete captcha!');
       this.loading = false;
       return false;
-    }else if (!email) {
+    } else if (!email) {
       this.error('Please enter a valid email!');
       this.loading = false;
       return false;
     }
-    
+
     return true;
   }
 
@@ -120,8 +128,7 @@ export class RegisterComponent implements OnInit {
     }, duration);
   }
 
-  captureResponse(event){
+  captureResponse(event) {
     this.verified = event;
   }
-
 }
