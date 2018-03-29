5613b219fb55c0cfe47804172d68bf45b188bb2f
Nicholas Visalli <nvisal1225@gmail.com>
2018-03-29 13:31:02 -0400

    added minor functionality -- no email hash

--- src/app/cube/user-profile/gravatar-image/gravatar-image.component.ts
+++ src/app/cube/user-profile/gravatar-image/gravatar-image.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'clark-gravatar-image',
-  templateUrl: './gravatar-image.component.html',
-  styleUrls: ['./gravatar-image.component.scss']
+  template: `
+        <div id="container">
+            <img [src]="getGravatarImage()">
+            <input [(ngModel)]="email" type="email" placeholder="Enter your Gravatar email">
+        </div>
+    `,
+    styles: []
 })
 export class GravatarImageComponent implements OnInit {
+  @Input() size:number = 200;
+  @Input() email:string = "";
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getGravatarImage():string {
+    return 'http://www.gravatar.com/avatar/' + this.email + '?s=' + this.size;
   }
 
 }
