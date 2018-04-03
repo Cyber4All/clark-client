57fcb05bf2c043f82c0645c6af883dc11d16672b
Gustavus Shaw II <gshaw1997@gmail.com>
2018-04-03 12:15:41 -0400

    Clean up typings and remove View Materials Component

--- src/app/onion/content-upload/app/content-upload.routes.ts
+++ src/app/onion/content-upload/app/content-upload.routes.ts
@@ -1,15 +1,9 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UploadComponent } from './upload/upload.component';
-import { ViewComponent } from './view/view.component';
-
 
 export const router: Routes = [
-    { path: 'upload/:learningObjectName', component: UploadComponent },
-    { path: 'view/:learningObjectName', component: ViewComponent },
+  { path: 'upload/:learningObjectName', component: UploadComponent }
 ];
-
-
-
 
 export const routes: ModuleWithProviders = RouterModule.forChild(router);
