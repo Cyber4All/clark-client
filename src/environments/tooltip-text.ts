75b0f6c59fe2f67172d442b5b98a49abe3358b0b
Gustavus Shaw II <gshaw1997@gmail.com>
2018-04-05 18:00:09 -0400

    Add to tip for instructions on managing file

--- src/environments/tooltip-text.ts
+++ src/environments/tooltip-text.ts
@@ -2,7 +2,9 @@
 export const TOOLTIP_TEXT = {
   LEARNING_OBJECT:
     'A Learning Object is a shareable, self-contained piece of curriculum that has clearly identified learning outcomes, assessment strategies, and instructional strategies.',
-  FILE_UPLOAD: 'Zip folders before uploading to maintain directory structure.',
-  UNVERIFIED_UPLOAD: 'You must have a verified email address to upload materials!',
-  UNVERIFIED_PUBLISH: 'You must have a verified email address to publish learning objects!'
+  FILE_OPTIONS: 'Right click file or folder to delete or add description.',
+  UNVERIFIED_UPLOAD:
+    'You must have a verified email address to upload materials!',
+  UNVERIFIED_PUBLISH:
+    'You must have a verified email address to publish learning objects!'
 };
