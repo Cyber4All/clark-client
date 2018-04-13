eeb29df47247f7f1127a95da7f41263eac5bed1a
Nick Winner <design@winnerdigital.net>
2018-04-12 21:34:04 -0400

    Implement mobile navbar and slideout container, move toolbar into navbar component

--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -11,37 +11,13 @@ import { NotificationService } from '../shared/notifications';
   styleUrls: ['./cube.component.scss']
 })
 export class CubeComponent implements OnInit {
-  activeContentSwitcher = 'search';
   hideTopbar: any = false;
   filterButton = false;
 
   constructor(private router: Router, private route: ActivatedRoute, private noteService: NotificationService, private modalService: ModalService) { }
 
   ngOnInit() {
-    this.router.events.filter(event => event instanceof NavigationEnd).subscribe(event => {
-      const root: ActivatedRoute = this.route.root;
-      this.hideTopbar = root.children[0].snapshot.data.hideTopbar;
-    });
-  }
-
-  /**
-   * Manages click events for the button for switching between contributing and searching (onion and cube)
-   * @param event
-   */
-  contentSwitchClick(event) {
-    const el = event.target;
-    const h = document.getElementsByClassName('content-switch')[0];
-    if (el.classList.contains('contribute') && this.activeContentSwitcher !== 'contribute') {
-      h.classList.remove('right');
-      h.classList.add('left');
-      this.activeContentSwitcher = 'contribute';
-
-      this.router.navigate(['onion']);
-    } else if (el.classList.contains('search') && this.activeContentSwitcher !== 'search') {
-      h.classList.remove('left');
-      h.classList.add('right');
-      this.activeContentSwitcher = 'search';
-    }
+    
   }
 
   filterButtonClick() {
@@ -51,19 +27,6 @@ export class CubeComponent implements OnInit {
   filterButtonClickAway() {
     if (this.filterButton) {
       this.filterButtonClick();
-    }
-  }
-
-  /**
-   * Takes a reference to the searchbar input to pass as a query to the browse component.
-   * @param query
-   */
-  performSearch(searchbar) {
-    searchbar.value = searchbar.value.trim();
-    const query = searchbar.value;
-    if (query.length) {
-      // FIXME: Should use a relative route './browse'
-      this.router.navigate(['/browse', { query }]);
     }
   }
 }
