38e249c654f36fd9ade5a127c4c61e38837dd7c6
Nick Winner <design@winnerdigital.net>
2018-04-17 20:31:25 -0400

    Move establishSocket() subscription code into function

--- src/app/clark.component.ts
+++ src/app/clark.component.ts
@@ -9,30 +9,46 @@ import { ModalService, ModalListElement } from './shared/modals';
   styleUrls: ['./clark.component.scss']
 })
 export class ClarkComponent {
+  connectedToSocket: boolean;
 
   constructor(private authService: AuthService, private cartService: CartV2Service, private modal: ModalService) {
+    this.connectedToSocket = false;
+
     this.authService.isLoggedIn.subscribe(val => {
       if (val) {
         this.cartService.updateUser();
         this.cartService.getCart();
 
-        if (!this.authService.user.emailVerified) {
-          this.authService.establishSocket().subscribe(res => {
-            if (res === 'VERIFIED_EMAIL') {
-              this.modal.makeDialogMenu(
-                'emailVerified',
-                'Email Verified!',
-                'Thank you for verifying your email! Now you can do awesome things like publish learning objects and upload materials!',
-                'title-good',
-                'center',
-                [new ModalListElement('Got it!', 'done', 'green')]
-              );
-            }
-          });
-        }
-      } else {
+        this.connectedToSocket = this.attemptSocketConnection();
+
+      } else if (this.connectedToSocket) {
         this.authService.destroySocket();
       }
     });
+  }
+
+  /**
+   * Checks if the user is unverified and if they are establishes connection to gateway via socket
+   */
+  attemptSocketConnection(): boolean {
+    if (!this.authService.user.emailVerified) {
+      this.authService.establishSocket().subscribe(res => {
+        // events
+        if (res === 'VERIFIED_EMAIL') {
+          this.modal.makeDialogMenu(
+            'emailVerified',
+            'Email Verified!',
+            'Thank you for verifying your email! Now you can do awesome things like publish learning objects and upload materials!',
+            'title-good',
+            'center',
+            [new ModalListElement('Got it!', 'done', 'green')]
+          );
+        }
+      });
+
+      return true;
+    }
+
+    return false;
   }
 }
