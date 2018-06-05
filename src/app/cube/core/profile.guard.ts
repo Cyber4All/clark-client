81c48b78b868e8d5f9ac2963045d1a8aee2f8314
Gustavus Shaw II <gshaw1997@gmail.com>
2018-06-05 14:30:28 -0400

    Fix issue causing profile guard to fetch all learning objects and users

--- src/app/cube/core/profile.guard.ts
+++ src/app/cube/core/profile.guard.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { UserService } from '../../core/user.service';
 
 @Injectable()
 export class ProfileGuard implements CanActivate {
-
-  constructor(private router: Router, private user: UserService) { }
+  constructor(private router: Router, private user: UserService) {}
 
   /**
    * Checks if a username is tied to a profile and redirects to that profile.
@@ -17,16 +21,26 @@ export class ProfileGuard implements CanActivate {
    * @returns {(Observable<boolean> | Promise<boolean> | boolean)} whether or not the route can be activated.
    * @memberof ProfileGuard
    */
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    const username = state.url.split('/')[1];
-    return this.user.validateUser(username).then(val => {
-      if (val) {
-        this.router.navigate(['users', username]);
-        return true;
-      } else {
-        this.router.navigate(['/home']);
-        return false;
-      }
-    });
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    const username = state.url.split('/')[1]
+      ? state.url.split('/')[1].trim()
+      : null;
+    if (username) {
+      return this.user.validateUser(username).then(val => {
+        if (val) {
+          this.router.navigate(['users', username]);
+          return true;
+        } else {
+          this.router.navigate(['/home']);
+          return false;
+        }
+      });
+    } else {
+      this.router.navigate(['/home']);
+      return false;
+    }
   }
 }
