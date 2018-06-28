aa79e960cfa2cdd7f9bbd2ea299cdced49242563
Nicholas Visalli <nvisal1225@gmail.com>
2018-06-28 13:44:19 -0400

    tests for cartv2 service

--- src/app/core/cartv2.service.spec.ts
+++ src/app/core/cartv2.service.spec.ts
@@ -1,8 +1,7 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { CartV2Service } from './cartv2.service';
+import { CartV2Service, sanitizeFileName } from './cartv2.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CookieService, CookieOptionsProvider, CookieModule } from 'ngx-cookie';
-
 import { Router } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { AuthService } from './auth.service';
@@ -18,4 +17,25 @@ describe('CartV2Service', () => {
   it('should be created', inject([CartV2Service], (service: CartV2Service) => {
     expect(service).toBeTruthy();
   }));
+  it('should run update user', inject([CartV2Service], (service: CartV2Service) => {
+    expect(service.updateUser()).toBeUndefined();
+  }));
+  it('should return cart', inject([CartV2Service, AuthService], (service: CartV2Service, auth: AuthService) => {
+    const loginInfo = {
+      username: 'nvisal1',
+      password: '122595'
+    };
+    return auth.login(loginInfo).then(val => {
+      service.updateUser();
+      return service.getCart().then(val => {
+        console.log(val);
+        expect(val).toBeTruthy();
+      });
+    });
+  }));
+  it('should return cart', inject([CartV2Service], (service: CartV2Service) => {
+    const result = sanitizeFileName('///****hjklkjh!@#$%^&');
+    expect(result).toBeDefined();
+  }));
 });
+
