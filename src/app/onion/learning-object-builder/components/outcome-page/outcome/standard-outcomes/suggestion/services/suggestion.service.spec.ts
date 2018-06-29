32fefe5d1e7556713c8d8c0d89ded506eb26ab9e
Nicholas Visalli <nvisal1225@gmail.com>
2018-06-29 11:24:09 -0400

    test suggestion service

--- src/app/onion/learning-object-builder/components/outcome-page/outcome/standard-outcomes/suggestion/services/suggestion.service.spec.ts
+++ src/app/onion/learning-object-builder/components/outcome-page/outcome/standard-outcomes/suggestion/services/suggestion.service.spec.ts
@@ -13,4 +13,17 @@ describe('SuggestionService', () => {
   it('should be created', inject([SuggestionService], (service: SuggestionService) => {
     expect(service).toBeTruthy();
   }));
+  it('should return observable', inject([SuggestionService], (service: SuggestionService) => {
+    const result = service.observe();
+    expect(result).toBeDefined();
+  }));
+  it('should return mappings', inject([SuggestionService], (service: SuggestionService) => {
+    const result = service.mappings;
+    expect(result).toBeDefined();
+  }));
+  it('should set service.total', inject([SuggestionService], (service: SuggestionService) => {
+    service.emit('text');
+    expect(service.total).toEqual(0);
+  }));
 });
+
