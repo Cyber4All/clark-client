611185ac821972797702c205be085342aa6c683f
Nicholas Visalli <nvisal1225@gmail.com>
2018-06-29 10:30:47 -0400

    test notification service

--- src/app/shared/notifications/notification.service.spec.ts
+++ src/app/shared/notifications/notification.service.spec.ts
@@ -11,4 +11,9 @@ describe('NotificationService', () => {
   it('should be created', inject([NotificationService], (service: NotificationService) => {
     expect(service).toBeTruthy();
   }));
+  it('should create a notification', inject([NotificationService], (service: NotificationService) => {
+    service.notify('title', '', '', '');
+    expect(service.content).toMatchObject({ title: 'title', text: '', classes: '', icon: '' });
+  }));
 });
+
