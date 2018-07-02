e640ab4786324e6c49096f67b48662fe8c490361
Nicholas Visalli <nvisal1225@gmail.com>
2018-07-02 15:14:45 -0400

    more ui testing

--- cypress/integration/cube.spec.js
+++ cypress/integration/cube.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import Chance from 'chance';
+import { SSL_OP_CISCO_ANYCONNECT } from 'constants';
 const chance = new Chance();
 
 describe('Cube', () => {
@@ -8,7 +9,7 @@ describe('Cube', () => {
 
     beforeEach(() => {
         // Return to home page before each test
-        cy.visit('http://localhost:4201/home');
+        cy.visit('http://localhost:4200/home');
     });
 
     it('Has page title', () => {
@@ -17,7 +18,7 @@ describe('Cube', () => {
 
     it('Navbar renders correctly', () => {
         cy.get('.topbar').contains('C.L.A.R.K.');
-        cy.get('input[placeholder=Search...]');
+        cy.get('div.nav-search-bar');
         cy.get('.topbar').contains('Sign in');
         cy.get('.topbar').contains('Register');
     });
@@ -29,9 +30,165 @@ describe('Cube', () => {
         // Assert URL 
         cy.url().should('include', 'login');
 
+        // Enter login info 
+        cy.get('input[name=username]').type('nvisal1');
+        cy.get('input[name=password]').type('122595');
+        cy.get('.auth-button').click();
+
+        // Assert new navbar elements
+        cy.contains('Contributor');
+        cy.get('.navbar-gravatar').click();
     });
 
     it('Register new user', () => {
-        cy.contains('Register');
+        cy.contains('Register').click();
+
+        // Assert URL 
+        cy.url().should('include', 'register');
+    });
+
+    it('Navigate to browse', () => {
+        cy.contains('View All').click();
+
+        // Assert URL 
+        cy.url().should('include', 'browse');
+    });
+
+    // it('Navigate to details', () => {
+    //     cy.get('.card').click({ multiple: true });
+
+    //     // Assert URL 
+    //     cy.url().should('include', 'details');
+    // });
+
+    it('Clicking display search without entering text does not search', () => {
+        cy.contains('Search').click();
+
+        // Assert URL 
+        cy.url().should('include', 'home');
+    });
+
+    it('Clicking navbar search after typing in it', () => {
+        // Type in to search bar
+        cy.get('.search-input').type('Nick');
+
+        // Click search 
+        cy.contains('Search').click();
+
+        // Assert URL 
+        cy.url().should('include', 'query');
+    });
+
+    it('Clicking navbar search without entering text does not search', () => {
+        // Click search
+        cy.contains('Search').click();
+
+        // Assert URL 
+        cy.url().should('include', 'home');
+    });
+
+    it('Clicking display search after typing in it', () => {
+        // Type in to search bar
+        cy.get('.search-input').type('Nick');
+
+        // Click search 
+        cy.contains('Search').click();
+
+        // Assert URL 
+        cy.url().should('include', 'query');
+    });
+
+    it('Click on contribute button while logged out.', () => {
+        cy.contains('Contribute to CLARK').click();
+        // Assert URL 
+        cy.url().should('include', 'login');
+    });
+
+    it('Click on contribute button while logged in.', () => {
+        // Click sign in button 
+        cy.contains('Sign in').click();
+
+        // Assert URL 
+        cy.url().should('include', 'login');
+
+        // Enter login info 
+        cy.get('input[name=username]').type('nvisal1');
+        cy.get('input[name=password]').type('122595');
+        cy.get('.auth-button').click();
+
+        // Click contribute button at bottom of page
+        cy.contains('Contribute to CLARK').click();
+
+        // Assert URL 
+        cy.url().should('include', 'dashboard');
+    });
+
+    it('Navigate to library.', () => {
+        // Click sign in button 
+        cy.contains('Sign in').click();
+
+        // Assert URL 
+        cy.url().should('include', 'login');
+
+        // Enter login info 
+        cy.get('input[name=username]').type('nvisal1');
+        cy.get('input[name=password]').type('122595');
+        cy.get('.auth-button').click();
+
+        // Click contribute button at bottom of page
+        cy.get('.fa-university').click();
+
+        // Assert URL 
+        cy.url().should('include', 'library');
+    });
+
+    it('Navigate from library to home.', () => {
+        // Click sign in button 
+        cy.contains('Sign in').click();
+
+        // Assert URL 
+        cy.url().should('include', 'login');
+
+        // Enter login info 
+        cy.get('input[name=username]').type('nvisal1');
+        cy.get('input[name=password]').type('122595');
+        cy.get('.auth-button').click();
+
+        // Click contribute button at bottom of page
+        cy.get('.fa-university').click();
+
+        // Assert library URL 
+        cy.url().should('include', 'library');
+
+        // Return to home page 
+        cy.contains('browsing some learning objects!').click();
+
+        // Assert home URL
+        cy.url().should('include', 'home');
+    });
+
+    it('Navigate from library to home by clicking on CLARK logo in navbar.', () => {
+        // Click sign in button 
+        cy.contains('Sign in').click();
+
+        // Assert URL 
+        cy.url().should('include', 'login');
+
+        // Enter login info 
+        cy.get('input[name=username]').type('nvisal1');
+        cy.get('input[name=password]').type('122595');
+        cy.get('.auth-button').click();
+
+        // Click contribute button at bottom of page
+        cy.get('.fa-university').click();
+
+        // Assert library URL 
+        cy.url().should('include', 'library');
+
+        // Return to home page 
+        cy.get('.topbar').contains('C.L.A.R.K.').click();
+
+        // Assert home URL
+        cy.url().should('include', 'home');
     });
 });
