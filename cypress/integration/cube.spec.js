901d73294fdaf30a9aaab065a3731708be7d41aa
Nicholas Visalli <nvisal1225@gmail.com>
2018-07-02 12:52:06 -0400

    cube spec testing

--- cypress/integration/cube.spec.js
+++ cypress/integration/cube.spec.js
@@ -14,4 +14,24 @@ describe('Cube', () => {
     it('Has page title', () => {
         cy.contains('Cybersecurity curriculum at your fingertips.');
     });
+
+    it('Navbar renders correctly', () => {
+        cy.get('.topbar').contains('C.L.A.R.K.');
+        cy.get('input[placeholder=Search...]');
+        cy.get('.topbar').contains('Sign in');
+        cy.get('.topbar').contains('Register');
+    });
+
+    it('Sign in', () => {
+        // Click sign in button 
+        cy.contains('Sign in').click();
+
+        // Assert URL 
+        cy.url().should('include', 'login');
+
+    });
+
+    it('Register new user', () => {
+        cy.contains('Register');
+    });
 });
