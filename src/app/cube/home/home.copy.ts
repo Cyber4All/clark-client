65276a13c0dc68d47bd0ea344e449d8e5d36bdd9
Nick Winner <design@winnerdigital.net>
2018-08-13 15:56:15 -0400

    Fix wording on homepage

--- src/app/cube/home/home.copy.ts
+++ src/app/cube/home/home.copy.ts
@@ -1,8 +1,8 @@
 export const COPY = {
-    HERO: `Effective cybersecurity curriculum at your fingertips.`,
+    HERO: `Effective cybersecurity curriculum at your fingertips`,
 
     WHATISTITLE: `What is C.L.A.R.K.?`,
-    WHATIS: `CLARK is a prototype curriculum management platform that will host a diverse library cybersecurity learning objects. 
+    WHATIS: `CLARK is a prototype digital library that will host a diverse collection of cybersecurity learning objects. 
     It was created because there is a demonstrated need for a high-quality and high-availability repository for 
     curricular and ancillary resources in the cybersecurity education community.`,
 
