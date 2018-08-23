0f9471aad16189375b7dcd9827bec4d65f99053e
Ballen2713 <ballen15@students.towson.edu>
2018-08-23 15:09:01 -0400

    Comments comform to JDOCS standards

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -138,15 +138,16 @@ export class NavbarComponent implements OnInit, AfterContentChecked, OnDestroy {
   }
 
   /**
-   * naviagtes to the users information by using the 
-   * @constructor authService 
+   * naviagtes to the users information by using the  authService 
    * to open the users profile 
    */
   userprofile() {
     this.router.navigate(['users', this.authService.user.username]);
   }
   
-  /**routes the users to the forgot-password page*/
+  /** 
+   * routes the users to the forgot-password page
+  */
   reset(){
     this.router.navigate(['/auth/forgot-password']);
    }
