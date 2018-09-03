704bacc7a1001a35b8175ccc8ac9ed28fd4d98af
Gustavus Shaw II <gshaw1997@gmail.com>
2018-09-03 09:02:53 -0400

    Remove useless code

--- src/app/onion/content-upload/app/services/file-storage.service.ts
+++ src/app/onion/content-upload/app/services/file-storage.service.ts
@@ -5,56 +5,13 @@ import { Http, Headers } from '@angular/http';
 import { AuthService } from '../../../../core/auth.service';
 import { LearningObject } from '@cyber4all/clark-entity';
 import { File } from '@cyber4all/clark-entity/dist/learning-object';
-import { DZFile } from '../upload/upload.component';
-type LearningObjectFile = File;
+
 @Injectable()
 export class FileStorageService {
   private headers: Headers = new Headers();
 
   constructor(private http: Http, private auth: AuthService) {
     this.headers.append('Content-Type', 'application/json');
-  }
-
-  /**
-   * Sends file to API for uploading to S3 bucket
-   *
-   * @param {string} learningObjectID
-   * @param {any[]} file
-   * @returns {Promise<LearningObjectFile[]>}
-   * @memberof FileStorageService
-   */
-  upload(
-    learningObject: LearningObject,
-    file: DZFile | any
-  ): Promise<LearningObjectFile> {
-    return new Promise((resolve, reject) => {
-      const formData: FormData = new FormData(),
-        xhr: XMLHttpRequest = new XMLHttpRequest();
-      formData.append('learningObjectID', learningObject.id);
-      formData.append(
-        'uploads',
-        file,
-        `${file.fullPath ? this.encodeFilePath(file.fullPath) : file.name}`
-      );
-
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            let response = xhr.response;
-            if (typeof response === 'string') {
-              response = JSON.parse(response);
-            }
-            resolve(response as LearningObjectFile);
-          } else {
-            reject(xhr.response);
-          }
-        }
-      };
-      const route = USER_ROUTES.POST_FILE_TO_LEARNING_OBJECT;
-      xhr.open('POST', route, true);
-      xhr.withCredentials = true;
-      xhr.send(formData);
-    });
   }
 
   /**
@@ -75,15 +32,5 @@ export class FileStorageService {
     return this.http
       .delete(route, { headers: this.headers, withCredentials: true })
       .toPromise();
-  }
-
-  private encodeFilePath(path: string): string {
-    const escapedSlash = /%2F/g;
-    const replacementChar = '%2F';
-    if (escapedSlash.test(path)) {
-      path = path.replace(escapedSlash, `"%2F"`);
-    }
-    const sanitized = path.replace(/\//g, replacementChar);
-    return sanitized;
   }
 }
