b8a2e6d620552ddf57d7c5a37c792f7df17418b6
Nick Winner <design@winnerdigital.net>
2018-09-04 20:09:59 -0400

    :ok_hand: Removed unused methods, replaced concat call with call to makeArray()

--- src/app/onion/content-upload/app/upload/file-upload-status/file-upload-status.component.ts
+++ src/app/onion/content-upload/app/upload/file-upload-status/file-upload-status.component.ts
@@ -26,51 +26,10 @@ export class FileUploadStatusComponent implements OnInit {
   ngOnInit() {}
 
   /**
-   * Returns array of files that are not in a folder
-   *
-   * @returns
-   * @memberof FileUploadStatusComponent
-   */
-  filterFiles() {
-    return this.files.filter(file => !file.rootFolder);
-  }
-
-  /**
    * Concat files and foldesr array
    */
   makeArray() {
     // these types are in fact compatable
     return this.files.concat(this.folders as any);
-  }
-
-  /**
-   * Returns array of Folder objects
-   *
-   * @returns
-   * @memberof FileUploadStatusComponent
-   */
-  filterFolders() {
-    const folders: { items: number; progress: number; name: string }[] = [];
-    const folderMap = new Map<string, number>();
-    const files = this.files.filter(f => f.rootFolder);
-
-    for (const file of files) {
-      const index = folderMap.get(file.rootFolder);
-      let folder = folders[index];
-      if (folder) {
-        folder.items++;
-        folder.progress = (folder.progress + (file.progress ? file.progress : 0)) / 2;
-        folders[index] = folder;
-      } else {
-        folder = {
-          items: 1,
-          progress: file.progress ? file.progress : 0,
-          name: file.rootFolder
-        };
-        folders.push(folder);
-        folderMap.set(file.rootFolder, folders.length - 1);
-      }
-    }
-    return folders;
   }
 }
