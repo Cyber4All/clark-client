7945ff02c53727f6039663a8b47b600feafa9206
Emily Vogel <eav990@gmail.com>
2018-09-05 12:30:23 -0400

    Added comment for reference

--- src/app/cube/details/action-panel/action-panel.component.ts
+++ src/app/cube/details/action-panel/action-panel.component.ts
@@ -103,6 +103,7 @@ export class ActionPanelComponent implements OnInit, OnDestroy {
     }
 
     try {
+      // when the user downloads an object it tries to add it to their library
       if (!this.userIsAuthor) {
         await this.cartService.addToCart(this.learningObject.author.username, this.learningObject.name);
 
