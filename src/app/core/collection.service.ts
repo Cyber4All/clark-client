c6d42cbe11f9019f0dd832c7701045e911a35991
codefactor-io <support@codefactor.io>
2018-10-08 14:52:32 +0000

    [CodeFactor] Apply fixes

--- src/app/core/collection.service.ts
+++ src/app/core/collection.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
-import { USER_ROUTES, PUBLIC_LEARNING_OBJECT_ROUTES } from '@env/route'
+import { USER_ROUTES, PUBLIC_LEARNING_OBJECT_ROUTES } from '@env/route';
 
 export interface Collection {
   name: string;
