647cc3a443db3d7213e43ecce53be8b3e0911f4e
Nick Winner <design@winnerdigital.net>
2018-10-12 14:36:48 -0400

    :lipstick: Animate builder entry

--- src/app/onion/learning-object-builder/components/builder-navbar/builder-navbar.component.ts
+++ src/app/onion/learning-object-builder/components/builder-navbar/builder-navbar.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Routes, ActivatedRoute } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'onion-builder-navbar',
   templateUrl: './builder-navbar.component.html',
-  styleUrls: ['./builder-navbar.component.scss']
+  styleUrls: ['./builder-navbar.component.scss'],
+  animations: [
+    trigger('builderNavbar', [
+      transition(':enter', [
+        style({ 'transform': 'translateY(-200px)', opacity: 0 }),
+        animate('300ms ease', style({ 'transform': 'translateY(0px)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class BuilderNavbarComponent implements OnInit {
 
--- src/app/onion/learning-object-builder/components/column-wrapper/column-wrapper.component.ts
+++ src/app/onion/learning-object-builder/components/column-wrapper/column-wrapper.component.ts
@@ -1,9 +1,29 @@
 import { Component, OnInit, ContentChild, ElementRef, AfterViewInit, ContentChildren } from '@angular/core';
+import { trigger, state, style, transition, animate, query, stagger, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'clark-column-wrapper',
   templateUrl: './column-wrapper.component.html',
-  styleUrls: ['./column-wrapper.component.scss']
+  styleUrls: ['./column-wrapper.component.scss'],
+  animations: [
+    trigger('columns', [
+      transition('* => *', [
+        query(':enter', [
+          style({ 'transform': 'translateY(-200px)', opacity: 0 }),
+        ], { optional: true }),
+        query(':enter', [
+          stagger('250ms', [
+            animate('450ms ease', style({ 'transform': 'translateY(0px)', opacity: 1 }))
+          ])
+        ], { optional: true }),
+        query(':leave', [
+          stagger('250ms', [
+            animate('450ms ease', style({ 'transform': 'translateY(-200px)', opacity: 0 }))
+          ])
+        ], { optional: true }),
+      ])
+    ]),
+  ]
 })
 export class ColumnWrapperComponent implements OnInit, AfterViewInit {
   @ContentChild('left') leftCol: any;
