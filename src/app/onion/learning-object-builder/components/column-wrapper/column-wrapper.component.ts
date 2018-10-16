80cd3bf3efe10c2cadca1f1a642d7efa2cb6db0f
Nick Winner <design@winnerdigital.net>
2018-10-16 14:22:25 -0400

    :lipstick: Modify column layout to simulate fixed positioning of sidebar components in builder

--- src/app/onion/learning-object-builder/components/column-wrapper/column-wrapper.component.ts
+++ src/app/onion/learning-object-builder/components/column-wrapper/column-wrapper.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ContentChild, ElementRef, AfterViewInit, ContentChildren } from '@angular/core';
+import { Component, OnInit, ContentChild, ElementRef, AfterViewInit, ContentChildren, ViewChild, OnDestroy } from '@angular/core';
 import { trigger, state, style, transition, animate, query, stagger, animateChild } from '@angular/animations';
 
 @Component({
@@ -25,20 +25,37 @@ import { trigger, state, style, transition, animate, query, stagger, animateChil
     ]),
   ]
 })
-export class ColumnWrapperComponent implements OnInit, AfterViewInit {
-  @ContentChild('left') leftCol: any;
+export class ColumnWrapperComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('columnWrapper') columnWrapper: ElementRef;
+
+  @ContentChild('left') leftCol: ElementRef;
   @ContentChild('main') mainCol: ElementRef;
   @ContentChild('right') rightCol: ElementRef;
 
-  constructor() { }
+  columnOffset: number;
+  columnHeight: number;
+
+  constructor() {}
 
   ngOnInit() {
+    // calculate the height of the scroll wrapper
+    this.columnOffset = (this.columnWrapper.nativeElement as HTMLElement).offsetTop;
+    this.columnHeight = window.innerHeight - this.columnOffset + 30;
+
+    // set overflow of body to hidden to prevent parent scrolling
+    if (this.columnHeight >= 560) {
+      // this check prevents obsucring the outcomes sidebar
+      document.body.style.overflow = 'hidden';
+    }
   }
 
   ngAfterViewInit() {
     // TODO check for right column and adjust mobile threshold here
     // TODO collapse left panel on smaller screens
-    // TODO add animations unless element has permanent attribute
+  }
+
+  ngOnDestroy() {
+    document.body.style.overflow = 'visible';
   }
 
 }
