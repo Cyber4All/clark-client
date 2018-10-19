c950dad6bc63044f0816816c7e6b4c934d108710
Nick Winner <design@winnerdigital.net>
2018-10-19 11:20:58 -0400

    :fire: Remove duplicate content-upload application

--- src/app/onion/content-upload/app/content-upload.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'content-upload-root',
-  templateUrl: './content-upload.component.html'
-})
-export class ContentUploadComponent {}
