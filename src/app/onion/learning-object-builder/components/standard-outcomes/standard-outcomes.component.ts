a4c6825aae11fa69940fd2d9758f3c4ab123b410
Nick Winner <design@winnerdigital.net>
2018-10-29 11:21:17 -0400

    :bug: :refactor: Fix bug where standard outcomes component doesn't search on load, refactor component into smaller functions

--- src/app/onion/learning-object-builder/components/standard-outcomes/standard-outcomes.component.ts
+++ src/app/onion/learning-object-builder/components/standard-outcomes/standard-outcomes.component.ts
@@ -1,6 +1,5 @@
 import {
   Component,
-  OnInit,
   Input,
   OnChanges,
   SimpleChanges,
@@ -26,7 +25,7 @@ export interface SuggestedOutcome extends LearningOutcome {
   templateUrl: './standard-outcomes.component.html',
   styleUrls: ['./standard-outcomes.component.scss']
 })
-export class StandardOutcomesComponent implements OnInit, OnChanges, OnDestroy {
+export class StandardOutcomesComponent implements OnChanges, OnDestroy {
   // id of the currently selected outcome
   @Input()
   activeOutcome: string;
@@ -57,7 +56,27 @@ export class StandardOutcomesComponent implements OnInit, OnChanges, OnDestroy {
   constructor(
     private outcomeService: OutcomeService,
     private store: BuilderStore
-  ) {}
+  ) {
+    // handle searching
+    this.searchString$
+      .pipe(
+        takeUntil(this.componentDestroyed$),
+        debounceTime(650)
+      )
+      .subscribe(() => {
+        this.performSearch();
+      });
+
+    // handle suggesting
+    this.suggestString$
+      .pipe(
+        takeUntil(this.componentDestroyed$),
+        debounceTime(1000)
+      )
+      .subscribe((val: string) => {
+        this.performSuggest(val);
+      });
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.activeOutcome && changes.activeOutcome.currentValue) {
@@ -72,9 +91,7 @@ export class StandardOutcomesComponent implements OnInit, OnChanges, OnDestroy {
       this.activeOutcomeSubscription = this.store.outcomeEvent
         .pipe(
           takeUntil(this.componentDestroyed$),
-          filter(
-            x => x && !!x.get(this.activeOutcome)
-          ),
+          filter(x => x && !!x.get(this.activeOutcome)),
           map(x => x.get(this.activeOutcome))
         )
         .subscribe((outcome: LearningOutcome) => {
@@ -99,65 +116,53 @@ export class StandardOutcomesComponent implements OnInit, OnChanges, OnDestroy {
     }
   }
 
-  ngOnInit() {
-    // handle searching
-    this.searchString$
-      .pipe(
-        takeUntil(this.componentDestroyed$),
-        debounceTime(650)
-      )
-      .subscribe(() => {
-        if (!this.searchStringValue || this.searchStringValue === '') {
-          // string was empty, clear results
-          this.searchResults = [];
-        } else {
-          // perform search
-          this.loading = 'search';
-          this.outcomeService
-            .getOutcomes({
-              text: this.searchStringValue
-            })
-            .then(results => {
-              if (this.suggestions.length) {
-                // if we have suggestions, tag any of the search results that are also suggestions
-                const suggestedIds: string[] = this.suggestions.map(o => o.id);
-                this.searchResults = results.outcomes.map(o => {
-                  o.suggested = suggestedIds.includes(o.id);
-                  return o;
-                });
-              } else {
-                this.searchResults = results.outcomes;
-              }
-
-              this.loading = undefined;
-            });
-        }
-      });
-
-    // handle suggesting
-    this.suggestString$
-      .pipe(
-        takeUntil(this.componentDestroyed$),
-        debounceTime(1000)
-      )
-      .subscribe((val: string) => {
-        if (val && val !== '') {
-          this.loading = 'suggest';
-          this.outcomeService
-            .suggestOutcomes(this.store.learningObjectEvent.getValue(), { text: val })
-            .then(results => {
-              this.suggestions = results.map(o => {
-                o.suggested = true;
-                return o;
-              });
-              this.loading = undefined;
-            });
-        }
-      });
-  }
-
   toggleStandardOutcome(standardOutcome: LearningOutcome, value: boolean) {
     this.toggleMapping.emit({ standardOutcome, value });
+  }
+
+  performSearch() {
+    if (!this.searchStringValue || this.searchStringValue === '') {
+      // string was empty, clear results
+      this.searchResults = [];
+    } else {
+      // perform search
+      this.loading = 'search';
+      this.outcomeService
+        .getOutcomes({
+          text: this.searchStringValue
+        })
+        .then(results => {
+          if (this.suggestions.length) {
+            // if we have suggestions, tag any of the search results that are also suggestions
+            const suggestedIds: string[] = this.suggestions.map(o => o.id);
+            this.searchResults = results.outcomes.map(o => {
+              o.suggested = suggestedIds.includes(o.id);
+              return o;
+            });
+          } else {
+            this.searchResults = results.outcomes;
+          }
+
+          this.loading = undefined;
+        });
+    }
+  }
+
+  performSuggest(val: string) {
+    if (val && val !== '') {
+      this.loading = 'suggest';
+      this.outcomeService
+        .suggestOutcomes(this.store.learningObjectEvent.getValue(), {
+          text: val
+        })
+        .then(results => {
+          this.suggestions = results.map(o => {
+            o.suggested = true;
+            return o;
+          });
+          this.loading = undefined;
+        });
+    }
   }
 
   ngOnDestroy() {
