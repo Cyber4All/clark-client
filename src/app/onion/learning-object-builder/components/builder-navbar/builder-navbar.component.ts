0d243c6252abd9d1a19b57bd69652c5ea30bb07e
Nick Winner <design@winnerdigital.net>
2018-10-30 09:32:22 -0400

    :sparkles: Stop saving when a learning object's name is removed

--- src/app/onion/learning-object-builder/components/builder-navbar/builder-navbar.component.ts
+++ src/app/onion/learning-object-builder/components/builder-navbar/builder-navbar.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Routes, ActivatedRoute } from '@angular/router';
-import { trigger, transition, style, animate } from '@angular/animations';
+import { trigger, transition, style, animate, state } from '@angular/animations';
+import { BuilderStore } from '../../builder-store.service';
+import { AuthService } from 'app/core/auth.service';
 
 @Component({
   selector: 'onion-builder-navbar',
   templateUrl: './builder-navbar.component.html',
   styleUrls: ['./builder-navbar.component.scss'],
   animations: [
+    trigger('route', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(25px)' }),
+        animate('250ms ease', style({ opacity: 1, transform: 'translateY(0px)' }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, transform: 'translateY(0px)' }),
+        animate('250ms ease', style({ opacity: 0, transform: 'translateY(15px)' }))
+      ])
+    ])
   ]
 })
 export class BuilderNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store: BuilderStore, private auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  canRoute(route: string) {
+    switch (route) {
+      case 'outcomes':
+        return !!(this.store.saveable);
+      case 'materials':
+        return !!(this.auth.user.emailVerified && this.store.saveable);
+    }
   }
 
 }
