0d497fd114197b8336a296b6ed7f5862a62a8562
Gustavus Shaw II <gshaw1997@gmail.com>
2018-11-07 12:40:55 -0500

    :sparkles: Add methods for initializing and finalizing multipart upload

--- src/app/onion/content-upload/app/services/file-storage.service.ts
+++ src/app/onion/content-upload/app/services/file-storage.service.ts
@@ -8,8 +8,63 @@ import { File } from '@cyber4all/clark-entity/dist/learning-object';
 
 @Injectable()
 export class FileStorageService {
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
-  constructor(private http: HttpClient, private auth: AuthService) {
+  /**
+   * Sends request to initiate multipart upload
+   *
+   * @param {{
+   *     learningObject: LearningObject;
+   *     fileId: string;
+   *     filePath: string;
+   *   }} params
+   * @returns {Promise<any>}
+   * @memberof FileStorageService
+   */
+  initMultipart(params: {
+    learningObject: LearningObject;
+    fileId: string;
+    filePath: string;
+  }): Promise<any> {
+    const route = USER_ROUTES.INIT_MULTIPART({
+      fileId: params.fileId,
+      username: this.auth.user.username,
+      objectId: params.learningObject.id
+    });
+
+    return this.http
+      .post(route, { filePath: params.filePath }, { withCredentials: true })
+      .toPromise();
+  }
+
+  /**
+   * Sends request to finalize multipart upload
+   *
+   * @param {{
+   *     learningObject: LearningObject;
+   *     fileId: string;
+   *   }} params
+   * @returns {Promise<any>}
+   * @memberof FileStorageService
+   */
+  finalizeMultipart(params: {
+    learningObject: LearningObject;
+    fileId: string;
+    fileMeta: any;
+  }): Promise<any> {
+    const route = USER_ROUTES.FINALIZE_MULTIPART({
+      fileId: params.fileId,
+      username: this.auth.user.username,
+      objectId: params.learningObject.id
+    });
+
+    return this.http
+      .patch(
+        route,
+        { fileMeta: params.fileMeta },
+        { withCredentials: true, responseType: 'text' }
+      )
+      .toPromise();
   }
 
   /**
