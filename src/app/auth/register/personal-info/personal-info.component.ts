9c80d96a027672c0083e401f97669cdfd19a4e26
Nick Visalli <nvisal1@students.towson.edu>
2018-11-09 10:54:09 -0500

    remove organization check in registration

--- src/app/auth/register/personal-info/personal-info.component.ts
+++ src/app/auth/register/personal-info/personal-info.component.ts
@@ -27,8 +27,6 @@ export class PersonalInfoComponent implements AfterViewInit, OnDestroy {
   @Output() hasResults = new EventEmitter();
   @ViewChild('emailInput', { read: ElementRef })
   emailInput: ElementRef;
-  @ViewChild('organization', { read: ElementRef })
-  organization: ElementRef;
   emailError = false;
   querying = false;
   result: boolean;
@@ -63,75 +61,6 @@ export class PersonalInfoComponent implements AfterViewInit, OnDestroy {
         });
       })
     );
-    // listen for input events on the income input and send text to suggestion component after 650 ms of debounce
-    this.subs.push(fromEvent(this.organization.nativeElement, 'input')
-      .map(x => x['currentTarget'].value)
-      .debounceTime(400)
-      .subscribe(val => {
-        this.querying = true;
-        this.getOrganizations(val);
-      })
-    );
-  }
-
-  getOrganizations(currentOrganization) {
-    this.auth.getOrganizations(currentOrganization).then(val => {
-        this.querying = false;
-        // destroy results display when no results or empty query
-        if (!val[0] || currentOrganization === '') {
-          this.organizationsList = [];
-          this.checkOrganization();
-        } else {
-          // Display top 5 matching organizations
-          for (let i = 0; i < 5; i++) {
-            if (val[i]) {
-              this.organizationsList[i] = val[i]['institution'];
-          }
-        }
-        this.checkOrganization();
-      }
-    });
-  }
-
-  chooseOrganization(organization: string) {
-    // Prevent user from clicking on emtpy result.
-    if (organization !== '') {
-      this.group.controls['organization'].setValue(organization);
-      this.checkOrganization();
-      // After org is selected, destroy results display
-      this.organizationsList = [];
-    }
-  }
-
-  private checkOrganization() {
-    if (this.organizationsList.length > 0) {
-      // Check if current query matches any org within the results
-      this.searchResults();
-    } else {
-      // Allow user to enter organization that does not exist within our database
-      // when no results are returned
-      // Check for empty input exception
-      if (this.group.controls['organization'].value === '') {
-        this.hasResults.emit(false);
-      } else {
-        this.hasResults.emit(true);
-      }
-    }
-  }
-
-  private searchResults() {
-    for (let i = 0; i < this.organizationsList.length; i++) {
-      if (this.group.controls['organization'].value === this.organizationsList[i]) {
-        // Emit boolean that the parent component uses as a check when proceeding to next step
-        this.hasResults.emit(true);
-        this.isValid = true;
-      }
-    }
-    // Emit boolean that the parent component uses as a check when proceeding to next step
-    if (!this.isValid) {
-      this.hasResults.emit(false);
-    }
-    this.isValid = false;
   }
 
   ngOnDestroy() {
