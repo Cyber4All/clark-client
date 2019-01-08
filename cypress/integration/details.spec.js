220bd3b29412b696b2d98c928f3732ce5098ed13
Nick Visalli <nick@Nicks-MacBook-Pro.local>
2019-01-08 08:28:09 -0500

    remove old builder and dashboard tests

--- cypress/integration/details.spec.js
+++ cypress/integration/details.spec.js
@@ -58,42 +58,7 @@ describe('Details', () => {
         cy.url().should('include', 'home');
     });
 
-     it('Navigate to details and clicking the rating to scroll page', () => {
-        // Wait for learning objects to load on page
-        cy.wait(1000);
-
-        // Click left-most card
-        cy.get('.learning-object').first().click({ force: true }, { multiple: true });
-
-        // TODO all login code can be removed when pulling ratings from server
-        // Click author name
-        cy.get('.topbar .login').click({ force: true });
-
-        // Assert URL 
-        cy.url().should('include', 'login');
-
-        cy.wait(1000);
-
-        // Enter login info
-        // Different steps for login - don't replace with helper method
-        cy.get('input[name=username]').type('nwinne1');
-        cy.get('input[name=password]').type('testpassword');
-        cy.get('#auth-button').click({ force: true });
-
-        cy.wait(1000);
-
-        cy.url().should('include', 'details');
-
-        cy.get('.rating > clark-rating-stars').first().click({ force: true }, { multiple: true });
-
-        cy.window().then(($w) => {
-            cy.get('.ratings').invoke('offset').its('top').should(($p) => {
-                expect($p - $w.scrollY).to.be.at.most(200)
-            });      
-        })
-     });
-
-     it('Navigate to details and clicking on \'Write a review\'', () => {
+    it('Navigate to details and clicking on \'Write a review\'', () => {
         // Wait for learning objects to load on page
         cy.wait(1000);
 
@@ -112,20 +77,8 @@ describe('Details', () => {
 
         cy.get('.new-rating-wrapper').should('be.visible').should('have.class', 'active');
         cy.get('.new-rating').should('be.visible');
-     });
+    });
 
-    //  it('From details page, click \'report\' on a rating', () => {
-    //     // Wait for learning objects to load on page
-    //     cy.visit('http://localhost:4200/details/dark/Cybersecurity%20and%20Society');
-
-    //     cy.wait(1000);
-
-    //     cy.visit('.rating-list-element .options').first().click({multiple: true})
-    //  })
-
-    // it('Navigate to details and click DOWNLOAD NOW button when logged out', () => {
-    //     // Wait for learning objects to load on page
-    //     cy.wait(1000);
     it('Navigate to details and click DOWNLOAD NOW button when logged out', () => {
         // Wait for learning objects to load on page
         cy.wait(1000);
@@ -140,25 +93,6 @@ describe('Details', () => {
 
         // Assert URL 
         cy.url().should('include', 'details');
-    });
-
-    it('Navigate to details and click DOWNLOAD NOW button', () => {
-        // Login 
-        cy.login();
-
-        // Wait for learning objects to load on page
-        cy.wait(1000);
-
-        // Click left-most card
-        cy.get('#learning-object').first().click({ force: true }, { multiple: true });
-
-        // Assert Details page URL 
-        cy.url().should('include', 'details');
-
-        // Click DOWNLOAD NOW button 
-        // if disabled class is present, it is working correctly.
-        cy.wait(1000);
-        cy.get('#download-button').click({ force: true });
     });
 
     it('Navigate to details and click SAVE TO LIBRARY button', () => {
@@ -190,6 +124,26 @@ describe('Details', () => {
         cy.get('#remove-library-item').click({ force: true });
 
         cy.get('#library-count').contains('1');
+    });
+
+
+    it('Navigate to details and click DOWNLOAD NOW button', () => {
+        // Login 
+        cy.login();
+
+        // Wait for learning objects to load on page
+        cy.wait(1000);
+
+        // Click left-most card
+        cy.get('#learning-object').first().click({ force: true }, { multiple: true });
+
+        // Assert Details page URL 
+        cy.url().should('include', 'details');
+
+        // Click DOWNLOAD NOW button 
+        // if disabled class is present, it is working correctly.
+        cy.wait(1000);
+        cy.get('#download-button').click({ force: true });
     });
 
     it('Navigate to details and click all of the share buttons', () => {
