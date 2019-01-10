8dd7a274bf50b39c18cda98da37a13e011e482d0
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-10 14:04:07 -0500

    :sparkles: Add distribution chart component

---
+++ src/app/cube/home/usage-stats/distribution-chart/distribution-chart.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { PieChart, DoughnutChart } from '../types';
+
+@Component({
+  selector: 'cube-distribution-chart',
+  templateUrl: 'distribution-chart.component.html',
+  styleUrls: ['distribution-chart.component.scss']
+})
+export class DistributionChartComponent implements OnInit {
+  @Input() chart: PieChart | DoughnutChart;
+  constructor() {}
+
+  ngOnInit() {}
+}
