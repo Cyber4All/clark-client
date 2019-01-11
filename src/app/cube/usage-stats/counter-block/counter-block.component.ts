388b0e3aecf6601183c2af3d3bcd192c52cc98be
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-11 12:55:57 -0500

    :sparkles: Add counter block component

---
+++ src/app/cube/usage-stats/counter-block/counter-block.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+export interface CounterStat {
+  title: string;
+  value: number;
+}
+
+@Component({
+  selector: 'cube-counter-block',
+  templateUrl: 'counter-block.component.html',
+  styleUrls: ['counter-block.component.scss']
+})
+export class CounterBlockComponent implements OnInit {
+  @Input() stat: CounterStat;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
