85ad3f9fb5076494d452db5465bdf0fc1191ef92
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-14 17:36:10 -0500

    :sparkles: Add percentage numbers on pie charts

--- src/app/cube/usage-stats/usage-stats.component.ts
+++ src/app/cube/usage-stats/usage-stats.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UsageStats, PieChart } from '../shared/types';
 import { CounterStat } from './counter-block/counter-block.component';
 
+// This variable is used to decided whether or not percentages should be rendered.
+// If CHART_HOVERED, tooltips are visible and we do not want to render percentages over tooltips
+let CHART_HOVERED = false;
 @Component({
   selector: 'cube-usage-stats',
   templateUrl: 'usage-stats.component.html',
@@ -64,16 +67,16 @@ export class UsageStatsComponent implements OnInit {
           value: this.usageStats.objects.underReview
         },
         {
-          title: 'Learning Objects Downloaded',
-          value: this.usageStats.objects.underReview
-        },
-        {
           title: 'Users',
           value: this.usageStats.users.total
         },
         {
           title: 'Affiliated Organizations',
           value: this.usageStats.users.organizations
+        },
+        {
+          title: 'Learning Objects Downloaded',
+          value: this.usageStats.objects.underReview
         }
       ]
     );
@@ -102,6 +105,14 @@ export class UsageStatsComponent implements OnInit {
         responsive: true,
         legend: {
           position: 'bottom'
+        },
+        hover: {
+          onHover: () => {
+            CHART_HOVERED = true;
+          }
+        },
+        animation: {
+          onComplete: generatePieSliceLabels
         }
       },
       colors: [
@@ -142,6 +153,15 @@ export class UsageStatsComponent implements OnInit {
         responsive: true,
         legend: {
           position: 'bottom'
+        },
+        hover: {
+          onHover: () => {
+            CHART_HOVERED = true;
+          }
+        },
+        // @ts-ignore
+        animation: {
+          onComplete: generatePieSliceLabels
         }
       },
       colors: [
@@ -150,5 +170,56 @@ export class UsageStatsComponent implements OnInit {
         }
       ]
     };
+  }
+
+  /**
+   * Sets CHART_HOVERED to true
+   *
+   * @memberof UsageStatsComponent
+   */
+  handleChartNotHovered() {
+    CHART_HOVERED = false;
+  }
+}
+
+/**
+ * Renders percentages on pie slices
+ *
+ */
+function generatePieSliceLabels() {
+  const ctx = this.chart.ctx;
+  ctx.font = '16px Arial';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'bottom';
+  // If user is not hovering over chart (meaning tooltips are visible), render percentage slices
+  if (!CHART_HOVERED) {
+    this.data.datasets.forEach(function(dataset) {
+      // Sum of data values
+      const total = dataset._meta[Object.keys(dataset._meta)[0]].total;
+      for (let i = 0; i < dataset.data.length; i++) {
+        // Info about Pie Slice
+        const model =
+          dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model;
+        // Calc midpoint of radius
+        const mid_radius =
+          model.innerRadius + (model.outerRadius - model.innerRadius) / 2;
+        const start_angle = model.startAngle;
+        const end_angle = model.endAngle;
+        // Calc midpoint of angle
+        const mid_angle = start_angle + (end_angle - start_angle) / 2;
+        // Calc x coordinate of angle midpoint
+        const x = mid_radius * Math.cos(mid_angle);
+        // Calc y coordinate of angle midpoint
+        const y = mid_radius * Math.sin(mid_angle);
+
+        ctx.fillStyle = '#fff';
+        const percent = Math.round((dataset.data[i] / total) * 100);
+        // Don't Display If value is zero or percent is less than 10
+        if (dataset.data[i] !== 0 && percent >= 10) {
+          // Display percentage
+          ctx.fillText(`${percent}%`, model.x + x, model.y + y);
+        }
+      }
+    });
   }
 }
