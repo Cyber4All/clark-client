12bf300a6e61e6608ba51d89e6fa5b9a4924ea87
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-15 10:53:57 -0500

    :sparkles: Add LearningObjectStats and UserStats types

--- src/app/cube/shared/types/index.ts
+++ src/app/cube/shared/types/index.ts
@@ -1,4 +1,13 @@
 import { UsageStats } from './usage-stats';
 import { Chart, DoughnutChart, PieChart } from './chart';
+import { LearningObjectStats } from './learning-object-stats';
+import { UserStats } from './user-stats';
 
-export { UsageStats, Chart, PieChart, DoughnutChart };
+export {
+  UsageStats,
+  Chart,
+  PieChart,
+  DoughnutChart,
+  LearningObjectStats,
+  UserStats
+};
