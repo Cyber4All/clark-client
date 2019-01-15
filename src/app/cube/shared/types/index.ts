eacae35fd1730be3ae4ffb79c233f78456d2a651
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-15 13:25:50 -0500

    :fire: Remove chart types

--- src/app/cube/shared/types/index.ts
+++ src/app/cube/shared/types/index.ts
@@ -1,13 +1,5 @@
 import { UsageStats } from './usage-stats';
-import { Chart, DoughnutChart, PieChart } from './chart';
 import { LearningObjectStats } from './learning-object-stats';
 import { UserStats } from './user-stats';
 
-export {
-  UsageStats,
-  Chart,
-  PieChart,
-  DoughnutChart,
-  LearningObjectStats,
-  UserStats
-};
+export { UsageStats, LearningObjectStats, UserStats };
