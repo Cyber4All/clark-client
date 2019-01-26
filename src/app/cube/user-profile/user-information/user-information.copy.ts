8dccd2ea3d0c66fb3081a91ef0678921e4445471
Gustavus Shaw II <gshaw1997@gmail.com>
2019-01-25 22:40:17 -0500

    Update copy to say ' You don't have any released...'

--- src/app/cube/user-profile/user-information/user-information.copy.ts
+++ src/app/cube/user-profile/user-information/user-information.copy.ts
@@ -2,7 +2,7 @@ export const COPY = {
     LEARNINGOBJECTS: 'Learning Objects',
     EMAILVERIFY: 'Verify your email',
     NOOBJECTS: (self?: boolean) => {
-        return `${self ? 'You don\'t' : 'This user doesn\'t'} have any learning objects yet!`;
+        return `${self ? 'You don\'t' : 'This user doesn\'t'} have any released learning objects yet!`;
     },
     CREATE: 'Create a Learning Object!',
     PRINT: 'Print Clark Cards'
