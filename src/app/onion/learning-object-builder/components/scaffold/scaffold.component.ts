cb4c5fff090d57201af88649589f570fee4ecf3f
Paige Zaleppa <paigezaleppa@paiges-mbp.towson.edu>
2019-02-04 13:05:04 -0500

    Start of adding functionality

--- src/app/onion/learning-object-builder/components/scaffold/scaffold.component.ts
+++ src/app/onion/learning-object-builder/components/scaffold/scaffold.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop'; 
+import { LearningObject } from '@cyber4all/clark-entity';
 
 @Component({
   selector: 'clark-scaffold',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scaffold.component.scss']
 })
 export class ScaffoldComponent implements OnInit {
-
+  @Input() learningObject: LearningObject;
   constructor() { }
 
   ngOnInit() {
+    disabled: true; 
+  }
+  children = [
+    'Bronze age',
+    'Iron age',
+    'Middle ages',
+    'Early modern period',
+    'Long nineteenth century'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.children, event.previousIndex, event.currentIndex);
   }
 
 }
