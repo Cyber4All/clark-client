a01ef00418d66a95400837f0f73fbbb313245518
Paige Zaleppa <paigezaleppa@paiges-mbp.towson.edu>
2019-02-06 10:41:20 -0500

    Add hierarchy component

---
+++ src/app/onion/learning-object-builder/components/scaffold/hierarchy/hierarchy.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'clark-hierarchy',
+  templateUrl: './hierarchy.component.html',
+  styleUrls: ['./hierarchy.component.scss']
+})
+export class HierarchyComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
