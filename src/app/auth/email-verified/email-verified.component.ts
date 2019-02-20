51afbcde48dcc6c835fd5993eea2fdfe2d33e69a
Sean Donnelly <sdonnelly3000@gmail.com>
2019-02-20 00:11:52 -0500

    :bug: Fix bad refresh token request

--- src/app/auth/email-verified/email-verified.component.ts
+++ src/app/auth/email-verified/email-verified.component.ts
@@ -3,12 +3,31 @@ import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'app-email-verified',
+  styles: [
+    `.message--in-progress {
+      color: red;
+    }`
+  ],
   templateUrl: './email-verified.component.html'
 })
 export class EmailVerifiedComponent implements OnInit {
+  isLoading = true;
+  hasValidToken;
+
   constructor(private auth: AuthService) {}
 
   ngOnInit() {
-    this.auth.refreshToken();
+    this.auth.validate()
+      .then(async () => {
+        // Token is good, refresh it and go home
+        await this.auth.refreshToken();
+        this.hasValidToken = true;
+        this.isLoading = false;
+      })
+      .catch(e => {
+        this.isLoading = false;
+        this.hasValidToken = false;
+        // Token is bad, they must sign in
+      });
   }
 }
