6752fb3e3c3d10345af64e29a82229eecd9fe3ab
Sean Donnelly <sdonnelly3000@gmail.com>
2019-02-25 12:03:09 -0500

    :rotating_light: Update selector prefix

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../core/auth.service';
 
 @Component({
-  selector: 'app-forgot-password',
+  selector: 'clark-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
