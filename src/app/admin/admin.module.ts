ac23b8ad61aabd66c2563128ccb69b9b2b3658a7
Nick Visalli <nvisal1@students.towson.edu>
2019-02-25 10:52:15 -0500

    create admin search bar component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,9 +7,18 @@ import { LearningObjectsComponent } from './pages/learning-objects/learning-obje
 import { AnalyticsComponent } from './pages/analytics/analytics.component';
 import { UsersComponent } from './pages/users/users.component';
 import { ContentWrapperComponent } from './components/content-wrapper/content-wrapper.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
-  declarations: [AdminComponent, SidebarComponent, LearningObjectsComponent, AnalyticsComponent, UsersComponent, ContentWrapperComponent],
+  declarations: [
+    AdminComponent,
+    SidebarComponent,
+    LearningObjectsComponent,
+    AnalyticsComponent,
+    UsersComponent,
+    ContentWrapperComponent,
+    SearchBarComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
