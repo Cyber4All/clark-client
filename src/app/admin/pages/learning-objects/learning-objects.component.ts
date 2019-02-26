23e60b064ad2e1fece1b46435bb4d176ff8deaa1
Nick Visalli <nvisal1@students.towson.edu>
2019-02-26 10:24:58 -0500

    rewrite selectors to follow bem

--- src/app/admin/pages/learning-objects/learning-objects.component.ts
+++ src/app/admin/pages/learning-objects/learning-objects.component.ts
@@ -20,27 +20,27 @@ export class LearningObjectsComponent implements OnInit {
 
   ngOnInit() {
     this.learningObjects = [
-      {
-        _id: '5aa0183becba9a264dcd806b',
-        authorID: '5a70fb5ed45bde3f9d65a88c',
-        date: '1520530818714',
-        goals: [
-          {
-            text: 'Introduce the concept of integer overflow in first programming courses'
-          }
-        ],
-        outcomes: [
-          '5aa0183becba9a264dcd806c'
-        ],
-        published: true,
-        name: 'Integer Error - CS2 - C++',
-        length: "nanomodule",
-        materials:{
-          files:[],
-          urls:[
-            // tslint:disable-next-line:max-line-length
-            {title:'Online module','url':'http://cis1.towson.edu/~cyber4all/modules/nanomodules/Integer_Error-CS2_C++.html'},{'title':'See a Security Injection Module in Action','url':'https://youtu.be/MH_RD1jh0AE'},{'title':'How to use this module in your class','url':'https://youtu.be/u47q-qX52JI'}],'notes':'','pdf':{'name':'0ReadMeFirst - Integer Error - CS2 - C++.pdf','url':'https://neutrino-file-uploads.s3.us-east-2.amazonaws.com/skaza/5aa0183becba9a264dcd806b/0ReadMeFirst%20-%20Integer%20Error%20-%20CS2%20-%20C%2B%2B.pdf'}},'levels':['undergraduate'],'contributors':['5c6d71c29554c723fba68b19'],'collection':'secinj','lock':{'date':{'$numberDouble':'1538061211867'},'restrictions':['download']},'status':'released','description':'Introduce the concept of integer overflow in first programming courses'}
-    ]
+      // {
+      //   _id: '5aa0183becba9a264dcd806b',
+      //   authorID: '5a70fb5ed45bde3f9d65a88c',
+      //   date: '1520530818714',
+      //   goals: [
+      //     {
+      //       text: 'Introduce the concept of integer overflow in first programming courses'
+      //     }
+      //   ],
+      //   outcomes: [
+      //     '5aa0183becba9a264dcd806c'
+      //   ],
+      //   published: true,
+      //   name: 'Integer Error - CS2 - C++',
+      //   length: "nanomodule",
+      //   materials:{
+      //     files:[],
+      //     urls:[
+      //       // tslint:disable-next-line:max-line-length
+      //       {title:'Online module','url':'http://cis1.towson.edu/~cyber4all/modules/nanomodules/Integer_Error-CS2_C++.html'},{'title':'See a Security Injection Module in Action','url':'https://youtu.be/MH_RD1jh0AE'},{'title':'How to use this module in your class','url':'https://youtu.be/u47q-qX52JI'}],'notes':'','pdf':{'name':'0ReadMeFirst - Integer Error - CS2 - C++.pdf','url':'https://neutrino-file-uploads.s3.us-east-2.amazonaws.com/skaza/5aa0183becba9a264dcd806b/0ReadMeFirst%20-%20Integer%20Error%20-%20CS2%20-%20C%2B%2B.pdf'}},'levels':['undergraduate'],'contributors':['5c6d71c29554c723fba68b19'],'collection':'secinj','lock':{'date':{'$numberDouble':'1538061211867'},'restrictions':['download']},'status':'released','description':'Introduce the concept of integer overflow in first programming courses'}
+    ];
   }
 
   /**
