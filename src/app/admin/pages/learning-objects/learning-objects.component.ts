e09e3341ffd91f1e730659e278ca56905cc40f82
Nick Visalli <nvisal1@students.towson.edu>
2019-02-28 08:17:49 -0500

    implement learning object search and virtual scrolling

--- src/app/admin/pages/learning-objects/learning-objects.component.ts
+++ src/app/admin/pages/learning-objects/learning-objects.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LearningObject } from '@cyber4all/clark-entity';
 import { LearningObjectService } from 'app/cube/learning-object.service';
-import { DashboardLearningObject } from 'app/onion/dashboard/dashboard.component';
-import { lengths as LengthsSet } from '@cyber4all/clark-taxonomy';
 
 @Component({
   selector: 'clark-learning-objects',
@@ -12,58 +9,23 @@ import { lengths as LengthsSet } from '@cyber4all/clark-taxonomy';
 })
 export class LearningObjectsComponent implements OnInit {
 
-  learningObjects: any[];
+  learningObjects: any;
+  searchBarPlaceholder = 'Learning Objects';
 
   constructor(
     private learningObjectService: LearningObjectService,
   ) { }
 
-  ngOnInit() {
-    this.learningObjects = [
-      {
-        _id: '5aa0183becba9a264dcd806b',
-        author: {
-          name: 'nick visalli',
-        },
-        date: '1520530818714',
-        goals: [
-          {
-            text: 'Introduce the concept of integer overflow in first programming courses'
-          }
-        ],
-        outcomes: [
-          '5aa0183becba9a264dcd806c'
-        ],
-        published: true,
-        name: 'This is a really really really really really really really really long title',
-        length: 'nanomodule',
-        materials:{
-          files:[],
-          urls:[
-            // tslint:disable-next-line:max-line-length
-            {title:'Online module','url':'http://cis1.towson.edu/~cyber4all/modules/nanomodules/Integer_Error-CS2_C++.html'},{'title':'See a Security Injection Module in Action','url':'https://youtu.be/MH_RD1jh0AE'},{'title':'How to use this module in your class','url':'https://youtu.be/u47q-qX52JI'}],'notes':'','pdf':{'name':'0ReadMeFirst - Integer Error - CS2 - C++.pdf','url':'https://neutrino-file-uploads.s3.us-east-2.amazonaws.com/skaza/5aa0183becba9a264dcd806b/0ReadMeFirst%20-%20Integer%20Error%20-%20CS2%20-%20C%2B%2B.pdf'}},'levels':['undergraduate'],'contributors':['5c6d71c29554c723fba68b19'],'collection':'secinj','lock':{'date':{'$numberDouble':'1538061211867'},'restrictions':['download']},'status':'released','description':'Introduce the concept of integer overflow in first programming courses'},
-        {
-        _id: '5aa0183becba9a264dcd806b',
-        author: {
-          name: 'nick visalli',
-        },
-        date: '1520530818714',
-        goals: [
-          {
-            text: 'Introduce the concept of integer overflow in first programming courses'
-          }
-        ],
-        outcomes: [
-          '5aa0183becba9a264dcd806c'
-        ],
-        published: true,
-        name: 'Integer Error - CS2 - C++',
-        length: 'nanomodule',
-        materials:{
-          files:[],
-          urls:[
-            // tslint:disable-next-line:max-line-length
-            {title:'Online module','url':'http://cis1.towson.edu/~cyber4all/modules/nanomodules/Integer_Error-CS2_C++.html'},{'title':'See a Security Injection Module in Action','url':'https://youtu.be/MH_RD1jh0AE'},{'title':'How to use this module in your class','url':'https://youtu.be/u47q-qX52JI'}],'notes':'','pdf':{'name':'0ReadMeFirst - Integer Error - CS2 - C++.pdf','url':'https://neutrino-file-uploads.s3.us-east-2.amazonaws.com/skaza/5aa0183becba9a264dcd806b/0ReadMeFirst%20-%20Integer%20Error%20-%20CS2%20-%20C%2B%2B.pdf'}},'levels':['undergraduate'],'contributors':['5c6d71c29554c723fba68b19'],'collection':'secinj','lock':{'date':{'$numberDouble':'1538061211867'},'restrictions':['download']},'status':'released','description':'Introduce the concept of integer overflow in first programming courses'},
-      ];
+  ngOnInit() {}
+
+  getLearningObjects(text: string) {
+    console.log(text);
+    const query = {
+      text
+    };
+    this.learningObjectService.getLearningObjects(query)
+      .then(val => {
+        this.learningObjects = val.learningObjects;
+      });
   }
  }
