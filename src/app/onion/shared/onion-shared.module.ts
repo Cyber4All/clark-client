074e99617f32f50020c31d43fc9892fc8a1443f4
Sean Donnelly <sdonnelly3000@gmail.com>
2019-03-01 12:13:47 -0500

    :bug: Fix denied vs rejected mismatch

--- src/app/onion/shared/onion-shared.module.ts
+++ src/app/onion/shared/onion-shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LearningObjectStatusIndicatorComponent } from './status-indicator.component';
 import { SharedModule } from 'app/shared/shared.module';
+import { LearningObjectStatusIndicatorComponent } from './status-indicator/status-indicator.component';
 
 @NgModule({
   declarations: [
--- src/app/onion/shared/status-indicator/status-indicator.component.ts
+++ src/app/onion/shared/status-indicator/status-indicator.component.ts
@@ -14,44 +14,14 @@ import { Component, OnInit, Input } from '@angular/core';
       <span *ngIf="status === 'waiting'"><i class="fas fa-hourglass"></i></span>
       <span *ngIf="status === 'review'"><i class="fas fa-sync"></i></span>
       <span *ngIf="status === 'released'"><i class="fas fa-eye"></i></span>
-      <span *ngIf="status === 'denied'"><i class="fas fa-ban"></i></span>
+      <span *ngIf="status === 'rejected'"><i class="fas fa-ban"></i></span>
     </div>
   `,
-  styles: [`
-    .top__status {
-      width: 32px;
-      height: 32px;
-      color: rgba(255, 255, 255, 0.9);
-      font-size: 16px;
-      border-radius: 50%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background: rgb(189, 193, 195);
-      transition: all 0.2s ease;
-      box-shadow: none;
-      &.waiting {
-        background: #5ec9da;
-      }
-      &.review {
-        background: #f5a623;
-      }
-      &.released {
-        background: $green;
-      }
-      &.denied {
-        background: saturate($error-red, 15);
-      }
-      &:hover {
-        box-shadow: 0 0 20px 2px rgba(0, 0, 0, 0.08);
-      }
-    }
-  `]
+  styleUrls: ['status-indicator.component.scss']
 })
 export class LearningObjectStatusIndicatorComponent {
   @Input() status;
   @Input() states;
 
   constructor() { }
-
 }
