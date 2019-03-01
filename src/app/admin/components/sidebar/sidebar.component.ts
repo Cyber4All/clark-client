cda833e6a51f804dadd896639b071470fb79ceb3
Nick Winner <design@winnerdigital.net>
2019-03-01 12:53:09 -0500

    :lipstick: Finish and animate sidebar

--- src/app/admin/components/sidebar/sidebar.component.ts
+++ src/app/admin/components/sidebar/sidebar.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
-import { increaseElementDepthCount } from '@angular/core/src/render3/state';
+import { Collection } from 'app/core/collection.service';
+import { trigger, query, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'clark-admin-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  animations: [
+    trigger('sidebar', [
+      transition('* => *', [
+        query(':enter', [
+          style({ opacity: 0, height: 0 }),
+          animate('200ms ease', style({ opacity: 1, height: '*' }))
+        ], { optional: true }),
+        query(':leave', [
+          style({ opacity: 1, height: '*' }),
+          animate('200ms ease', style({ opacity: 0, height: 0 }))
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class SidebarComponent implements OnInit, OnDestroy {
   destroyed$: Subject<void> = new Subject();
 
-  @Input() authorizedCollections: string[] = [];
+  @Input() authorizedCollections: Collection[] = [];
   @Input() activeCollection: string;
   @Input() adminMode: boolean;
 
