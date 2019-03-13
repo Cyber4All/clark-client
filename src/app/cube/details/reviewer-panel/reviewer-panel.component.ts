409cd680773d3ecac3530c4b31a9c7a81d9e2fcd
Paige <pz0342@gmail.com>
2019-03-13 13:07:53 -0400

    Add side panel for downloading of revisions

---
+++ src/app/cube/details/reviewer-panel/reviewer-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'clark-reviewer-panel',
+  templateUrl: './reviewer-panel.component.html',
+  styleUrls: ['./reviewer-panel.component.scss']
+})
+export class ReviewerPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
