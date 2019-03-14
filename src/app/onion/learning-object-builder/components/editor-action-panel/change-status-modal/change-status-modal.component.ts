10e9634960073cf1aff7e4b20a10d51712f359a9
Nick Winner <design@winnerdigital.net>
2019-03-14 13:36:19 -0400

    :lipstick: Animate the change-sdtatus/edit-changelog transition to carousel

--- src/app/onion/learning-object-builder/components/editor-action-panel/change-status-modal/change-status-modal.component.ts
+++ src/app/onion/learning-object-builder/components/editor-action-panel/change-status-modal/change-status-modal.component.ts
@@ -1,11 +1,32 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output, ChangeDetectorRef} from '@angular/core';
 import {LearningObject} from '@cyber4all/clark-entity';
 import {BUILDER_ACTIONS, BuilderStore} from '../../../builder-store.service';
+import { trigger, transition, style, animate, state, query } from '@angular/animations';
 
 @Component({
   selector: 'clark-change-status-modal',
   templateUrl: './change-status-modal.component.html',
-  styleUrls: ['./change-status-modal.component.scss']
+  styleUrls: ['./change-status-modal.component.scss'],
+  animations: [
+    trigger('carousel', [
+      transition('void => next', [
+        style({ transform: 'translateX(600px)', opacity: 0 }),
+        animate('300ms 150ms ease', style({ transform: 'translateX(0) ', opacity: 1 }))
+      ]),
+      transition('next => void', [
+        style({ transform: 'translateX(0)', opacity: 1 }),
+        animate('300ms ease', style({ transform: 'translateX(-600px)', opacity: 0 }))
+      ]),
+      transition('void => prev', [
+        style({ transform: 'translateX(-600px)', opacity: 0 }),
+        animate('300ms 150ms ease', style({ transform: 'translateX(0)', opacity: 1 }))
+      ]),
+      transition('prev => void', [
+        style({ transform: 'translateX(0)', opacity: 1 }),
+        animate('300ms ease', style({ transform: 'translateX(600px)', opacity: 0 }))
+      ])
+    ]),
+  ]
 })
 export class ChangeStatusModalComponent {
   @Input() shouldShow;
@@ -20,19 +41,24 @@ export class ChangeStatusModalComponent {
     'waiting'
   ];
 
-  constructor(private builderStore: BuilderStore) { }
+  constructor(private builderStore: BuilderStore, private cd: ChangeDetectorRef) { }
 
   page: 1 | 2 = 1;
+  direction: 'prev' | 'next' = 'next';
 
   closeModal() {
     this.closed.next();
   }
 
   advance() {
+    this.direction = 'next';
+    this.cd.detectChanges();
     this.page = 2;
   }
 
   regress() {
+    this.direction = 'prev';
+    this.cd.detectChanges();
     this.page = 1;
   }
 
