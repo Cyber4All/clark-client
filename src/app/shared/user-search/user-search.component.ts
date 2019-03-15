5a41e1cba58d6fb91eb281ada2f90bb36c7e05c3
jbarlow96 <33383985+jbarlow96@users.noreply.github.com>
2019-03-15 15:01:06 -0400

    add new component for user-search under shared

---
+++ src/app/shared/user-search/user-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'clark-users',
+    templateUrl: './user-search.component.html',
+    styleUrls: ['./user-search.component.scss']
+  })
+  export class UserSearchComponent {
+
+  }
