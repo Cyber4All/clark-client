11ad3038673b1b3488b4707ff5b595c467c9e7d2
Nick Visalli <nvisal1@students.towson.edu>
2019-03-15 21:01:00 -0400

    use ngx-virtual-scroll in learning object list

--- src/app/admin/components/user-search-wrapper/user-search-wrapper.component.ts
+++ src/app/admin/components/user-search-wrapper/user-search-wrapper.component.ts
@@ -70,7 +70,6 @@ export class UserSearchWrapperComponent implements OnInit, OnDestroy {
             results.splice(i, 1);
           }
         }
-
         this.searchResults = results;
         this.loading = false;
       });
