24638f131979f862c8b3e0566cc6f1500d48bf97
Nick Winner <design@winnerdigital.net>
2019-03-18 10:13:10 -0400

    :ok_hand: Move changelog animations to a separate file and change min-width to width in change-status-modal to fully encompass the ckeditor frame

--- src/app/onion/learning-object-builder/components/editor-action-panel/change-status-modal/change-status-modal.component.ts
+++ src/app/onion/learning-object-builder/components/editor-action-panel/change-status-modal/change-status-modal.component.ts
@@ -7,50 +7,14 @@ import {
 } from '@angular/core';
 import { LearningObject } from '@cyber4all/clark-entity';
 import { BUILDER_ACTIONS, BuilderStore } from '../../../builder-store.service';
-import {
-  trigger,
-  transition,
-  style,
-  animate,
-} from '@angular/animations';
 import { ChangelogService } from 'app/core/changelog.service';
+import { carousel } from './clark-change-status-modal.animations';
 
 @Component({
   selector: 'clark-change-status-modal',
   templateUrl: './change-status-modal.component.html',
   styleUrls: ['./change-status-modal.component.scss'],
-  animations: [
-    trigger('carousel', [
-      transition('void => next', [
-        style({ transform: 'translateX(600px)', opacity: 0 }),
-        animate(
-          '300ms 150ms ease',
-          style({ transform: 'translateX(0) ', opacity: 1 })
-        )
-      ]),
-      transition('next => void', [
-        style({ transform: 'translateX(0)', opacity: 1 }),
-        animate(
-          '300ms ease',
-          style({ transform: 'translateX(-600px)', opacity: 0 })
-        )
-      ]),
-      transition('void => prev', [
-        style({ transform: 'translateX(-600px)', opacity: 0 }),
-        animate(
-          '300ms 150ms ease',
-          style({ transform: 'translateX(0)', opacity: 1 })
-        )
-      ]),
-      transition('prev => void', [
-        style({ transform: 'translateX(0)', opacity: 1 }),
-        animate(
-          '300ms ease',
-          style({ transform: 'translateX(600px)', opacity: 0 })
-        )
-      ])
-    ])
-  ]
+  animations: [ carousel ]
 })
 export class ChangeStatusModalComponent {
   @Input() shouldShow;
