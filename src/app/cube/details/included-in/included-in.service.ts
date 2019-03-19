5493b611d09ad943e3e6e19405d77c1d9c9e00af
Sean Donnelly <sdonnelly3000@gmail.com>
2019-03-19 17:20:04 -0400

    :hankey: Filter unrelease parents

--- src/app/cube/details/included-in/included-in.service.ts
+++ src/app/cube/details/included-in/included-in.service.ts
@@ -1,16 +1,24 @@
 
-import {take} from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PUBLIC_LEARNING_OBJECT_ROUTES } from '@env/route';
+import { LearningObject } from '@cyber4all/clark-entity';
 
 @Injectable()
 export class IncludedInService {
   constructor(private httpClient: HttpClient) { }
 
   fetchParents(id: string) {
-    return this.httpClient.get(PUBLIC_LEARNING_OBJECT_ROUTES.GET_LEARNING_OBJECT_PARENTS(id)).pipe(
-      take(1))
-      .toPromise();
+    return this.httpClient.get<LearningObject[]>(PUBLIC_LEARNING_OBJECT_ROUTES.GET_LEARNING_OBJECT_PARENTS(id))
+    .pipe(
+      take(1),
+      /* TODO: Remove this. 
+       * It is a stopcap until the service stops returning unreleased.
+       * More info at https://github.com/Cyber4All/learning-object-service/pull/282
+       */
+      map(parents => parents.filter(parent => parent.status !== 'unreleased')),
+    )
+    .toPromise();
   }
 }
