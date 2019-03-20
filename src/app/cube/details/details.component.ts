d2f36a1c9b929481ea5a10a248dc01a9a7cf79dc
Paige <pz0342@gmail.com>
2019-03-20 14:07:28 -0400

    Add logic to load working copy

--- src/app/cube/details/details.component.ts
+++ src/app/cube/details/details.component.ts
@@ -107,6 +107,16 @@ export class DetailsComponent implements OnInit, OnDestroy {
     // this.hasRevisions = this.learningObject.hasRevisions;
     // this.reviewer = this.auth.hasReviewerAccess();
 
+    if (this.hasRevisions) {
+      this.route.params.pipe(takeUntil(this.isDestroyed$)).subscribe(params => {
+        const learningObjectName = decodeURIComponent(params['learningObjectName']);
+        // will get replaced with function to load revisions
+        this.fetchLearningObject(
+          params['username'],
+          learningObjectName,
+        );
+      });
+    }
   }
 
   viewReleased(released: boolean) {
