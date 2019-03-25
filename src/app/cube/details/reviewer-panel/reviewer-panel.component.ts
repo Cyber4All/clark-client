2fc1942521ba1a2afc1c38b9a102a674256405c2
Paige <pz0342@gmail.com>
2019-03-25 08:17:00 -0400

    Remove download functionality from reviewer panel

--- src/app/cube/details/reviewer-panel/reviewer-panel.component.ts
+++ src/app/cube/details/reviewer-panel/reviewer-panel.component.ts
@@ -1,12 +1,5 @@
-import { Component, ElementRef, HostListener, Input, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { LearningObject, User } from '@cyber4all/clark-entity';
-import { AuthService, DOWNLOAD_STATUS } from '../../../core/auth.service';
-import { environment } from '@env/environment';
-import { TOOLTIP_TEXT } from '@env/tooltip-text';
-import { Subject } from 'rxjs';
-import { CartV2Service, iframeParentID } from '../../../core/cartv2.service';
-import { ToasterService } from '../../../shared/toaster/toaster.service';
-import { takeUntil } from 'rxjs/operators';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { LearningObject } from '@cyber4all/clark-entity';
 
 @Component({
   selector: 'clark-reviewer-panel',
@@ -14,97 +7,14 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./reviewer-panel.component.scss']
 })
 export class ReviewerPanelComponent implements OnInit {
-  @Input() learningObject: LearningObject;
-  @ViewChild('objectLinkElement') objectLinkElement: ElementRef;
-  @ViewChild('ratingsWrapper') ratingsWrapper: ElementRef;
-  @ViewChild('savesRef') savesRef: ElementRef;
-
-  private destroyed$ = new Subject<void>();
-  hasDownloadAccess = false;
-  downloading = false;
-  author: string;
-  learningObjectName: string;
-  saved = false;
-  url: string;
-  showAddRating = false;
-  windowWidth: number;
-  showDownloadModal = false;
-  isEditButtonViewable = false;
-
-
-  contributorsList = [];
-  iframeParent = iframeParentID;
-  error = false;
-  userIsAuthor = false;
-
-  public tips = TOOLTIP_TEXT;
-
-  @HostListener('window:keyup', ['$event']) handleKeyUp(event: KeyboardEvent) {
-    if (event.keyCode === 27) {
-      this.showAddRating = false;
-    }
-  }
-
-  @HostListener('window:resize', ['$event']) handleResize(event) {
-    this.windowWidth = event.target.outerWidth;
-  }
-  constructor(
-    public auth: AuthService,
-    private cartService: CartV2Service,
-    private renderer: Renderer2,
-    private noteService: ToasterService,
-  ) { }
+  @Input() releasedDate;
+  @Input() revisedDate;
+  @Output() download: EventEmitter<boolean> = new EventEmitter();
+  constructor() { }
 
   ngOnInit() {
-    this.auth.group
-      .pipe(takeUntil(this.destroyed$))
-      .subscribe(() => {
-        this.isEditButtonViewable = this.auth.hasEditorAccess();
-      });
-    this.hasDownloadAccess = this.auth.hasReviewerAccess();
-    this.url = this.buildLocation();
-    this.saved = this.cartService.has(this.learningObject);
-    const userName = this.auth.username;
-    this.userIsAuthor = (this.learningObject.author.username === userName);
   }
-
-  async addToCart(download?: boolean) {
-    this.error = false;
-    this.downloading = true;
-
-    if (download) {
-      this.download(
-        this.learningObject.author.username,
-        this.learningObject.name
-      );
-    }
-  }
-
-  download(author: string, learningObjectName: string) {
-    this.downloading = true;
-    const loaded = this.cartService
-      .downloadLearningObject(author, learningObjectName).pipe(
-      takeUntil(this.destroyed$));
-
-    this.toggleDownloadModal(true);
-
-    loaded.subscribe(finished => {
-      if (finished) {
-        this.downloading = false;
-      }
-    });
-  }
-
-  private buildLocation(encoded?: boolean) {
-    const u = window.location.protocol + '//' + window.location.host +
-      '/details' +
-      '/' + encodeURIComponent(this.learningObject.author.username) +
-      '/' + encodeURIComponent(this.learningObject.name);
-
-    return encoded ? encodeURIComponent(u) : u;
-  }
-
-  toggleDownloadModal(val?: boolean) {
-    this.showDownloadModal = val;
+  click(download: boolean) {
+    this.download.emit(download);
   }
 }
