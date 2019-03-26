2903fa09792cc2e135c7ddeed7168a0de51b613a
Paige <pz0342@gmail.com>
2019-03-26 13:32:07 -0400

    Remove unnecessary import

--- src/app/cube/details/reviewer-panel/reviewer-panel.component.ts
+++ src/app/cube/details/reviewer-panel/reviewer-panel.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { LearningObject } from '@cyber4all/clark-entity';
 
 @Component({
   selector: 'clark-reviewer-panel',
