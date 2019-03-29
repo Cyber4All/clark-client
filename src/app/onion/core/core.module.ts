8e2571893c9b352f66c6a19585315628aa6db17f
EVogel1999 <eav990@gmail.com>
2019-03-29 13:27:05 -0400

    Remove instances of the builder guard

--- src/app/onion/core/core.module.ts
+++ src/app/onion/core/core.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { LearningObjectService } from './learning-object.service';
-import { LearningObjectBuilderGuard } from './learning-object-builder.guard';
 
 /**
  * All singleton services that are global to the onion feature module should be provided here.
@@ -9,8 +8,7 @@ import { LearningObjectBuilderGuard } from './learning-object-builder.guard';
   imports: [],
   exports: [],
   providers: [
-    LearningObjectService,
-    LearningObjectBuilderGuard
+    LearningObjectService
   ]
 })
 export class OnionCoreModule { }
