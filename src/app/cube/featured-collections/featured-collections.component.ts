386b8dd29c3d2597d1edf43bfff94b7a3164abde
EVogel1999 <eav990@gmail.com>
2019-04-01 15:43:59 -0400

    Pull collection data from api

--- src/app/cube/featured-collections/featured-collections.component.ts
+++ src/app/cube/featured-collections/featured-collections.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CollectionService } from 'app/core/collection.service';
 
 @Component({
   selector: 'clark-featured-collections',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturedCollectionsComponent implements OnInit {
 
-  constructor() { }
+  collections;
+
+  constructor(private collectionService: CollectionService) { }
 
   ngOnInit() {
+    this.collectionService
+      .getCollections()
+      .then(collections => {
+        this.collections = collections;
+      })
+      .catch(e => {
+        console.error(e.message);
+      });
   }
 
 }
