2c0986815b494a617fd4e7e5e536db1652fd74ee
Gustavus Shaw II <gshaw1997@gmail.com>
2019-04-03 17:29:30 -0400

    :recycle: Update URL to file upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiURL: 'https://api-gateway.clark.center',
   STATE_STORAGE_LOCATION: 'state',
   suggestionUrl: 'https://api-outcome-suggestion.clark.center',
-  contentManagerURL: 'https://api-learning-objects.clark.center',
+  contentManagerURL: 'https://api-file-upload.clark.center',
   adminAppUrl: 'https://admin.clark.center'
 };
 
