fb64cfa05d240e3a53a938585fb74ccc558e8553
jbarlow96 <33383985+jbarlow96@users.noreply.github.com>
2019-04-04 17:22:47 -0400

    :ok_hand: add JSDocs to functions

--- src/app/admin/pages/users/users.component.ts
+++ src/app/admin/pages/users/users.component.ts
@@ -31,6 +31,10 @@ export class UsersComponent implements OnInit {
    });
   }
 
+/**
+ * Text search that returns the list of users to add
+ * @param text text string to query by
+ */
   getUsers(text: string) {
     this.loading = true;
     this.user.searchUsers(text)
@@ -40,22 +44,36 @@ export class UsersComponent implements OnInit {
       });
   }
 
+  /**
+   * Returns the current reviewer members of the specified collection
+   */
   async fetchReviewers() {
     this.loading = true;
     this.users = await this.user.fetchReviewers(this.activeCollection, {role: 'reviewer'});
     this.loading = false;
   }
 
+  /**
+   * Adds a user as a reviewer to a collection after being clicked on in the user search
+   * @param user the elements of a clark user such as id and role
+   */
   async addReviewer(user: User) {
     await this.user.assignMember(user.id, this.activeCollection, {role: 'reviewer'});
     await this.fetchReviewers();
   }
 
+/**
+ * Opens up a modal of a selected reviewer in a collection which gives the choice to remove their reviewer access
+ * @param reviewerId the id of the of reviewer which allows us to remove their access
+ */
   showModal(reviewerId: string) {
     this.removeReviewerId = reviewerId;
     this.displayRemoveReviewerModal = true;
   }
 
+  /**
+   * Removes the reviewer access from a user in a collection
+   */
   async removeReviewer() {
     this.displayRemoveReviewerModal = false;
     await this.user.removeMember(this.activeCollection, this.removeReviewerId, {role: 'reviewer'} );
