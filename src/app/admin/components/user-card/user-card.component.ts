cc44b3bb9ff5302af7a89ad918ee5cef5194f31e
Nick Winner <design@winnerdigital.net>
2019-04-19 13:50:48 -0400

    :lipstick: Animate the user-card menus in the collection dashboard

--- src/app/admin/components/user-card/user-card.component.ts
+++ src/app/admin/components/user-card/user-card.component.ts
@@ -1,8 +1,7 @@
 import { Component, Input, EventEmitter, Output, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { User } from '@entity';
 import { UserService } from 'app/core/user.service';
-import { trigger, transition, style, animate } from '@angular/animations';
-import { setClassMetadata } from '@angular/core/src/render3';
+import { trigger, transition, style, animate, query, animateChild, stagger } from '@angular/animations';
 
 @Component({
   selector: 'clark-admin-user-card',
@@ -12,13 +11,22 @@ import { setClassMetadata } from '@angular/core/src/render3';
   animations: [
     trigger('menu', [
       transition(':enter', [
+        query('*', [
+          style({ opacity: 0 })
+        ]),
         style({ opacity: 0 }),
-        animate('200ms ease', style({ opacity: 1 }))
+        animate('200ms ease', style({ opacity: 1 })),
+        query('*', [
+          stagger(35, [
+            style({ opacity: 0, transform: 'translateY(15px)' }),
+            animate('200ms ease', style({ opacity: 1, transform: 'translateY(0px)' }))
+          ])
+        ])
       ]),
       transition(':leave', [
         style({ opacity: 1 }),
         animate('200ms ease', style({ opacity: 0 }))
-      ])
+      ]),
     ]),
   ]
 })
