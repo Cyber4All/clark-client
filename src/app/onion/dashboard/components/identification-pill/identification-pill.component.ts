3fd32e8e2a5461b54adaeffbb41cfb8ed941c15a
Nick Visalli <nvisal1@students.towson.edu>
2019-04-23 11:15:14 -0400

    Add css grid to change log items

--- src/app/onion/dashboard/components/identification-pill/identification-pill.component.ts
+++ src/app/onion/dashboard/components/identification-pill/identification-pill.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class IdentificationPillComponent implements OnInit {
   @Input() accessGroup: string;
+  @Input() theme = 'default';
 
   constructor() { }
 
