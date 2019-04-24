5a77e0dcd73fcbb7333b8975cead30bd5f3d31a7
Gustavus Shaw II <gshaw1997@gmail.com>
2019-04-24 15:31:53 -0400

    :wrench: Configure global in polyfills for aws-sdk

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -69,3 +69,8 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Need to import at least one locale-data with intl.
  */
 // import 'intl/locale-data/jsonp/en';
+
+/**
+ * Set Global
+ */
+(window as any).global = window;
