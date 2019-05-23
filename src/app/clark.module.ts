81afde370342e3bc6ca8783c2bd509ee23404843
Nick Winner <design@winnerdigital.net>
2019-05-23 09:58:49 -0400

    :fire: Remove broken imports from Clark Module

--- src/app/clark.module.ts
+++ src/app/clark.module.ts
@@ -11,7 +11,6 @@ import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UnsupportedComponent } from './unsupported.component';
 import { NotFoundComponent } from './not-found.component';
-import { GenericCollectionLogoComponent } from './generic-collection-logo/generic-collection-logo.component';
 
 @NgModule({
   imports: [
@@ -21,7 +20,7 @@ import { GenericCollectionLogoComponent } from './generic-collection-logo/generi
     CoreModule.forRoot(),
     BrowserAnimationsModule
   ],
-  declarations: [ClarkComponent, UnsupportedComponent, NotFoundComponent, GenericCollectionLogoComponent],
+  declarations: [ClarkComponent, UnsupportedComponent, NotFoundComponent],
   bootstrap: [ClarkComponent],
   providers: [{ provide: UrlSerializer, useClass: CustomUrlSerializer }]
 })
