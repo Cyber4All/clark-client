c8801a172a0fd4eb93ccec02ab43f5d4251c0d84
Nick Winner <design@winnerdigital.net>
2019-06-12 13:41:46 -0400

    :speech_balloon: Document the getDescription function and it's needsCollection variable

--- src/environments/status-descriptions.ts
+++ src/environments/status-descriptions.ts
@@ -15,13 +15,16 @@ export class StatusDescriptions {
       return 'This Learning Object was rejected. Contact your review team for further information.';
     },
     waiting(collection: string) {
-      return `This Learning Object is waiting to be reviewed by the ${collection} review team. It is not yet released and cannot be edited until the review process is complete.`;
+      return `This Learning Object is waiting to be reviewed by the ${collection} review team.
+       It is not yet released and cannot be edited until the review process is complete.`;
     },
     review(collection: string) {
-      return `This Learning Object is currently under review by the ${collection} review team. It is not yet released and cannot be edited until the review process is complete.`
+      return `This Learning Object is currently under review by the ${collection} review team.
+       It is not yet released and cannot be edited until the review process is complete.`;
     },
     proofing() {
-      return 'This Learning Object is currently undergoing proofing by the CLARK editorial team. It is not yet released and cannot be edited until the proofing process is complete.'
+      return `This Learning Object is currently undergoing proofing by the CLARK editorial team.
+       It is not yet released and cannot be edited until the proofing process is complete.`;
     },
     released(collection: string) {
       return `This Learning Object is released in the ${collection} collection and can be browsed for.`;
@@ -30,9 +33,20 @@ export class StatusDescriptions {
 
   constructor(private collectionService: CollectionService) { }
 
-  async getDescription(status: LearningObject.Status, collectionAbbreviation?: string) {
-    const needsCollection = [LearningObject.Status.RELEASED, LearningObject.Status.WAITING, LearningObject.Status.REVIEW];
+  /**
+   * Returns the appropriate status description for a given status and
+   * fetches the appropriate collection from the abbreviated collection if needed
+   *
+   * @param {LearningObject.Status} status
+   * @param {string} [collectionAbbreviation] the abbreviated name of the collection relevant to the supplied status
+   * @returns {Promise<string>}
+   * @memberof StatusDescriptions
+   */
+  async getDescription(status: LearningObject.Status, collectionAbbreviation?: string): Promise<string> {
     let collection: string;
+
+    // the list of statuses that require loading the full collection
+    const needsCollection = [LearningObject.Status.RELEASED, LearningObject.Status.WAITING, LearningObject.Status.REVIEW];
 
     if (needsCollection.includes(status)) {
       collection = (await this.collectionService.getCollection(collectionAbbreviation)).name;
