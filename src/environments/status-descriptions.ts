31af842f8615c4be18ade01e1085c49405059993
Nick Winner <design@winnerdigital.net>
2019-06-12 11:50:07 -0400

    :speech_balloon: Update the Learning Object status descriptions to capitalize 'Learning Object'

--- src/environments/status-descriptions.ts
+++ src/environments/status-descriptions.ts
@@ -9,22 +9,22 @@ export class StatusDescriptions {
 
   private templates = {
     unreleased() {
-      return 'This learning object is visible only to you. Submit it for review to make it publicly available.';
+      return 'This Learning Object is visible only to you. Submit it for review to make it publicly available.';
     },
     rejected() {
-      return 'This learning object was rejected. Contact your review team for further information.';
+      return 'This Learning Object was rejected. Contact your review team for further information.';
     },
     waiting(collection: string) {
-      return `This learning object is waiting to be reviewed by the ${collection} review team. It is not yet released and cannot be edited until the review process is complete.`;
+      return `This Learning Object is waiting to be reviewed by the ${collection} review team. It is not yet released and cannot be edited until the review process is complete.`;
     },
     review(collection: string) {
-      return `This learning object is currently under review by the ${collection} review team. It is not yet released and cannot be edited until the review process is complete.`
+      return `This Learning Object is currently under review by the ${collection} review team. It is not yet released and cannot be edited until the review process is complete.`
     },
     proofing() {
-      return 'This learning object is currently undergoing proofing by the CLARK editorial team. It is not yet released and cannot be edited until the proofing process is complete.'
+      return 'This Learning Object is currently undergoing proofing by the CLARK editorial team. It is not yet released and cannot be edited until the proofing process is complete.'
     },
     released(collection: string) {
-      return `This learning object is released in the ${collection} collection and can be browsed for.`;
+      return `This Learning Object is released in the ${collection} collection and can be browsed for.`;
     }
   };
 
