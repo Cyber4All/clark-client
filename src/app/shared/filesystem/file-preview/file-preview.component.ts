f2f6d4ad95385af710b06645f9cb574f80202628
jbarlow96 <33383985+jbarlow96@users.noreply.github.com>
2019-07-08 13:33:02 -0400

    add login check to display the appropriate message

--- src/app/shared/filesystem/file-preview/file-preview.component.ts
+++ src/app/shared/filesystem/file-preview/file-preview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'app/core/auth.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'clark-file-preview',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-preview.component.scss']
 })
 export class FilePreviewComponent implements OnInit {
+  private isDestroyed$ = new Subject<void>();
+  loggedin: boolean;
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
+    this.auth.isLoggedIn.pipe(takeUntil(this.isDestroyed$)).subscribe(val => {
+      this.loggedin = val;
+    });
   }
 
 }
