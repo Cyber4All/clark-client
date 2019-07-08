67e69b12bf9bb3a5dbe0a8d7e6a4db201ee77b14
Nick Winner <design@winnerdigital.net>
2019-07-08 12:45:21 -0400

    :fire: Remove accordion feature from dashboard-list component

--- src/app/onion/dashboard/components/list/list.component.ts
+++ src/app/onion/dashboard/components/list/list.component.ts
@@ -1,18 +1,10 @@
 import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { LearningObject } from '@entity';
-import { trigger, transition, state, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'clark-dashboard-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
-  animations: [
-    trigger('accordion', [
-      state('open', style({ height: '*', overflow: 'visible' })),
-      state('closed', style({ height: '70px', overflow: 'hidden' })),
-      transition('* => *', animate('{{ accordionSpeed }}ms ease'), { params: { accordionSpeed: 200 } })
-    ])
-  ]
 })
 export class ListComponent {
   @ViewChild('tableItems') tableItemsElement: ElementRef;
@@ -20,15 +12,5 @@ export class ListComponent {
   @Input() showOptions: boolean;
   @Input() learningObjects: LearningObject[];
   @Input() title: string;
-
-  state: 'open' | 'closed' = 'closed';
-
-  toggle() {
-    this.state = this.state === 'open' ? 'closed' : 'open';
-  }
-
-  get accordionSpeed() {
-    return Math.min(Math.max(this.tableItemsElement.nativeElement.offsetHeight, 250), 600);
-  }
 
 }
