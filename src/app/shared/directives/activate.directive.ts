00957a6e72c5f1cd01ef4d70e8694061bb146e98
Nick Winner <design@winnerdigital.net>
2019-07-10 11:54:28 -0400

    :sparkles: Handle routerLinks in activate directive

--- src/app/shared/directives/activate.directive.ts
+++ src/app/shared/directives/activate.directive.ts
@@ -20,6 +20,12 @@ export class ActivateDirective {
       if (event.code !== 'Enter' && event.code !== 'Space') {
         return;
       }
+
+      if (this.el.nativeElement.hasAttribute('ng-reflect-router-link')) {
+        // trigger a synthetic click event that will not bubble if the element has a routerLink directive
+        const syntheticEvent = new Event('click', { bubbles: false });
+        this.el.nativeElement.dispatchEvent(syntheticEvent);
+      }
     }
 
     this.activate.emit(event);
