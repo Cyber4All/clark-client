69e5c5245439e9f6a415c3ca4f02d5d76212899e
jbarlow96 <33383985+jbarlow96@users.noreply.github.com>
2019-07-10 16:14:01 -0400

    :white_check_mark: update tests for file-preview responses

--- src/app/shared/filesystem/file-preview/file-preview.component.spec.ts
+++ src/app/shared/filesystem/file-preview/file-preview.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FilePreviewComponent } from './file-preview.component';
+import { AuthService } from 'app/core/auth.service';
+import { Component } from '@angular/core';
+import { FileBrowserUtilityService } from '../file-functions';
+import { LearningObject } from '@entity';
+import { BehaviorSubject } from 'rxjs';
 
 describe('FilePreviewComponent', () => {
   let component: FilePreviewComponent;
   let fixture: ComponentFixture<FilePreviewComponent>;
-  
+  const AuthServiceStub = {
+    isLoggedIn: new BehaviorSubject(false),
+  };
+  const FileBrowserUtilityServiceStub = {
+    getPreviewUrl(file: LearningObject.Material.File): string {
+      return '';
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      providers: [
+        { provide: AuthService, useValue: AuthServiceStub },
+        { provide: FileBrowserUtilityService, useValue: FileBrowserUtilityServiceStub}
+      ],
       declarations: [ FilePreviewComponent ]
     })
     .compileComponents();
@@ -22,5 +38,44 @@ describe('FilePreviewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('hasPreviewLink', () => {
+    it('should have a preview link when the previewUrl is not empty', () => {
+      component.previewUrl = 'https://clark.center';
+      expect(component.hasPreviewLink).toBeTruthy();
+    });
+
+    it('should not have a preview link when the previewUrl is empty', () => {
+      expect(component.hasPreviewLink).toBeFalsy();
+    });
+    describe('when the user is logged in', () => {
+      describe('and it has an empty previewUrl', () => {
+        it('should return the noPreview response', () => {
+          expect(component.copy).toBe(
+            'Preview is not available for this type of file. Click the \"Download Now\" button located above to download this object.'
+            );
+        });
+      });
+    });
+
+    describe('when the user is not logged in', () => {
+      describe('and it has an empty previewUrl', () => {
+        it('should return the noPreview response', () => {
+          expect(component.copy).toBe(
+            'Preview is not available for this type of file. Click the \"Download Now\" button located above to download this object.'
+            );
+        });
+      });
+
+      describe('and it has a non-empty previewUrl', () => {
+        it('should return the notLoggedIn response', () => {
+          component.previewUrl = 'https://clark.center';
+          expect(component.copy).toBe(
+            'Please log in to preview file. Click the \"Download Now\" button located above to download this object.'
+            );
+        });
+      });
+    });
   });
 });
