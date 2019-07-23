f60def8474382d5ccf9dee93c284de485c4810cb
Paige Zaleppa <pz0342@gmail.com>
2019-07-23 13:20:34 -0400

    :construction: componetize

---
+++ src/app/onion/old-dashboard/components/side-panel-content/side-panel-content.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MetricsComponent } from './metrics/metrics.component';
+
+@NgModule({
+  declarations: [
+    MetricsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SidePanelContentModule { }
