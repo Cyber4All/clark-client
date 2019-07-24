b13b421b3296b95d410a497284d28a9c93a5930f
Nick Winner <design@winnerdigital.net>
2019-07-24 14:08:40 -0400

    :white_check_mark: Update tests to reflect refactor of TipDirective

--- src/app/onion/dashboard/components/side-panel-content/side-panel-content.component.spec.ts
+++ src/app/onion/dashboard/components/side-panel-content/side-panel-content.component.spec.ts
@@ -3,7 +3,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SidePanelContentComponent } from './side-panel-content.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TooltipModule } from 'app/shared/tooltips/tip.module';
 import { RatingService } from 'app/core/rating.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from 'app/core/auth.service';
@@ -19,7 +18,7 @@ describe('SidePanelContentComponent', () => {
     TestBed.configureTestingModule({
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [ SidePanelContentComponent ],
-      imports: [ RouterTestingModule, TooltipModule, HttpClientModule, CookieModule.forRoot(), NoopAnimationsModule ],
+      imports: [ RouterTestingModule, HttpClientModule, CookieModule.forRoot(), NoopAnimationsModule ],
       providers: [
         RatingService,
         AuthService
