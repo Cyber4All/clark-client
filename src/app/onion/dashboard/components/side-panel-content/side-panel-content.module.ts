0e5031616d8fafa835fef864c7e32c6dae1fd279
Paige Zaleppa <pz0342@gmail.com>
2019-07-24 09:28:20 -0400

    Move module to new dashboard

--- src/app/onion/dashboard/components/side-panel-content/side-panel-content.module.ts
+++ src/app/onion/dashboard/components/side-panel-content/side-panel-content.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidePanelContentComponent } from './side-panel-content.component';
+// import { SidePanelContentComponent } from './side-panel-content.component';
 import { MetricsComponent } from './metrics/metrics.component';
 import { RevisionComponent } from './revision/revision.component';
 import { LearningObjectComponent } from './learning-object/learning-object.component';
 import { OnionRoutingModule } from '../../../onion.routing';
 import { SharedModule } from '../../../../shared/shared.module';
-
+import { SidePanelContentComponent } from './side-panel-content.component';
 @NgModule({
   declarations: [
     SidePanelContentComponent,
--- src/app/onion/onion.module.ts
+++ src/app/onion/onion.module.ts
@@ -18,7 +18,7 @@ import { OnionSharedModule } from './shared/onion-shared.module';
 import { SidePanelModule } from './old-dashboard/components/side-panel/side-panel.module';
 import { DashboardItemComponent } from './old-dashboard/components/dashboard-item/dashboard-item.component';
 import { DashboardModule } from './dashboard/dashboard.module';
-import { SidePanelContentModule } from './old-dashboard/components/side-panel-content/side-panel-content.module';
+import { SidePanelContentModule } from './dashboard/components/side-panel-content/side-panel-content.module';
 /**
  * Defines the root module that is bootstrapped to start the application.
  * This tells Angular how to handle all of the files and dependencies in use.
