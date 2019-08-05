c3dd160348cc360a2240150ce791eabad56ca2f8
Paige Zaleppa <pz0342@gmail.com>
2019-08-05 14:33:57 -0400

    :fire: Remove spacing

--- src/environments/route.ts
+++ src/environments/route.ts
@@ -86,8 +86,8 @@ export const USER_ROUTES = {
   },
   GET_MY_DRAFT_LEARNING_OBJECTS(username, filters: any, query: string) {
     // Onion Dashboard
-    return `${environment.apiURL}/users/${encodeURIComponent(username)}/learning-objects?text=${encodeURIComponent(query)}
-    &${querystring.stringify(filters)}&draftsOnly=true`;
+    return `${environment.apiURL}/users/${encodeURIComponent(username)}/learning-objects?text=${encodeURIComponent(query
+      )}&${querystring.stringify(filters)}&draftsOnly=true`;
   },
   ADD_TO_MY_LEARNING_OBJECTS(username) {
     return `${environment.apiURL}/users/${encodeURIComponent(
