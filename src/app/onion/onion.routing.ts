da8d7e7b6ce43c0771c1f81b261c074fdc47a847
Paige Zaleppa <pz0342@gmail.com>
2019-08-06 08:55:51 -0400

    Remove environment from onion routing imports

--- src/app/onion/onion.routing.ts
+++ src/app/onion/onion.routing.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AuthGuard } from '../core/auth-guard.service';
 import { OnionComponent } from './onion.component';
-import { environment } from '@env/environment';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 
