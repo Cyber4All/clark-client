5560ef7790b62b0d8d196290a613f6d044ed5442
Nick Winner <design@winnerdigital.net>
2019-08-21 06:44:00 -0400

    Revert ":construction: Begin implementing details splash component"
    This reverts commit 0ffb3cc57e1e7f15f5e7d72ad1aaf69f65342c9f.

--- src/app/cube/details/details.module.ts
+++ src/app/cube/details/details.module.ts
@@ -2,22 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailsComponent } from './details.component';
 import { RouterModule } from '@angular/router';
-import { SplashComponent } from './components/splash/splash.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      { path: ':username/:learningObjectName', component: DetailsComponent, pathMatch: 'full' },
-      { path: '', redirectTo: '/home', pathMatch: 'full' }
-    ]),
+    RouterModule.forChild([{ path: 'details/:learningObjectAuthor/:learningObjectName', component: DetailsComponent }]),
   ],
   exports: [],
   declarations: [
-    // root level component
-    DetailsComponent,
-    // page components
-    SplashComponent
+    DetailsComponent
   ],
   providers: []
 })
