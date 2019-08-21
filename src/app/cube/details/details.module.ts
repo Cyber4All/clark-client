7d9455c3f64130f0db465e7acef4b6dac4dde874
Nick Winner <design@winnerdigital.net>
2019-08-21 10:58:29 -0400

    Merge branch 'design/details-page' into design/details-page-splash

--- src/app/cube/details/details.module.ts
+++ src/app/cube/details/details.module.ts
@@ -4,11 +4,13 @@ import { DetailsComponent } from './details.component';
 import { RouterModule } from '@angular/router';
 import { SplashComponent } from './components/splash/splash.component';
 import { LengthComponent } from './components/splash/length/length.component';
+import { SharedPipesModule } from 'app/shared/pipes/shared-pipes.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild([ { path: ':username/:learningObjectName', component: DetailsComponent} ]),
+    RouterModule.forChild([ { path: ':username/:learningObjectName', component: DetailsComponent } ]),
+    SharedPipesModule
   ],
   exports: [],
   declarations: [
