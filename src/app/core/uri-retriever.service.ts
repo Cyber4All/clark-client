355224921a96b1d0ec1ec84ebafb44292c795cb0
Paige Zaleppa <pz0342@gmail.com>
2019-09-05 10:20:13 -0400

    :sparkles: Adding functionality
    Co-authored-by: EVogel1999 <eav990@gmail.com>

--- src/app/core/uri-retriever.service.ts
+++ src/app/core/uri-retriever.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError  } from 'rxjs';
+import { LearningObject, StandardOutcome } from '@entity';
+import { PUBLIC_LEARNING_OBJECT_ROUTES } from '@env/route';
+import { catchError, retry } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,8 +11,25 @@ export class UriRetrieverService {
 
   constructor(private http: HttpClient) { }
 
-  getLearningObject(options) {
-    return options;
+  getLearningObject(author: string, learningObjectName: string, options?: string[]) {
+    this.getLearningObjectMeta(author, learningObjectName).then(val => {
+      console.log(val);
+      return options;
+    });
+  }
+  /**
+   * Retrieves the Learning Object Metadata
+   */
+  getLearningObjectMeta(author: string, learningObjectName: string): Promise<LearningObject> {
+    return this.http.get<LearningObject>(
+      PUBLIC_LEARNING_OBJECT_ROUTES.GET_PUBLIC_LEARNING_OBJECT(
+        author, learningObjectName
+        ))
+        .pipe(
+          retry(3),
+          catchError(this.handleError)
+        )
+        .toPromise();
   }
   /**
    * @param uri this is the uri that should be hit to get the objects outcomes
@@ -51,5 +70,15 @@ export class UriRetrieverService {
    */
   getLearningObjectRatings(uri: string): Observable<any> {
     return null;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Client-side or network returned error
+      return throwError(error.error.message);
+    } else {
+      // API returned error
+      return throwError(error);
+    }
   }
 }
