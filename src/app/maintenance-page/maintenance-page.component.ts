0d6247315ba7336af4bbec468abfe82c1284c5fc
Nick Winner <design@winnerdigital.net>
2019-09-16 12:14:58 -0400

    :lipstick: Design down page

--- src/app/maintenance-page/maintenance-page.component.ts
+++ src/app/maintenance-page/maintenance-page.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'clark-maintenance-page',
   templateUrl: './maintenance-page.component.html',
   styleUrls: ['./maintenance-page.component.scss']
 })
-export class MaintenancePageComponent implements OnInit {
+export class MaintenancePageComponent implements AfterViewInit {
+
+  cogs: any[];
+
+  @HostListener('window:resize') resizeHandler() {
+    this.countCogs()
+  }
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.countCogs();
+  }
+
+  countCogs() {
+    // each cog is 125 pixels wide
+    const windowWidth = window.innerWidth;
+    this.cogs = new Array(Math.ceil(windowWidth / 125) + 5);
   }
 
 }
