e51e66325763d41ee8a0d86ea14d94f112e3fa6d
Nick Winner <design@winnerdigital.net>
2019-09-19 14:53:12 -0400

    :lipstick: Style message bar with a blue warning style

--- src/app/core/messages.service.ts
+++ src/app/core/messages.service.ts
@@ -5,49 +5,51 @@ import { throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
 export class Message {
-    isUnderMaintenance: boolean;
-    message: string;
-
-    constructor(isUnderMaintenance: boolean, message: string) {
-        this.isUnderMaintenance = isUnderMaintenance;
-        this.message = message;
-    }
+  constructor(public isUnderMaintenance: boolean, public message: string, public iconClass?: string) { }
 }
 @Injectable()
 export class MessagesService {
-    private _message: Message;
+  private _message: Message;
 
-    get message() {
-        return this._message;
+  get message() {
+    return this._message;
+  }
+
+  constructor(private http: HttpClient) { }
+
+  getStatus(): Promise<Message> {
+    if (this._message) {
+      return Promise.resolve(this._message);
+    } else {
+      return this.http.get(MISC_ROUTES.CHECK_STATUS, { withCredentials: true })
+        .pipe(
+          retry(3),
+          catchError(this.handleError)
+        )
+        .toPromise()
+        .then((val: Message) => {
+
+          // TODO remove this
+          val = {
+            iconClass: 'far fa-exclamation-circle',
+            message: 'CLARK will be offline for scheduled maintenance Saturday, September 21st, from 10AM to 12PM.',
+            isUnderMaintenance: true
+          };
+
+          this._message = new Message(val.isUnderMaintenance, val.message, val.iconClass);
+          return this._message;
+        });
     }
+  }
 
-    constructor(private http: HttpClient) { }
-
-    getStatus(): Promise<Message> {
-        if (this._message) {
-            return Promise.resolve(this._message);
-        } else {
-            return this.http.get(MISC_ROUTES.CHECK_STATUS, { withCredentials: true })
-                .pipe(
-                    retry(3),
-                    catchError(this.handleError)
-                )
-                .toPromise()
-                .then((val: Message) => {
-                    this._message = new Message(val.isUnderMaintenance, val.message);
-                    return this._message;
-                });
-        }
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Client-side or network returned error
+      return throwError(error.error.message);
+    } else {
+      // API returned error
+      return throwError(error);
     }
-
-    private handleError(error: HttpErrorResponse) {
-        if (error.error instanceof ErrorEvent) {
-          // Client-side or network returned error
-          return throwError(error.error.message);
-        } else {
-          // API returned error
-          return throwError(error);
-        }
-      }
+  }
 }
 
