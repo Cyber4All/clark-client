5365100aed4b70df5beb6c514237dc790243a594
Nick Winner <design@winnerdigital.net>
2019-10-12 11:48:51 -0400

    :lipstick: Adjust spacing of side-panel card titles and box shadow in revision animation

--- src/app/onion/dashboard/components/side-panel-content/revision/revision.animations.ts
+++ src/app/onion/dashboard/components/side-panel-content/revision/revision.animations.ts
@@ -1,10 +1,10 @@
 import { trigger, transition, style, animate, query, stagger, animateChild, keyframes } from '@angular/animations';
 
 const backToFront = [
-  style({ backgroundColor: 'white', position: 'absolute', transform: 'translateY(0px) scale(0.9)', boxShadow: '0 2px 20px 1px rgba(0, 0, 0, 0)', left: 0, right: 0, zIndex: 1 }),
+  style({ backgroundColor: 'white', position: 'absolute', transform: 'translateY(0px) scale(0.9)', boxShadow: '0 2px 20px 1px rgba(0, 0, 0, 0.04)', left: 0, right: 0, zIndex: 1 }),
   animate('500ms 50ms ease', keyframes([
     style({ transform: 'translateY(50px) scale(0.95)', zIndex: 2, offset: 0.5 }),
-    style({ transform: 'translateY(0px) scale(1)', boxShadow: '0 2px 20px 1px rgba(0, 0, 0, 0.04)', zIndex: 2, offset: 1 })
+    style({ transform: 'translateY(0px) scale(1)', boxShadow: '0 1px 6px 1px rgba(0, 0, 0, 0.0)', zIndex: 2, offset: 1 })
   ])),
 ];
 
@@ -12,7 +12,7 @@ const frontToBack = [
   style({ backgroundColor: 'white', transform: 'translateY(0px) scale(1)', boxShadow: '0 2px 20px 1px rgba(0, 0, 0, 0.04)', position: 'absolute', left: 0, right: 0, zIndex: 2 }),
   animate('550ms ease', keyframes([
     style({ transform: 'translateY(-50px) scale(0.95)', zIndex: 1, offset: 0.5 }),
-    style({ transform: 'translateY(0px) scale(0.9)', boxShadow: '0 2px 12px 1px rgba(0, 0, 0, 0)', zIndex: 1, offset: 1 })
+    style({ transform: 'translateY(0px) scale(0.9)', boxShadow: '0 1px 6px 1px rgba(0, 0, 0, 0)', zIndex: 1, offset: 1 })
   ])),
 ];
 
