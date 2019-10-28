14d9b001c64a8b88dc65ba0043751172f8ab1f5d
Nick Winner <design@winnerdigital.net>
2019-10-28 10:55:41 -0400

    :wrench: Update environments to include a staging configuration

---
+++ src/environments/environment.stating.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  production: true,
+  experimental: false,
+  apiURL: 'https://api-gateway-dev.clark.center',
+  STATE_STORAGE_LOCATION: 'state',
+  suggestionUrl: 'https://api-guidelines-dev.clark.center',
+  contentManagerURL: 'localhost',
+  contentManagerURLAdmin: 'localhost',
+  adminAppUrl: 'https://admin.clark.center',
+  s3Bucket: 'clark-prod-file-uploads-backup',
+  s3BucketRegion: 'us-east-1',
+  cognitoRegion: 'us-east-1',
+  cognitoIdentityPoolId: 'us-east-1:1ad4e60a-9773-4a67-92b5-6cc2c7b3328f',
+  cognitoAdminIdentityPoolId: 'us-east-1:6691336e-11a1-48db-9774-5f5a2c8dc270'
+};
+
+export enum LearningObjectStatus {
+  PUBLISHED = 'published',
+  UNDER_REVIEW = 'review',
+  WAITING = 'waiting',
+  DENIED = 'denied',
+  UNPUBLISHED = 'unpublished'
+}
