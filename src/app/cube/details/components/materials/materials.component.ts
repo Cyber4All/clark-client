99293f14986dad9b931e1d4bad63a773d72b6793
Nick Visalli <nvisal1@students.towson.edu>
2019-12-20 11:43:19 -0500

    animate materials component for the new details page

--- src/app/cube/details/components/materials/materials.component.ts
+++ src/app/cube/details/components/materials/materials.component.ts
@@ -11,13 +11,33 @@ export class MaterialsComponent implements OnInit, OnChanges {
 
   @Input() materials: LearningObject.Material;
 
-  files$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
-  folderMeta$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+  previousSelection: string;
 
   currentSelection: 'Files' | 'URLs' | 'Notes' = 'Files';
 
-  setSelection(currentSelection: 'Files' | 'URLs' | 'Notes') {
-    this.currentSelection = currentSelection;
+  carouselPosition = {
+    Files: 0,
+    URLs: 1,
+    Notes: 2,
+  };
+
+  action$: BehaviorSubject<number> = new BehaviorSubject(0);
+
+  files$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+  folderMeta$: BehaviorSubject<any> = new BehaviorSubject<any>([]);
+
+
+  setSelection(newSelection: 'Files' | 'URLs' | 'Notes') {
+    this.previousSelection = this.currentSelection;
+    this.currentSelection = newSelection;
+    this.rotateCarousel();
+  }
+
+  rotateCarousel() {
+    const previousPosition = this.carouselPosition[this.previousSelection];
+    const currentPosition = this.carouselPosition[this.currentSelection];
+    const movementLength = currentPosition - previousPosition;
+    this.action$.next(movementLength);
   }
 
   emit(): void {
