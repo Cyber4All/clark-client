95be664854de6d9cc227119230ff119021f2db5c
Nick Visalli <nvisal1@students.towson.edu>
2020-01-06 08:58:16 -0500

    remove unused Storybook configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,3 @@
 import { configure } from '@storybook/angular';
-import '../src/globals.scss';
-import '../src/_vars.scss';
 
 configure(require.context('../src', true, /\.stories\.[tj]s$/), module);
