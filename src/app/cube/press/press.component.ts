199d9d2285ea4405b5f6ce479881af9fab4979bf
pzalep1 <pz0342@gmail.com>
2020-01-09 16:45:02 -0500

    :sparkles: Add json

--- src/app/cube/press/press.component.ts
+++ src/app/cube/press/press.component.ts
@@ -8,13 +8,12 @@ import { Mention } from '../../../entity/mention/mention';
 })
 export class PressComponent implements OnInit {
 
-  mentions: Mention = [];
+  mentions: Mention[];
 
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-    console.log('hi');
-    this.httpClient.get('assets/images/press/mentions.json', { responseType: 'json' }).toPromise().then((data: Mention) => {
+    this.httpClient.get('assets/images/press/mentions.json', { responseType: 'json' }).toPromise().then((data: Mention[]) => {
       this.mentions = data;
     });
   }
