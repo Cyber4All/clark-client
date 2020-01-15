7f859a785fa7b31a3c656ef6a925ca6d3e3955f1
pzalep1 <pz0342@gmail.com>
2020-01-15 08:27:48 -0500

    Make adjustments for deployment

--- src/app/cube/press/components/media-card/media-card.component.ts
+++ src/app/cube/press/components/media-card/media-card.component.ts
@@ -8,10 +8,19 @@ import { Mention } from 'app/core/press-coverage.service';
 })
 export class MediaCardComponent implements OnInit {
   @Input() mention: Mention;
+  imgSrc: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.imgSrc = '../../../../../assets/images/press/' + this.mention.icon;
+  }
+
+  /**
+   * Open the media link in a new tab
+   */
+  followLink() {
+    window.open(this.mention.link, '_blank');
   }
 
 }
