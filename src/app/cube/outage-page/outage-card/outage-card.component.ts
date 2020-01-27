f46e59c5d8d4182b524542065420f78a189d0aa7
Emily Vogel <eav990@gmail.com>
2020-01-27 16:25:31 -0500

    Generate outage card component

---
+++ src/app/cube/outage-page/outage-card/outage-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'clark-outage-card',
+  templateUrl: './outage-card.component.html',
+  styleUrls: ['./outage-card.component.scss']
+})
+export class OutageCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
