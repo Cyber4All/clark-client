4ef6a82c9762f06b208236ff19f506cd34c493d3
Emily Vogel <eav990@gmail.com>
2020-01-30 14:47:22 -0500

    Scaffold the past issue card

--- src/app/cube/outage-page/past-issue/past-issue.component.ts
+++ src/app/cube/outage-page/past-issue/past-issue.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { OutageReport } from '../types/outageReport';
 
 @Component({
   selector: 'clark-past-issue',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./past-issue.component.scss']
 })
 export class PastIssueComponent implements OnInit {
+
+  @Input() issue: OutageReport;
+  icon: string;
 
   constructor() { }
 
