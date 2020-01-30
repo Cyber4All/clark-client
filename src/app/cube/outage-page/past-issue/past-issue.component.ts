9da88c6d342542b994e7b1bbc8a659d5d7314ff3
Emily Vogel <eav990@gmail.com>
2020-01-30 14:37:41 -0500

    Generate past-issue card

---
+++ src/app/cube/outage-page/past-issue/past-issue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'clark-past-issue',
+  templateUrl: './past-issue.component.html',
+  styleUrls: ['./past-issue.component.scss']
+})
+export class PastIssueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
