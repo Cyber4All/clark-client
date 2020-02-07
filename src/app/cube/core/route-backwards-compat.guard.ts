e40193df4171bcc36d4982583311cd370d9afab9
Emily Vogel <eav990@gmail.com>
2020-02-07 12:36:38 -0500

    Reroute user if using malformed uri

--- src/app/cube/core/route-backwards-compat.guard.ts
+++ src/app/cube/core/route-backwards-compat.guard.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { router } from 'app/onion/learning-object-builder/components/content-upload/app/content-upload.routes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteBackwardsCompatGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
@@ -14,7 +18,8 @@ export class RouteBackwardsCompatGuard implements CanActivate {
       if (next.params.learningObjectName.search(cuidRegex) === 0) {
         return true;
       } else {
-        return false;
+        // Route home first, change to found cuid later
+        return this.router.createUrlTree(['']);
       }
   }
   
