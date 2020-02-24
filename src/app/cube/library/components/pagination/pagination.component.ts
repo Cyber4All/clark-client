9bb68b5ba99f3bf503c6c3c77c12751bcf9e1c31
Nick Visalli <nvisal1@students.towson.edu>
2020-02-24 08:36:09 -0500

    Fix left side pagination bug

--- src/app/cube/library/components/pagination/pagination.component.ts
+++ src/app/cube/library/components/pagination/pagination.component.ts
@@ -64,13 +64,13 @@ export class PaginationComponent implements OnInit {
 
     // If there are less page numbers to show on the
     // right side, show more on the left side
-    if (rightSideCount === 0) { leftSideCount = 4; }
-    else if (rightSideCount === 1) { leftSideCount = 3; }
+    if (rightSideCount === 0 && this.currentPageNumber > 4) { leftSideCount = 4; }
+    else if (rightSideCount === 1 && this.currentPageNumber >= 4) { leftSideCount = 3; }
 
     // If there are less page numbers to show on
     // the left side, show more on the right side
-    if (leftSideCount === 0) { rightSideCount = 4; }
-    else if (leftSideCount === 1) { rightSideCount = 3; }
+    if (leftSideCount === 0 && this.lastPageNumber >= 5) { rightSideCount = 4; }
+    else if (leftSideCount === 1 && this.lastPageNumber >= 5) { rightSideCount = 3; }
 
     return { leftSideCount, rightSideCount };
   }
