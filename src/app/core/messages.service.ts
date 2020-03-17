27b4abc526a6efa0f14b48287270c9010fd51442
Emily Vogel <eav990@gmail.com>
2020-03-17 09:39:57 -0400

    Hardcode plan c message

--- src/app/core/messages.service.ts
+++ src/app/core/messages.service.ts
@@ -9,7 +9,7 @@ export class Message {
 }
 @Injectable()
 export class MessagesService {
-  private _message: Message;
+  private _message: Message = new Message(true, 'Clark is now accepting curriculum submissions to the Plan C collection.');
 
   get message() {
     return this._message;
@@ -18,6 +18,7 @@ export class MessagesService {
   constructor(private http: HttpClient) { }
 
   getStatus(): Promise<Message> {
+    console.log('Getting status... ', this._message);
     if (this._message) {
       return Promise.resolve(this._message);
     } else {
