00580f552acc685f0a0fa4c47368cc145d149abf
pzalep1 <pz0342@gmail.com>
2020-04-03 10:29:35 -0400

    Swtich to link

--- src/app/unauthorized.component.ts
+++ src/app/unauthorized.component.ts
@@ -11,16 +11,16 @@ import { map } from 'rxjs/operators';
 export class UnauthorizedComponent implements OnInit {
 
   statusCode: string;
-  redirectUrl: any;
+  redirectUrl: string;
   constructor(private router: Router, private route: ActivatedRoute, private auth: AuthService) { }
 
   ngOnInit() {
     this.statusCode = this.route.snapshot.paramMap.get('code');
-    this.redirectUrl = this.route.snapshot.paramMap.get('redirectUrl');
+    this.redirectUrl = this.route.snapshot.paramMap.get('redirect');
   }
 
   login() {
-    this.router.navigate(['auth'], { queryParams: { redirectUrl: this.redirectUrl } });
+    this.router.navigate(['auth/login', { queryParams: { redirectUrl: this.redirectUrl }}]);
   }
 
 }
