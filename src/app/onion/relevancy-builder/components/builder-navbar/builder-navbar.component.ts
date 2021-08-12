bbd94f37397d36991af795072b4989ceb35c8853
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2021-08-12 13:46:31 -0400

    ts list fix

--- src/app/onion/relevancy-builder/components/builder-navbar/builder-navbar.component.ts
+++ src/app/onion/relevancy-builder/components/builder-navbar/builder-navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'relevancy-builder-navbar',
+  selector: 'onion-relevancy-builder-navbar',
   templateUrl: './builder-navbar.component.html',
   styleUrls: ['./builder-navbar.component.scss']
 })
