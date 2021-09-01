4fd3f3c55a2de08579bb1209f787f09cf0f0e54c
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2021-09-01 15:16:45 -0400

    block comment and aria fix

--- src/app/core/relevancy.service.ts
+++ src/app/core/relevancy.service.ts
@@ -104,6 +104,13 @@ export class RelevancyService {
     });
   }
 
+  /**
+   *  Function to update the learning object with new outcome mappings
+   * @param username username of learning object owner
+   * @param objectId learning objectId
+   * @param outcomeId outcomeId on learning object
+   * @param guidelineIds id of mapped guidelines
+   */
   async updateLearningOutcomeMappings(username: string, objectId: string, outcomeId: string, guidelineIds: string[]): Promise<void> {
     return await new Promise((resolve, reject) => {
       this.http
