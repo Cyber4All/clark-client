9badd44f17bad99ccb84586049c8e4cb5a977a69
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2021-09-15 12:36:28 -0400

    actual fix to LO cards

--- src/app/cube/user-profile/user-information/user-information.component.ts
+++ src/app/cube/user-profile/user-information/user-information.component.ts
@@ -12,12 +12,12 @@ import { titleCase } from 'title-case';
   templateUrl: './user-information.component.html',
   styleUrls: ['./user-information.component.scss']
 })
-export class UserInformationComponent implements OnInit, OnChanges {
+export class UserInformationComponent implements OnChanges {
   copy = COPY;
   // User Information
   @Input() user: User;
   @Input() self = false;
-  objects: LearningObject[];
+  objects: LearningObject[] = Array(5).fill(new LearningObject());
   loading = false;
 
   constructor(
@@ -27,10 +27,8 @@ export class UserInformationComponent implements OnInit, OnChanges {
     private notifications: ToastrOvenService
   ) {}
 
-  ngOnInit() {}
-
-  ngOnChanges() {
-    this.getUsersLearningObjects();
+  async ngOnChanges() {
+    await this.getUsersLearningObjects();
   }
 
   navigateToOrganizationPage() {
