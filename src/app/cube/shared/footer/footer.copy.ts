1544640608f92d4c0d394f38e7b7584f1a46dbf4
Paige Zaleppa <pz0342@gmail.com>
2021-09-24 14:38:57 -0400

    Add r in Barnardino

--- src/app/cube/shared/footer/footer.copy.ts
+++ src/app/cube/shared/footer/footer.copy.ts
@@ -1,7 +1,7 @@
 export const COPY = {
     SUPPORTEDBY: 'The CLARK System is partially supported by the National Security Agency ' +
     ' under NSA under Grant H9830-17-1-0405, H98230-21-1-0175, NCyTE Center, ' +
-    ' California State University San Bernadino, and Towson University.',
+    ' California State University San Bernardino, and Towson University.',
     TEAM: 'Meet the Team',
     USAGE: 'Usage Statistics',
     HELP: 'Ask for Help',
