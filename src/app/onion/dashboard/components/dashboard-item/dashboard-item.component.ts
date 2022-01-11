11e6e688fce3b088afbf0752ab82f370fa5e2184
Emily Vogel <eav990@gmail.com>
2022-01-11 11:02:09 -0500

    Update permissions on dashboard item

--- src/app/onion/dashboard/components/dashboard-item/dashboard-item.component.ts
+++ src/app/onion/dashboard/components/dashboard-item/dashboard-item.component.ts
@@ -127,16 +127,18 @@ export class DashboardItemComponent implements OnInit, OnChanges {
    */
   actionPermissions(action: string) {
     const permissions = {
-      edit: ['unreleased', 'denied'],
+      edit: ['unreleased', 'accepted_minor', 'accepted_major'],
       editChildren: [
         'unreleased',
-        'denied',
+        'accepted_minor',
+        'accepted_major',
         this.learningObject.length !== 'nanomodule'
       ],
-      manageMaterials: ['unreleased', 'denied', this.verifiedEmail],
-      submit: ['unreleased', 'denied', this.verifiedEmail],
+      manageMaterials: ['unreleased', 'accepted_minor', 'accepted_major', this.verifiedEmail],
+      submit: ['unreleased', this.verifiedEmail],
+      resubmit: ['accepted_minor', 'accepted_major'],
       view: ['released'],
-      delete: ['unreleased', 'denied'],
+      delete: ['unreleased', 'rejected'],
       cancelSubmission: ['waiting', this.verifiedEmail],
       infoPanel: ['released']
     };
