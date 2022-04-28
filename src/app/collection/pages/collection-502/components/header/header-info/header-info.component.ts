521a307b2cff7ca68d26c5d63dd39d01a8854868
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2022-04-28 14:47:35 -0400

    removed event emittiting

--- src/app/collection/pages/collection-502/components/header/header-info/header-info.component.ts
+++ src/app/collection/pages/collection-502/components/header/header-info/header-info.component.ts
@@ -1,12 +1,10 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'clark-header-502-info',
   templateUrl: './header-info.component.html',
   styleUrls: ['./header-info.component.scss']
 })
 export class HeaderInfo502Component implements OnInit {
-
-  @Output() about: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
--- src/app/collection/pages/collection-502/components/header/header.component.ts
+++ src/app/collection/pages/collection-502/components/header/header.component.ts
@@ -1,11 +1,10 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'clark-502-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class Header502Component implements OnInit {
-  @Output() about: EventEmitter<any> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
