4950f40a5a2e518816ae28685ddd1dce4315d165
Andy Stoerkel <astoer1@students.towson.edu>
2022-06-06 15:15:41 -0400

    added transition animations and login logic

--- src/app/new-auth/login/login.component.ts
+++ src/app/new-auth/login/login.component.ts
@@ -1,23 +1,98 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthValidationService } from 'app/core/auth-validation.service';
+import { AuthService } from 'app/core/auth.service';
 
 @Component({
   selector: 'clark-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger(
+      'inAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: 0, opacity: 0 }),
+            animate('.5s ease-out',
+                    style({ height: 85, opacity: 1 }))
+          ]
+        )
+      ]
+    ),
+        trigger(
+          'outAnimation',
+          [
+            transition(
+              ':leave',
+              [
+                style({ height: 50, opacity: 1 }),
+                animate('.5s ease-in',
+                        style({ height: 0, opacity: 0 }))
+              ]
+            )
+          ]
+        )
+      ]
 })
 export class LoginComponent implements OnInit{
 
-  loginFailure = false;
-  constructor(public authValidation: AuthValidationService) { }
+  loginFailure: Boolean = false;
+  isNameLogin = false;
+  authInfo: {username: string, password: string} = {username:'', password:''};
+  errorMsg: String = 'There was an error';
+  loading = false;
 
-  ngOnInit(): void { }
+  constructor(
+    private authValidation: AuthValidationService,
+    private auth: AuthService,
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+    this.authValidation.getErrorState().subscribe(err => this.loginFailure = err);
+  }
+
 /**
  * TO-DO: implement this method
  *
  * @param form data from NgForm
  */
-  public submit(form: NgForm): void { }
+  public async submit(form: NgForm): Promise<void> {
+    this.authInfo = form.value;
+    if(!this.isPopulated()) {
+      this.authValidation.showError();
+    }
 
+    this.auth
+      .login(this.authInfo)
+      .then(() => {
+          this.router.navigate(['home']);
+      })
+      .catch(error => {
+        this.loading = false;
+        this.errorMsg = error;
+        this.authValidation.showError();
+      });
+  }
+
+/**
+ * ensures all fields are populated when form is submitted
+ *
+ * @returns true if all fields are populated
+ */
+  isPopulated(): boolean {
+    if(this.authInfo.username && this.authInfo.password && true){
+      return true;
+    }
+    this.errorMsg = 'please fill out all fields';
+    return false;
+  }
+
+  showPass(){
+    this.isNameLogin = true;
+  }
 }
