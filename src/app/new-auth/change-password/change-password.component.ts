7df8ee1399b88f47f0ab92a1aeaa379a321c2ee9
Paulo Manalo <pmanal1@students.towson.edu>
2022-06-09 12:18:09 -0400

    Creates basic request password form

--- src/app/new-auth/change-password/change-password.component.ts
+++ src/app/new-auth/change-password/change-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { AuthValidationService } from 'app/core/auth-validation.service';
 
 @Component({
   selector: 'clark-change-password',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent implements OnInit {
+  isError: Boolean = true;
+  emailsMatch: Boolean = true;
+  currentEmail = new FormControl('');
+  confirmEmail = new FormControl('');
 
-  constructor() { }
+  constructor(private authValidationService: AuthValidationService) {
+  }
 
   ngOnInit(): void {
+    this.authValidationService.getErrorState().subscribe(err => this.isError = err);
+  }
+
+  showEmail(): void {
+    console.log(this.currentEmail.value);
+  }
+
+  matchEmails(): void {
+    this.currentEmail.value === this.confirmEmail.value ? this.emailsMatch = true : this.emailsMatch = false;
   }
 
 }
