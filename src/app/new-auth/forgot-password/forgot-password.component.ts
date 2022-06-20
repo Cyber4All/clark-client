98d84d5b94f991b2255d42ff7c0a0d01cdb9d603
Paulo Manalo <pmanal1@students.towson.edu>
2022-06-20 14:59:07 -0400

    Adds extra submit button functionality

--- src/app/new-auth/forgot-password/forgot-password.component.ts
+++ src/app/new-auth/forgot-password/forgot-password.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { RegisteredEmailValidator } from 'app/shared/validators/RegisteredEmailValidator';
 import { MatchValidator } from 'app/shared/validators/MatchValidator';
 import { AuthValidationService } from 'app/core/auth-validation.service';
+import { AuthService } from 'app/core/auth.service';
 
 @Component({
   selector: 'clark-forgot-password',
@@ -16,8 +17,14 @@ export class ForgotPasswordComponent implements OnInit, AfterViewInit {
 
   showError: Boolean = false;
   showDone: Boolean = false;
+  submitButton: String = 'disabled';
+  emails: FormGroup;
 
-  constructor(private registeredEmailValidator: RegisteredEmailValidator, private authValidationService: AuthValidationService) { }
+  constructor(
+    private registeredEmailValidator: RegisteredEmailValidator,
+    private authValidationService: AuthValidationService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
     this.authValidationService.getErrorState().subscribe(err => this.showError = err);
@@ -32,10 +39,27 @@ export class ForgotPasswordComponent implements OnInit, AfterViewInit {
     emailInputControl.updateValueAndValidity();
 
     // Combines inputs into a FormGroup to check if inputs match using MatchValidator
-    const emails = new FormGroup({
+    this.emails = new FormGroup({
       'email': emailInputControl,
       'confirmEmail': confirmEmailInputControl
     }, { validators: MatchValidator.mustMatch('email', 'confirmEmail') });
+  }
+
+  // Disables button if there are any input errors, enables them otherwise
+  toggleButton(): void {
+    if(this.emails.controls.email.errors || this.emails.controls.confirmEmail.errors) {
+      this.submitButton = 'disabled';
+    } else {
+      this.submitButton = 'enabled';
+    }
+  }
+
+  submit(): void {
+    if(this.submitButton === 'enabled') {
+      this.authService.initiateResetPassword(this.emailInput.valueAccessor.control.value).subscribe(val => {
+        this.showDone = true;
+      }, error => {});
+    }
   }
 
   switch(): void {
