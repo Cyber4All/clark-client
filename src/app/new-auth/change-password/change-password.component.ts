bc732938f4bd00cceda96b9e1143af2992e29c22
Paulo Manalo <pmanal1@students.towson.edu>
2022-06-29 17:11:01 -0400

    Adds error banner and a little bit of functionality

--- src/app/new-auth/change-password/change-password.component.ts
+++ src/app/new-auth/change-password/change-password.component.ts
@@ -1,6 +1,7 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 import { AuthValidationService } from 'app/core/auth-validation.service';
 import { AuthService } from 'app/core/auth.service';
@@ -21,10 +22,11 @@ import { MatchValidator } from 'app/shared/validators/MatchValidator';
   ]
 })
 export class ChangePasswordComponent implements OnInit {
-  isError: Boolean = true;
-  done = false;
+  errorMessage: String;
+  showError: Boolean;
+  otaCode: string;
 
-  view = 2;
+  view = 1;
 
   passwords: FormGroup = new FormGroup({
     'password': this.authValidationService.getInputFormControl('password'),
@@ -32,22 +34,23 @@ export class ChangePasswordComponent implements OnInit {
   }, { validators: MatchValidator.mustMatch('password', 'confirmPassword')});
 
   constructor(private authValidationService: AuthValidationService,
-              private authService: AuthService) {
+              private authService: AuthService,
+              private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.authValidationService.getErrorState().subscribe(err => this.isError = err);
+    this.authValidationService.getErrorState().subscribe(err => this.showError = err);
+    this.otaCode = this.activatedRoute.queryParams['_value']['otaCode'];
   }
 
-  // submit(): void {
-  //   this.authService.initiateResetPassword(this.currentEmail.value)
-  //   .subscribe(val => {
-  //     this.done = true;
-  //   }, error => {});
-  // }
-
-  setDone(): void {
-    this.done = true;
+  submit(): void {
+    this.authService.resetPassword(this.passwords.get('password').value, this.otaCode)
+    .subscribe(val => {
+      this.view = 2;
+    }, error => {
+      this.errorMessage = 'Something went wrong! We\'re looking into the issue. Please check back later.';
+      this.authValidationService.showError();
+    });
   }
 
   // emailsMatch(): void {
