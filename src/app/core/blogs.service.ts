5d7133c0a1983e09a911665edbcbe258de53492b
Paulo Manalo <pmanal1@students.towson.edu>
2022-07-08 13:04:36 -0400

    Adds getBlogs functions to service

--- src/app/core/blogs.service.ts
+++ src/app/core/blogs.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+import { Blog } from 'app/components/blogs/types/blog';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMostRecentBlog(): Observable<Blog[]> {
+    return this.http.get<Blog[]>(`${environment.apiURL}/blogs?recent=true`);
+  }
+
+  getAllBlogs(): Observable<Blog[]> {
+    return this.http.get<Blog[]>(`${environment.apiURL}/blogs`);
+  }
 }
