d2ac9740cb8b1c7968ccb053699cb0dedfd0cdc4
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2022-07-12 11:15:31 -0400

    added router toggle on component init

--- src/app/cube/user-profile/user-profile.component.ts
+++ src/app/cube/user-profile/user-profile.component.ts
@@ -1,7 +1,7 @@
-import { UserService } from '../../core/user.service';
 import { Component, OnInit, OnDestroy} from '@angular/core';
-import { AuthService } from '../../core/auth.service';
 import { ActivatedRoute } from '@angular/router';
+import { SubscriptionLike as ISubscription } from 'rxjs';
+import { User } from '@entity';
 
 @Component({
   selector: 'clark-user-profile',
@@ -10,15 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class UserProfileComponent implements OnInit, OnDestroy {
+  subscription: ISubscription;
+  user: User;
 
   constructor(
-    private userService: UserService,
-    private auth: AuthService,
     private route: ActivatedRoute,
   ) {}
 
   ngOnInit() {
-
+    this.subscription = this.route.data.subscribe(val => {
+      this.user = val.user;
+    })
   }
 
   ngOnDestroy() {
