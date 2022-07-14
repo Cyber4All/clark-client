6ec94a94706f57f9e7828e5580af9c8755668abb
Andy Stoerkel <astoer1@students.towson.edu>
2022-07-14 11:09:29 -0400

    setup and fixed some linting errors

--- src/app/admin/pages/learning-objects/learning-objects.component.ts
+++ src/app/admin/pages/learning-objects/learning-objects.component.ts
@@ -156,7 +156,7 @@ export class LearningObjectsComponent
         relativeTo: this.route,
         replaceUrl: true
       }
-    )
+    );
   }
 
   /**
---
+++ src/app/onion/learning-object-builder/components/material-notes/material-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'clark-material-notes',
+  templateUrl: './material-notes.component.html',
+  styleUrls: ['./material-notes.component.scss']
+})
+export class MaterialNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
