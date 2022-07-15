7509334128c9b0dd80a460057ee8983ee32c5be6
Andy Stoerkel <astoer1@students.towson.edu>
2022-07-15 09:25:30 -0400

    cleanup

--- src/app/onion/learning-object-builder/components/material-notes/material-notes.component.ts
+++ src/app/onion/learning-object-builder/components/material-notes/material-notes.component.ts
@@ -1,19 +1,16 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'clark-material-notes',
   templateUrl: './material-notes.component.html',
   styleUrls: ['./material-notes.component.scss']
 })
-export class MaterialNotesComponent implements OnInit {
+export class MaterialNotesComponent {
 
   open = false;
   @Input() note: {title: string, content: string };
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
   openNote(): void {
     this.open = !this.open;
