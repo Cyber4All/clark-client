2d99761a29ac8eea02e6475e23f2c3994688fa77
Paulo Manalo <pmanal1@students.towson.edu>
2022-07-19 12:34:44 -0400

    Adds new-home components

--- src/app/cube/new-home/new-home.module.ts
+++ src/app/cube/new-home/new-home.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewHomeComponent } from './new-home.component';
+import { SplashComponent } from './splash/splash.component';
+import { MissionComponent } from './mission/mission.component';
+import { HelpComponent } from './help/help.component';
+import { LearningObjectInfoComponent } from './learning-object-info/learning-object-info.component';
 
 
 
 @NgModule({
   declarations: [
-    NewHomeComponent
+    NewHomeComponent,
+    SplashComponent,
+    MissionComponent,
+    HelpComponent,
+    LearningObjectInfoComponent
   ],
   imports: [
     CommonModule
