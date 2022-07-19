c295a9faf1acb8c6a136a166ff5202605814e6b8
Paulo Manalo <pmanal1@students.towson.edu>
2022-07-19 12:13:05 -0400

    Instantiates new-home folder, module, and components

---
+++ src/app/cube/new-home/new-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'clark-new-home',
+  templateUrl: './new-home.component.html',
+  styleUrls: ['./new-home.component.scss']
+})
+export class NewHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/cube/new-home/new-home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewHomeComponent } from './new-home.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewHomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NewHomeModule { }
