71174aaa1b0fb16411c5f912d2d092cec103fcb3
Paulo Manalo <pmanal1@students.towson.edu>
2022-07-19 12:40:37 -0400

    Adds subcomponents to help component

---
+++ src/app/cube/new-home/help/teach-now/teach-now.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'clark-teach-now',
+  templateUrl: './teach-now.component.html',
+  styleUrls: ['./teach-now.component.scss']
+})
+export class TeachNowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
