1642f32ea625a121918a987a740e987f060279f6
Andy Stoerkel <astoer1@students.towson.edu>
2022-07-27 16:12:05 -0400

    added underlay and dropdown service

--- src/app/components/secondary-navbar/secondary-navbar.component.ts
+++ src/app/components/secondary-navbar/secondary-navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { NavbarDropdownService } from '../../core/navBarDropdown.service';
 
 @Component({
   selector: 'clark-secondary-navbar',
   templateUrl: './secondary-navbar.component.html',
-  styleUrls: ['./secondary-navbar.component.scss']
+  styleUrls: ['./secondary-navbar.component.scss'],
+  providers: [NavbarDropdownService]
 })
 export class SecondaryNavbarComponent implements OnInit {
 
@@ -11,28 +13,27 @@ export class SecondaryNavbarComponent implements OnInit {
   collectionsDropdown = false;
   resourcesDropdown = false;
   isDesktop = false;
-  constructor() { }
+  constructor(
+    private dropdowns: NavbarDropdownService
+  ) { }
 
   ngOnInit(): void {
     this.isDesktop = (window.innerWidth >= 1024) ? true : false;
+    this.dropdowns.topicDropdown.subscribe(val => {
+      this.topicDropdown = val;
+    });
+    this.dropdowns.collectionsDropdown.subscribe(val => {
+      this.collectionsDropdown = val;
+    });
+    this.dropdowns.resourcesDropdown.subscribe(val => {
+      this.resourcesDropdown = val;
+    });
   }
 
   @HostListener('window:resize', ['$event'])
 
   resizeWindow() {
     this.isDesktop = (window.innerWidth >= 1024) ? true : false;
-  }
-
-  openTopics() {
-    this.topicDropdown = !this.topicDropdown;
-  }
-
-  openCollections() {
-    this.collectionsDropdown = !this.collectionsDropdown;
-  }
-
-  openResources() {
-    this.resourcesDropdown = !this.resourcesDropdown;
   }
 
 }
