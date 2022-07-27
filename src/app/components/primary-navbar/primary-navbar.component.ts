941d9a965ab4ab091cfa29b821329fd465df5ed1
Andy Stoerkel <astoer1@students.towson.edu>
2022-07-27 16:12:05 -0400

    added underlay and dropdown service

--- src/app/components/primary-navbar/primary-navbar.component.ts
+++ src/app/components/primary-navbar/primary-navbar.component.ts
@@ -1,12 +1,15 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthService } from 'app/core/auth.service';
 import * as md5 from 'md5';
+import { HighlightSpanKind } from 'typescript';
+import { NavbarDropdownService } from '../../core/navBarDropdown.service';
 
 
 @Component({
   selector: 'clark-primary-navbar',
   templateUrl: './primary-navbar.component.html',
-  styleUrls: ['./primary-navbar.component.scss']
+  styleUrls: ['./primary-navbar.component.scss'],
+  providers: [NavbarDropdownService]
 })
 export class PrimaryNavbarComponent implements OnInit {
 
@@ -20,7 +23,7 @@ export class PrimaryNavbarComponent implements OnInit {
   topics = ['topic 1', 'topic 2'];
   showResources = false;
   // @Input()
-  externalResources = [{content: 'now this is content'}, {content: 'this is also content'}]
+  externalResources = [{content: 'now this is content'}, {content: 'this is also content'}];
   @HostListener('window:resize', ['$event'])
 
   resizeWindow() {
@@ -28,39 +31,33 @@ export class PrimaryNavbarComponent implements OnInit {
   }
 
   constructor(
-    public auth: AuthService
+    public auth: AuthService,
+    public dropdowns: NavbarDropdownService
   ) { }
 
   ngOnInit(): void {
     this.isDesktop = (window.innerWidth >= 1024) ? true : false;
     this.auth.isLoggedIn.subscribe(val => {
-      console.log(val);
       this.isLoggedIn = val;
     });
-  }
-
-  openMSearch() {
-    this.isMSearch = !this.isMSearch;
-  }
-
-  openMHamburger() {
-    this.isMHamburger = !this.isMHamburger;
-  }
-
-  openResources() {
-    this.showResources = !this.showResources;
-  }
-
-  openTopics() {
-    this.showTopics = !this.showTopics;
-  }
-
-  openAcademicLevels() {
-    this.levelsDropdown = !this.levelsDropdown;
-  }
-
-  openUserDropdown() {
-    this.userDropdown = !this.userDropdown;
+    this.dropdowns.isMHamburger.subscribe(val => {
+      this.isMHamburger = val;
+    });
+    this.dropdowns.userDropdown.subscribe(val => {
+      this.userDropdown = val;
+    });
+    this.dropdowns.levelsDropdown.subscribe(val => {
+      this.levelsDropdown = val;
+    });
+    this.dropdowns.resourcesDropdown.subscribe(val => {
+      this.showResources = val;
+    });
+    this.dropdowns.isMSearch.subscribe(val => {
+      this.isMSearch = val;
+    });
+    this.dropdowns.topicDropdown.subscribe(val => {
+      this.showTopics = val;
+    });
   }
 
   gravatarImage(size): string {
