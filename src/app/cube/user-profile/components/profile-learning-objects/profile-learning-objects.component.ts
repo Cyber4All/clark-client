b3b212c8c4152eba5037922a3a8f6f7a47ce014f
Katelyn Blomquist <katelyn.blomquist@gmail.com>
2022-08-16 10:41:14 -0400

    Collections refactor

--- src/app/cube/user-profile/components/profile-learning-objects/profile-learning-objects.component.ts
+++ src/app/cube/user-profile/components/profile-learning-objects/profile-learning-objects.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { LearningObjectService } from 'app/cube/learning-object.service';
 import { UserService } from 'app/core/user.service';
 import { CollectionService } from 'app/core/collection.service';
-import { element } from 'protractor';
 
 @Component({
   selector: 'clark-profile-learning-objects',
@@ -28,23 +27,22 @@ export class ProfileLearningObjectsComponent implements OnInit {
   collectionsReleased = [];
   collectionsUnreleased = [];
   collectionsAbreviated;
-  collectionsFullReleased = [];
-  collectionsFullUnreleased;
   tempCollectionsReleased = [];
   tempCollectionsUnreleased = [];
   currentPage = 1;
 
-
+  //Retrieves learning objects and collections list, sorts them both by released vs unreleased.
+  //Collections get stored in a temporary variable that then is used to generate an array of
+  //unique collection names. This is then used to generate an array of Full Collection names.
   async ngOnInit() {
     this.learningObjects = await this.learningObjectService.getUsersLearningObjects(this.username);
     this.collectionsAbreviated = await this.userService.getCollectionData(this.username);
     this.learningObjectsReleased = this.learningObjects.filter(learningObject => {
       return learningObject.status === 'released';
-      });
+    });
     this.learningObjectsUnreleased = this.learningObjects.filter(learningObject => {
       return learningObject.status !== 'released';
-      });
-    console.log(this.learningObjectsUnreleased);
+    });
     this.tempCollectionsReleased = this.collectionsAbreviated.filter(learningObject => {
       return learningObject.status === 'released';
     });
@@ -52,33 +50,36 @@ export class ProfileLearningObjectsComponent implements OnInit {
       return learningObject.status !== 'released';
     });
     this.collectionsReleased = this.genCollections(this.tempCollectionsReleased);
-    console.log(this.collectionsReleased);
     this.collectionsUnreleased = this.genCollections(this.tempCollectionsUnreleased);
-    this.collectionsFullReleased = this.getFullCollectionName(this.collectionsReleased);
-    console.log(this.collectionsFullReleased);
-    this.collectionsFullUnreleased = this.getFullCollectionName(this.collectionsUnreleased);
-    };
+  };
 
-
+  //sets active status for released vs unreleased
   activateMainTab(tabName: string) {
-    if(tabName === 'released') {
+    if (tabName === 'released') {
       this.tabMain = 1;
-    } else if(tabName === 'review') {
+    } else if (tabName === 'review') {
       this.tabMain = 2;
     }
     // else if (tabName === 'relevency') this.tabMain = 3;
     return this.tabMain;
   }
 
-  activateCollectionTab(tabName: string) {
-    if(this.tabMain === 1) {
-      this.tabCollection = this.collectionsReleased.indexOf(tabName);
-    } else if (this.tabMain === 2){
-      this.tabCollection = this.collectionsUnreleased.indexOf(tabName);
+  //sets active status for collection tabs based on whether or not you're in the relased or
+  //unreleased tab.
+  activateCollectionTab(tabName: number) {
+    console.log(tabName);
+    if (this.tabMain === 1) {
+      this.tabCollection = tabName;
+    } else if (this.tabMain === 2) {
+      this.tabCollection = tabName;
     }
+    console.log(this.tabCollection);
+    console.log(this.collectionsReleased.indexOf(tabName));
     return this.tabCollection;
   }
 
+  //retrieves learning objects based on status and collection with specification on how to
+  //handle missing collection names.
   content(status: number, collection: string) {
     if (status === 1) {
       this.learningObjectsReleased = this.learningObjects.filter(learningObject => {
@@ -91,37 +92,41 @@ export class ProfileLearningObjectsComponent implements OnInit {
         });
       } else {
         this.learningObjectsUnreleased = this.learningObjects.filter(learningObject => {
-      return learningObject.status !== 'released' && learningObject.collection === collection;
-      });
+          return learningObject.status !== 'released' && learningObject.collection === collection;
+        });
       }
     }
   }
 
+  //toggles dropdown for mobile
   toggleDropdown(open: boolean) {
     const x = [1, 2, 3];
     this.mobileDropdown = open;
   }
 
-  genCollections(arr: any){
-    arr.forEach(element => {
-      if (element.collection === ''){
-      element.collection = 'Drafts';
-    }
-    });
-     return [...new Set(arr.map(x => x.collection))];
-  }
 
-   getFullCollectionName(arr: any){
-    const fullName = [];
+  //generates array containing the full collection name and it's abreviated name
+  genCollections(arr: any) {
+    let uniqueCollectionNames = [];
+    const collectionNames = [];
+
     arr.forEach(element => {
-      if(element !== 'Drafts') {
-        element = this.collectionService.getCollection(element).then(c => {
-          fullName.push(c.name);
-        });
-      } else {
-        fullName.push(element);
+      if (element.collection === '') {
+        element.collection = 'Drafts';
       }
     });
-    return fullName;
+    uniqueCollectionNames = [...new Set(arr.map(x => x.collection))];
+    for (let i = 0; i < uniqueCollectionNames.length; i++) {
+      if (uniqueCollectionNames[i] !== 'Drafts') {
+        this.collectionService.getCollection(uniqueCollectionNames[i]).then(c => {
+          collectionNames.push({ ...c });
+        });
+      } else if (uniqueCollectionNames[i] === 'Drafts') {
+        collectionNames.push({abvName: 'Drafts', name:'Drafts'});
+      }
+    };
+    console.log(collectionNames);
+    return collectionNames;
   }
 }
+
