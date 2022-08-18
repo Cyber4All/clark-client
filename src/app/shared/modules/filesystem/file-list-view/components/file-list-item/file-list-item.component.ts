43f9bbdfc554ff7ae116f6594e86e0b48cc7d1c3
Paulo Manalo <pmanal1@students.towson.edu>
2022-08-18 13:09:37 -0400

    Checks if user accessGroups exists

--- src/app/shared/modules/filesystem/file-list-view/components/file-list-item/file-list-item.component.ts
+++ src/app/shared/modules/filesystem/file-list-view/components/file-list-item/file-list-item.component.ts
@@ -72,6 +72,9 @@ export class FileListItemComponent implements OnInit {
    * @returns boolean value if the user is valid
    */
   checkAccessGroups(): boolean {
-    return this.inBuilder && (this.accessGroups.includes('admin') || this.accessGroups.includes('curator'));
+    if(this.accessGroups && this.accessGroups.length > 0) {
+      return this.inBuilder && (this.accessGroups.includes('admin') || this.accessGroups.includes('curator'));
+    }
+    return false;
   }
 }
