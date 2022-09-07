30cba3492aec9601eaa22c3269fcab68ce8b9c80
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2022-09-07 11:41:54 -0400

    added behavior subject comment

--- src/app/cube/user-profile/components/profile-header/profile-header.component.ts
+++ src/app/cube/user-profile/components/profile-header/profile-header.component.ts
@@ -9,6 +9,17 @@ import { BehaviorSubject } from 'rxjs';
   styleUrls: ['./profile-header.component.scss']
 })
 export class ProfileHeaderComponent implements OnInit {
+  /**
+   * @private _user is an observable that subscribes to the user being set in the parent component
+   * Profiles uses a route resolver that fetches a profile when a user's profile is visited. This subscription
+   * allows the profile data to be rendered dynamically when visiting a new profile.
+   * EXAMPLE:
+   * 1. Dr. Taylor is logged into CLARK
+   * 2. Dr. Taylor visits Dr. Kaza's profile
+   * 3. Dr. Taylor decides to navigate to her profile from Dr. Kaza's profile
+   * Without the behavior subject, Dr. Taylor's profile would not render correctly. Dr. Kaza's
+   * header would persist through this example
+   */
   private _user = new BehaviorSubject<any>({});
   @Input() set user(user: any) {
     this._user.next(user);
