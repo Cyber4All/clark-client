cb3ac74d1c0d23881db10d5c4091767650d74fda
Cwagne17 <cwagne17@students.towson.edu>
2022-10-13 10:37:58 -0400

    pls work with bash

---
+++ .circleci/create-story.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o nounset
+
+WORKFLOW_STATE_ID=500005737
+PROJECT_ID=14743
+GROUP_ID="61ae65f8-ec5a-4f81-8b85-76516b014ed5"
+REPOSITORY=$CIRCLE_PROJECT_REPONAME
+DESCRIPTION="## *Repositories*: $REPOSITORY<br><br>Description<br><br>Post deployment or quarterly review of Software Bill of Materials (SBOM) for $REPOSITORY. This story will track the vulnerability analysis of the SBOM and any dependency upgrades.<br><br>Requirements<br><br>1. Download and scan SBOM using cve-bin-tool<br>2. Upload scan results to Shortcut story<br>3. Upgrade dependencies with HIGH or CRITICAL vulnerabilities<br><br>Documentation<br><br>[CVE_BIN_TOOL Documentation](https://cve-bin-tool.readthedocs.io/en/latest/README.html#scanning-an-sbom-file-for-known-vulnerabilities)<br><br>CVE Databases<br><br>[NIST NVD Homepage](https://nvd.nist.gov/)<br>[OpenSSF OSV Homepage](https://osv.dev/)"
+
+curl -X POST \
+  -L "https://api.app.shortcut.com/api/v3/stories" \
+  -H "Content-Type: application/json" \
+  -H "Shortcut-Token: $SHORTCUT_API_TOKEN" \
+  -d '{"deadline": "2022-12-31T12:30:00Z", "description": "'"$DESCRIPTION"'", "labels": [{"color": "#f8324e", "description": "Repository", "name": "'"$REPOSITORY"'"},{ "color": "#1c70dd", "description": "Language", "name": "'"$LANGUAGE"'"},{"color": "#4287f5", "description": "Version", "name": "'"$VERSION"'"}], "external_links": ["'"$CIRCLE_BUILD_URL"'"], "group_id": "'"$GROUP_ID"'", "move_to": "first", "name": "'"Review $REPOSITORY:$VERSION SBOM"'", "project_id": "'"$PROJECT_ID"'", "story_type": "chore", "workflow_state_id": "'"$WORKFLOW_STATE_ID"'" }'
