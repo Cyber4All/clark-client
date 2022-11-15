b831c1f5c4c6586adb88e537a0a17a63cb06ee71
Paulo Manalo <pmanal1@students.towson.edu>
2022-11-15 12:24:35 -0500

    Adds blooms cards info and adjusts some styling

--- src/app/cube/home/learning-object-info/learning-outcomes/learning-outcomes.component.ts
+++ src/app/cube/home/learning-object-info/learning-outcomes/learning-outcomes.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearningOutcomesComponent implements OnInit {
 
+  bloomCards = [{
+    icon: 'fal fa-cloud',
+    text: 'Remember & Understand',
+    description: `1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Morbi tincidunt ornare massa eget. Ut tellus elementum sagittis vitae et.`
+  }, {
+    icon: 'fal fa-bolt',
+    text: 'Apply & Analyze',
+    description: `2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Morbi tincidunt ornare massa eget. Ut tellus elementum sagittis vitae et.`
+  }, {
+    icon: 'fal fa-balance-scale-right',
+    text: 'Evaluate and Synthesize',
+    description: `3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Morbi tincidunt ornare massa eget. Ut tellus elementum sagittis vitae et.`
+  }];
+
   constructor() { }
 
   ngOnInit(): void {
