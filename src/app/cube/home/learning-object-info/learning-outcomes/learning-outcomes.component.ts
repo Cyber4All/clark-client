bc83ba565b72a1f32fb4c6761294547dc3bf0131
Paulo Manalo <pmanal1@students.towson.edu>
2022-11-17 14:54:38 -0500

    Adds some card flip animation

--- src/app/cube/home/learning-object-info/learning-outcomes/learning-outcomes.component.ts
+++ src/app/cube/home/learning-object-info/learning-outcomes/learning-outcomes.component.ts
@@ -1,28 +1,41 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'clark-learning-outcomes',
   templateUrl: './learning-outcomes.component.html',
-  styleUrls: ['./learning-outcomes.component.scss']
+  styleUrls: ['./learning-outcomes.component.scss'],
+  animations: [
+    trigger('flipState', [
+      state('active', style({
+        transform: 'rotateY(179deg)'
+      })),
+      state('inactive', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class LearningOutcomesComponent implements OnInit {
 
   bloomCards = [{
     icon: 'fal fa-cloud fa-3x',
     text: 'Remember & Understand',
-    flipped: false,
+    flipped: 'inactive',
     clicked: false,
     description: `Recalling relevant knowledge from long term memory and making sense of the knowledge that you have gained.`
   }, {
     icon: 'fal fa-bolt fa-3x',
     text: 'Apply & Analyze',
-    flipped: false,
+    flipped: 'inactive',
     clicked: false,
     description: `Using knowledge that you have gained and understanding how various pieces of knowledge are related.`
   }, {
     icon: 'fal fa-balance-scale-right fa-3x',
     text: 'Evaluate and Synthesize',
-    flipped: false,
+    flipped: 'inactive',
     clicked: false,
     description: `Making decisions based on known guidelines and creating something original.`
   }];
@@ -30,5 +43,9 @@ export class LearningOutcomesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleFlip(card: any, bool: boolean) {
+    card.flipped = bool ? 'active' : 'inactive';
   }
 }
