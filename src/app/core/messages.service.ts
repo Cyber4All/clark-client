5d3f75861eae4314ee39a6002ec7cb6c43ab99aa
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2023-02-20 11:38:44 -0500

    moved deployment to wednesday

--- src/app/core/messages.service.ts
+++ src/app/core/messages.service.ts
@@ -9,8 +9,8 @@ export class Message {
 }
 @Injectable()
 export class MessagesService {
-  private _message: Message = new Message(true, 'The CLARK team will conduct regular maintenance of the CLARK system on Tuesday February'+
-  ' 21, 2023 from 6:00AM-8:00AM EST. CLARK will be available but some users might see downgraded performance');
+  private _message: Message = new Message(true, 'The CLARK team will conduct regular maintenance of the CLARK system on Wednesday February'+
+  ' 22, 2023 from 6:00AM-8:00AM EST. CLARK will be available but some users might see downgraded performance');
 
   get message() {
     return this._message;
