5f75cf50438a40214e957f7f91031c09ad40ad67
Stefanos Savvides <stefanzew444@gmail.com>
2023-02-22 11:02:29 -0500

    Added an else statement to make sure the revised learning object is null if it's not true

--- src/app/cube/details/details.component.ts
+++ src/app/cube/details/details.component.ts
@@ -162,7 +162,7 @@ export class DetailsComponent implements OnInit, OnDestroy {
           owners.push(this.releasedLearningObject.author.username);
 
           this.learningObjectOwners = owners;
-          // this.hasRevision = !!this.releasedLearningObject.revisionUri;
+          this.hasRevision = !!this.releasedLearningObject.revisionUri;
           this.learningObject = this.releasedLearningObject;
           // Set page title
           this.titleService.setTitle('CLARK | '+ this.learningObject.name);
