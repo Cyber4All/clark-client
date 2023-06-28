7ba3e79419edd9416c3649ade9d016d03945b486
Shannon Ioffe <shannon.ioffe@secured.team>
2023-06-28 16:50:43 -0400

    implements hero, description, and matrix

--- src/app/collection/pages/security-injections/security-injections.component.ts
+++ src/app/collection/pages/security-injections/security-injections.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LearningObject } from '@entity';
+import { CollectionService } from 'app/core/collection.service';
+import { FeaturedObjectsService } from 'app/core/featuredObjects.service';
+import { NavbarService } from '../../../core/navbar.service';
 
 @Component({
   selector: 'clark-security-injections',
   templateUrl: './security-injections.component.html',
-  styleUrls: ['./security-injections.component.scss']
+  styleUrls: ['./security-injections.component.scss'],
 })
 export class SecurityInjectionsComponent implements OnInit {
 
-  constructor() { }
+  abvCollection = 'secinj';
+  collection;
+  learningObjects: LearningObject[];
 
-  ngOnInit(): void {
+  constructor(
+    private navbarService: NavbarService,
+    private collectionService: CollectionService,
+    private featureService: FeaturedObjectsService,
+    private router: Router,
+    ) { }
+
+  async ngOnInit() {
+    this.navbarService.show();
+
+    this.collection = await this.collectionService.getCollection(this.abvCollection);
+
+    this.collection = await this.collectionService.getCollectionMetadata(this.abvCollection).catch(e => {
+      if (e.status === 404) {
+        this.router.navigate(['not-found']);
+      }
+    });
+
+    this.learningObjects = await this.featureService.getCollectionFeatured(this.abvCollection);
+
   }
 
+  ngOnDestroy(): void {
+    this.navbarService.hide();
+  }
 }
