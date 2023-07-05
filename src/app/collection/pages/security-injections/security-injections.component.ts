c189c3df106d6ec68bef101a1682662aec48c50d
Shannon Ioffe <shannon.ioffe@secured.team>
2023-07-05 11:38:02 -0400

    update page

--- src/app/collection/pages/security-injections/security-injections.component.ts
+++ src/app/collection/pages/security-injections/security-injections.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Collection, LearningObject } from '@entity';
+import { CollectionService } from 'app/core/collection.service';
+import { FeaturedObjectsService } from 'app/core/featuredObjects.service';
+import { NavbarService } from '../../../core/navbar.service';
 
 @Component({
   selector: 'clark-security-injections',
   templateUrl: './security-injections.component.html',
   styleUrls: ['./security-injections.component.scss']
 })
-export class SecurityInjectionsComponent implements OnInit {
+export class SecurityInjectionsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  abvCollection = 'secinj';
+  collection: Collection;
+  learningObjects: LearningObject[];
 
-  ngOnInit(): void {
+  constructor(
+    private navbarService: NavbarService,
+    private collectionService: CollectionService,
+    private featureService: FeaturedObjectsService) { }
+
+  async ngOnInit() {
+
+    this.navbarService.show();
+
+    this.collection = await this.collectionService.getCollection(this.abvCollection);
+
+    this.learningObjects = await this.featureService.getCollectionFeatured(this.abvCollection);
+
+  }
+
+    ngOnDestroy(): void {
+    this.navbarService.hide();
   }
 
 }
