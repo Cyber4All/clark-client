6fc032436e762b339648fb4d850877f088cb492f
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2024-02-22 16:29:35 -0500

    remove extra line for bloc comments

--- src/app/core/access-group-module/access-group.routes.ts
+++ src/app/core/access-group-module/access-group.routes.ts
@@ -3,7 +3,6 @@ import { environment } from '@env/environment';
 export const ACCESS_GROUP_ROUTES = {
   /**
    * Request to retrieve the access groups for the given user
-   *
    * @param id user id
    * @method GET
    * @auth required
@@ -16,7 +15,6 @@ export const ACCESS_GROUP_ROUTES = {
   },
   /**
    * Request to add a single access group from a user
-   *
    * @param username username of the user
    * @method POST
    * @auth required
@@ -28,7 +26,6 @@ export const ACCESS_GROUP_ROUTES = {
   },
   /**
    * Request to add a single access group from a user
-   *
    * @param username username of the user
    * @method PATCH
    * @auth required
@@ -40,7 +37,6 @@ export const ACCESS_GROUP_ROUTES = {
   },
   /**
    * Request to get the users with access to the given collection
-   *
    * @param collectionAbvName the abbreviated name of the collection
    * @method GET
    * @auth required
