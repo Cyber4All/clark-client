55e36d9992d7ae776e9934369ae55ec639e21716
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2024-02-22 21:01:18 -0500

    sike, had to update he utilitys routes

--- src/app/core/utility-module/utility.routes.ts
+++ src/app/core/utility-module/utility.routes.ts
@@ -1,14 +1,37 @@
 import { environment } from '@env/environment';
 
 export const UTILITY_ROUTES = {
-  //Blog routes
-  // GET, AUTH REQUIRED
-  GET_BLOGS: `${environment.apiURL}/blogs`,
-
-  // POST, AUTH REQUIRED
-  POST_BLOG: `${environment.apiURL}/blogs`,
-
-  // Client Version routes
-  // GET
-  CLIENT_VERSION: `${environment.apiURL}/clientversion`,
+  /**
+   * Request to retrieve the list of blogs
+   * @method GET
+   * @returns list of blogs
+   */
+  GET_BLOGS() {
+    return `${environment.apiURL}/blogs`;
+  },
+  /**
+   * Request to post a blog
+   * @method POST
+   * @auth required
+   * @returns the blog that was posted
+   */
+  POST_BLOGS() {
+    return `${environment.apiURL}/blogs`;
+  },
+  /**
+   * Request to retrieve the client version
+   * @method GET
+   * @returns the client version
+   */
+  GET_CLIENT_VERSION() {
+    return `${environment.apiURL}/clientversion`;
+  },
+  /**
+   * Request to see if service is down or if there is a banner message
+   * @method GET
+   * @returns downtime information and/or banner message
+   */
+  GET_DOWNTIME() {
+    return `${environment.apiURL}/downtime`;
+  },
 };
