df7aabc7806730d8a5309ae957fa40efe8f9b713
Michael Franz <55514493+mfranz1@users.noreply.github.com>
2024-02-29 19:51:12 -0500

    added submission routes

--- src/app/core/learning-object-module/submissions/submissions.routes.ts
+++ src/app/core/learning-object-module/submissions/submissions.routes.ts
@@ -1,1 +1,36 @@
 import { environment } from '../../../../environments/environment';
+
+export const SUBMISSION_ROUTES = {
+    /**
+     * Request to get submissions for a learning object
+     * @param userId - The id of the author of the learning object
+     * @param learningObjectId - The id of the learning object to get submissions for
+     */
+    GET_SUBMISSIONS(userId: string, learningObjectId: string) {
+        return `${environment.apiURL}/users/${encodeURIComponent(
+            userId
+        )}/learning-objects/${encodeURIComponent(learningObjectId)}/submissions`;
+    },
+    /**
+     * Request to submit a learning object
+     * @param userId - The id of the author of the learning object
+     * @param learningObjectId - The id of the learning object to submit
+     * @auth required
+     */
+    SUBMIT_LEARNING_OBJECT(userId: string, learningObjectId: string) {
+        return `${environment.apiURL}/users/${encodeURIComponent(
+            userId
+        )}/learning-objects/${encodeURIComponent(learningObjectId)}/submissions`;
+    },
+    /**
+     * Request to cancel a submission
+     * @param userId - The id of the author of the learning object
+     * @param learningObjectId - The id of the learning object to cancel the submission for
+     * @auth required
+     */
+    DELETE_SUBMISSION(userId: string, learningObjectId: string) {
+        return `${environment.apiURL}/users/${encodeURIComponent(
+            userId
+        )}/learning-objects/${encodeURIComponent(learningObjectId)}/submissions`;
+    },
+};
