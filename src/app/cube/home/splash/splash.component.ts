a22464027375e228e0cdbe02a9941d36f17544ee
Michael Franz <michael.franz@secured.team>
2024-03-28 12:11:24 -0400

    added card browse button to homepage and disabled relevancy banner

--- src/app/cube/home/splash/splash.component.ts
+++ src/app/cube/home/splash/splash.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsageStatsService } from 'app/cube/core/usage-stats/usage-stats.service';
 import { GoogleTagService } from '../google-tag.service';
+import { UtilityService } from 'app/core/utility.service';
 
 @Component({
   selector: 'clark-splash',
@@ -9,15 +10,26 @@ import { GoogleTagService } from '../google-tag.service';
 })
 export class SplashComponent implements OnInit {
   numReleasedObjects = 0; // default number of released objects before the service provides a new number
-
+  resourceCount = 0; // default number of resources before the service provides a new number
   constructor(
     private usageStatsService: UsageStatsService,
-    public googleTagService: GoogleTagService
+    public googleTagService: GoogleTagService,
+    private utilityService: UtilityService
     ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.usageStatsService.getLearningObjectStats().then(stats => {
       this.numReleasedObjects = Math.floor(stats.released / 10) * 10;
     });
+    await this.utilityService.getAllResources().then((res: any) => {
+      this.resourceCount = Math.floor(res.total / 10) * 10;
+    });
+  }
+
+  /**
+   * Alert message for users leaving the site
+   */
+  leavingSite() {
+    window.alert('You are now leaving CLARK. You will be redirected to the CAE Resource Directory.');
   }
 }
