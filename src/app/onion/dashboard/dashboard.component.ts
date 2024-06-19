ce7a4601711a1d3dee940786dd81d5733cf2c158
johngonz0 <john.gonsalves@secured.team>
2024-06-19 10:27:49 -0400

    Client changes

--- src/app/onion/dashboard/dashboard.component.ts
+++ src/app/onion/dashboard/dashboard.component.ts
@@ -329,7 +329,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
       s => [LearningObject.Status.UNRELEASED, LearningObject.Status.REJECTED].includes(s.status)
     );
     if (canDelete.length === 1) {
-      return this.learningObjectService.delete(canDelete[0].author.username, canDelete[0].id)
+      return this.learningObjectService.delete(canDelete[0].id)
         .then(async () => {
           this.notificationService.success('Done!', 'Learning Object deleted!');
           await this.getDraftLearningObjects();
