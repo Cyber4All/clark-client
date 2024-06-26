8a2182b5ca8d23710295da12b4cdd124916f9ef3
johngonz0 <john.gonsalves@secured.team>
2024-06-26 11:29:32 -0400

    womp womp mike

--- src/app/onion/core/learning-object.service.ts
+++ src/app/onion/core/learning-object.service.ts
@@ -284,7 +284,7 @@ export class LearningObjectService {
    * @param outcomeId The outcome Id
    */
   deleteOutcome(outcomeId: string): Promise<any> {
-    console.log("here");
+    console.log('here');
     return this.http
       .delete(OUTCOME_ROUTES.DELETE_OUTCOME(outcomeId), { headers: this.headers, withCredentials: true })
       .pipe(
--- src/app/onion/learning-object-builder/builder-store.service.ts
+++ src/app/onion/learning-object-builder/builder-store.service.ts
@@ -1061,7 +1061,7 @@ export class BuilderStore {
       // clear the cache here so that new requests start a new cache
       this.outcomeCache$.next(undefined);
 
-      console.log(newValue); 
+      console.log(newValue);
       if (this.newOutcomes.get(newValue.id)) {
         // this is a new outcome that hasn't been saved, create it
         this.createLearningOutcome(newValue);
