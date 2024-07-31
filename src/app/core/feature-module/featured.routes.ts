436471e6a18a7ce0f528e8cf8a8ca0ff9854cc5b
stef359 <stefanossavv1@gmail.com>
2024-07-31 22:33:19 +0300

    Add small comment

--- src/app/core/feature-module/featured.routes.ts
+++ src/app/core/feature-module/featured.routes.ts
@@ -18,10 +18,11 @@ export const FEATURED_ROUTES = {
     return `${environment.apiURL}/featured/learning-objects?collection=${encodeURIComponent(collectionAbvName)}`;
   },
   /**
-  * Request to retrieve the featured learning objects for a specific collection with a limit
-  * @method GET
-  * @param collectionAbvName the abbreviated name of the collection
-  */
+   * Request to retrieve the featured learning objects for a specific collection with a limit
+   * @method GET
+   * @param collectionAbvName the abbreviated name of the collection
+   * @param limit the number of featured learning objects to retrieve
+   */
   GET_COLLECTION_FEATURED_OBJECTS_WITH_LIMIT(collectionAbvName: string, limit: number) {
       return `${environment.apiURL}/featured/learning-objects?
         collection=${encodeURIComponent(collectionAbvName)}&limit=${encodeURIComponent(limit)}`;
