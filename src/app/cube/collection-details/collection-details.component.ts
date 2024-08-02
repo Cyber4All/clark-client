42c28fe73b37f398f3542ef1629a208c03affa29
Paige Zaleppa <43140629+pzalep1@users.noreply.github.com>
2024-08-02 15:33:33 -0400

    console.log

--- src/app/cube/collection-details/collection-details.component.ts
+++ src/app/cube/collection-details/collection-details.component.ts
@@ -45,6 +45,7 @@ export class CollectionDetailsComponent implements OnInit, OnDestroy {
   }
 
   async fetchCollection(abvName: string) {
+    console.log(abvName);
     this.collection = await this.collectionService.getCollectionMetadata(abvName).catch(e => {
       if (e.status === 404) {
         this.router.navigate(['not-found']);
