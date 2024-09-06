22d481ab5c5cd12d132c805f53cb990991bd6256
Andreas <andreas.papacharalampous@secured.team>
2024-09-06 15:24:01 -0400

    remove `changeStatus` from `EditorService`
    Signed-off-by: Andreas <andreas.papacharalampous@secured.team>

--- src/app/core/learning-object-module/editor.service.ts
+++ src/app/core/learning-object-module/editor.service.ts
@@ -8,25 +8,4 @@ import { Observable } from 'rxjs';
 })
 export class EditorService {
   constructor(private http: HttpClient) { }
-
-  /**
-   * changeStatus updates the status of a given Learning Object.
-   *
-   * @param id the Learning Object ID
-   * @param authorUsername the username of the author
-   * @param status the new status for the Learning Object
-   */
-  changeStatus(
-    id: string,
-    authorUsername: string,
-    status: string,
-  ): Observable<string> {
-    const route = LEARNING_OBJECT_ROUTES.UPDATE_LEARNING_OBJECT_STATUS(id);
-    return this.http
-      .patch(
-        route,
-        { learningObject: { status } },
-        { withCredentials: true, responseType: 'text' }
-      );
-  }
 }
