95980ab46ee5d463f8f15ad0c5444e384fc8e156
Romerico David <romericodavid@Romericos-MBP.fios-router.home>
2024-09-10 21:25:23 -0400

    Removed GET_SUBMISSIONS route

--- src/app/core/learning-object-module/submissions/submissions.routes.ts
+++ src/app/core/learning-object-module/submissions/submissions.routes.ts
@@ -3,16 +3,6 @@ import { environment } from '../../../../environments/environment';
 
 export const SUBMISSION_ROUTES = {
     /**
-     * Request to get submissions for a learning object
-     * @param userId - The id of the author of the learning object
-     * @param learningObjectId - The id of the learning object to get submissions for
-     */
-    GET_SUBMISSIONS(userId: string, learningObjectId: string) {
-        return `${environment.apiURL}/users/${encodeURIComponent(
-            userId
-        )}/learning-objects/${encodeURIComponent(learningObjectId)}/submissions`;
-    },
-    /**
      * Request to check if a learning object has been submitted to a specific collection
      * @param learningObjectId - The id of the learning object to check
      * @param collection - The collection associated with the submission
