04ddad52fbca6d9e0474df1396ccacc93d898758
Romerico David <romericodavid@Romericos-MBP.fios-router.home>
2024-09-10 20:41:06 -0400

    Updated TODO comment

--- src/app/cube/details/components/action-panel/editorial-action-pad/editorial-action-pad.component.ts
+++ src/app/cube/details/components/action-panel/editorial-action-pad/editorial-action-pad.component.ts
@@ -84,7 +84,9 @@ export class EditorialActionPadComponent implements OnInit {
   async createRevision() {
     this.closeRevisionModal();
     this.toaster.success('One Moment Please', 'Your revision is being created.');
-    //TODO: Update createRevision to return the revised learning object than the URI that we use to make a GET request directly after.
+    // TODO: Update the createRevision's response to be a revised learning object rather than 
+    // using a GET request to make a request that would effectively do the same thing. 
+    // This will cut down on requests and simplify abstraction.
     await this.revisionsService
       .createRevision(this.learningObject.cuid).then(async (revisionUri: any) => {
         this.revisedLearningObject = (await this.learningObjectServiceUri.fetchUri(revisionUri.revisionUri).toPromise())[0];
