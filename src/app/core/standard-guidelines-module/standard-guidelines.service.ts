a796426a6d7463ef1184a5766270e83eb1f79861
Romerico David <romericodavid@Romericos-MacBook-Pro.local>
2024-09-18 12:21:11 -0400

    Created GuidelinesFilter interface to define possible inputs for filter function parameter. Updated types for page and limit parameters in GuidelinesFilter.

--- src/app/core/standard-guidelines-module/standard-guidelines.service.ts
+++ src/app/core/standard-guidelines-module/standard-guidelines.service.ts
@@ -2,20 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import * as querystring from 'querystring';
 import { throwError } from 'rxjs';
-import { retry, catchError, map } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { STANDARD_GUIDELINES_ROUTES } from './standard-guidelines.routes';
 import { FrameworkDocument } from '../../../entity/standard-guidelines/Framework';
 import { SearchItemDocument } from '../../../entity/standard-guidelines/search-index';
 
+interface GuidelinesFilter {
+  text?: string;
+  filterText?: string;
+  year?: string;
+  levels?: string;
+  page?: number;
+  limit?: number;
+  type?: string;
+  frameworks?: string | string[];
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GuidelineService {
-
-  constructor(public http: HttpClient) { }
+  constructor(public http: HttpClient) {}
 
   getGuidelines(
-    filter?
+    filter?: GuidelinesFilter,
   ): Promise<{ total: number; results: SearchItemDocument[] }> {
     let query = '';
     if (filter) {
@@ -23,31 +33,32 @@ export class GuidelineService {
     }
     // CLARK-SERVICE-FIX: SUPPORT SEARCH QUERY
     return this.http
-      .get<{ total: number, results: SearchItemDocument[] }>(STANDARD_GUIDELINES_ROUTES.SEARCH_GUIDELINES())
-      .pipe(
-
-        catchError(this.handleError)
+      .get<{ total: number; results: SearchItemDocument[] }>(
+        STANDARD_GUIDELINES_ROUTES.SEARCH_GUIDELINES(),
       )
+      .pipe(catchError(this.handleError))
       .toPromise()
-      .then((res: { total: number, results: any[] }) => {
+      .then((res: { total: number; results: any[] }) => {
         return res;
       });
   }
 
   // CLARK-SERVICE-FIX: SUPPORT SEARCH QUERY
-  async getFrameworks(query?: any): Promise<FrameworkDocument[]> {
+  async getFrameworks(filter?: GuidelinesFilter): Promise<FrameworkDocument[]> {
     return this.http
-      .get<{ total: number, results: FrameworkDocument[] }>(STANDARD_GUIDELINES_ROUTES.SEARCH_FRAMEWORKS(query))
+      .get<{ total: number; results: FrameworkDocument[] }>(
+        STANDARD_GUIDELINES_ROUTES.SEARCH_FRAMEWORKS(filter),
+      )
       .pipe(
-
         catchError(this.handleError),
         map((res) => {
           return res.results;
-        })
-      ).toPromise();
+        }),
+      )
+      .toPromise();
   }
 
-  private formatFilter(filter) {
+  private formatFilter(filter: GuidelinesFilter) {
     if (!filter) {
       return {};
     }
@@ -56,8 +67,8 @@ export class GuidelineService {
       text: filter.text || filter.filterText,
       year: filter.year !== '' ? filter.year : undefined,
       levels: filter?.levels?.length > 0 ? filter.levels : undefined,
-      page: filter.page !== '' ? filter.page : undefined,
-      limit: filter.limit !== '' ? filter.limit : undefined,
+      page: filter.page ? filter.page : undefined,
+      limit: filter.limit ? filter.limit : undefined,
       type: filter.type !== '' ? filter.type : undefined,
       frameworks: filter.frameworks !== '' ? filter.frameworks : undefined,
     };
