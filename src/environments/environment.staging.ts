f7dc76290ed4f8bbc2e4d619c22251d51d56a3a2
Christopher Wagner <christopher.wagner@secured.team>
2024-12-18 10:04:27 -0500

    update environments files

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,14 +2,12 @@ export const environment = {
   production: true,
   experimental: false,
   apiURL: 'https://api.yeetbot.click',
-  suggestionUrl: 'https://api.yeetbot.click', // TODO: Remove from client; delete env
   s3Bucket: 'clark-staging-file-uploads',
   s3BucketRegion: 'us-east-1',
   cognitoRegion: 'us-east-1',
   cognitoIdentityPoolId: 'us-east-1:3388292f-c48a-4257-aa55-d1816617b38f',
   cognitoAdminIdentityPoolId: 'us-east-1:a265148e-7418-4a40-aee2-78f5ae7cbf43',
-  cardOrganizationUrl: 'https://api-gateway.caeresource.directory/organizations?type=&verified=verified&mine=&sort=',
-  cardUrl: 'https://api-gateway.caeresource.directory',
+  cardUrl: 'https://api.clark.center',
 };
 
 export enum LearningObjectStatus {
