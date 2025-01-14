44e3de9ec7949f66897ad9716b5ef8a0a712c89d
Andreas <andreas.papacharalampous@secured.team>
2025-01-14 14:49:55 -0500

    description: replace ckeditor
    Signed-off-by: Andreas <andreas.papacharalampous@secured.team>

--- src/app/onion/learning-object-builder/components/description.component.ts
+++ src/app/onion/learning-object-builder/components/description.component.ts
@@ -1,21 +1,35 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {
+  BOLD_BUTTON,
+  ITALIC_BUTTON,
+  LINK_INPUT,
+  ORDERED_LIST_BUTTON,
+  REDO_BUTTON,
+  REMOVE_FORMAT_BUTTON,
+  UNDERLINE_BUTTON,
+  UNDO_BUTTON,
+  UNORDERED_LIST_BUTTON,
+  SEPARATOR
+} from 'ngx-simple-text-editor';
 
 @Component({
   selector: 'onion-learning-object-description',
-  template:
-  `
-  <div class="description-wrapper">
-    <!-- <div id="description-label" class="label">How woud you describe this learning object?
+  template: `
+    <div class="description-wrapper">
+      <!-- <div id="description-label" class="label">How woud you describe this learning object?
     <span tip="The description is a high level overview of the Learning Object that can be returned in search results">
       <a href="http://about.clark.center/tutorial/#Descriptions" target="blank"><i class="fas fa-question-circle"></i></a>
     </span>
     </div> -->
-    <div class="goals list">
-      <clark-text-editor (touched)="touched.emit()" [(savedContent)] = "learningObject.description"
-      editorPlaceholder = "Enter Learning Object description here"
-      (textOutput) = "bindEditorOutput($event)" name="description"></clark-text-editor>
+      <div class="goals list">
+        <st-editor
+          [formControl]="description"
+          [(ngModel)]="learningObject.description"
+          [config]="boxConfig"
+        ></st-editor>
+      </div>
     </div>
-  </div>
   `,
   styles: [
     `
@@ -28,17 +42,41 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
         max-width: 800px;
         width: 100%;
       }
-    `
-  ]
+    `,
+  ],
 })
-export class LearningObjectDescriptionComponent {
+export class LearningObjectDescriptionComponent implements OnInit {
   @Input() learningObject;
+
+  description = new FormControl('');
+  boxConfig = {
+    buttons: [
+      UNDO_BUTTON,
+      REDO_BUTTON,
+      SEPARATOR,
+      BOLD_BUTTON,
+      ITALIC_BUTTON,
+      UNDERLINE_BUTTON,
+      SEPARATOR,
+      REMOVE_FORMAT_BUTTON,
+      SEPARATOR,
+      ORDERED_LIST_BUTTON,
+      UNORDERED_LIST_BUTTON,
+      SEPARATOR,
+      LINK_INPUT,
+    ],
+  };
 
   @Output() touched: EventEmitter<void> = new EventEmitter();
   @Output() textOutput: EventEmitter<string> = new EventEmitter();
 
-  bindEditorOutput(event) {
-    this.learningObject.description = event;
-    this.textOutput.emit(event);
+  ngOnInit() {
+    this.description.valueChanges.subscribe((description: string) => {
+      this.textOutput.emit(description);
+    });
+
+    if (this.learningObject.description) {
+      this.description.patchValue(this.learningObject.description);
+    }
   }
 }
