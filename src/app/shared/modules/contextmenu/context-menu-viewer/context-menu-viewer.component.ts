5b83f8179aa5f7beb38b4899e682655e6be6279b
Diego Soto <diegosoto9744@gmail.com>
2025-02-07 13:40:48 -0500

    mousemove function closes context menu if mouse leaves element, and lint fix

--- src/app/shared/modules/contextmenu/context-menu-viewer/context-menu-viewer.component.ts
+++ src/app/shared/modules/contextmenu/context-menu-viewer/context-menu-viewer.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, HostListener, Renderer2,
-ElementRef, ChangeDetectorRef, AfterViewInit, SimpleChanges, AfterViewChecked } from '@angular/core';
+ElementRef, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
-import { element } from 'protractor';
 
 /**
  * This component is simply a wrapper for any content projected into it (from the original ContextMenuComponent).
@@ -25,14 +24,11 @@ import { element } from 'protractor';
     ])
   ]
 })
-export class ContextMenuViewerComponent implements AfterViewInit {
+export class ContextMenuViewerComponent {
 
-  private mouseX = 0;
-  private mouseY = 0;
   private isMouseOverElement = false;
-  private element: HTMLElement | null = null;
 
-  constructor(private el: ElementRef, private renderer: Renderer2, private cdr: ChangeDetectorRef) {}
+  constructor() {}
 
 
   @Input() close: EventEmitter<void> = new EventEmitter();
@@ -45,131 +41,24 @@ export class ContextMenuViewerComponent implements AfterViewInit {
     }
   }
 
-  // ngAfterViewChecked(){
-  //   console.log("ng after view checked!!!");
-  //   this.element = document.querySelector('clark-context-menu-viewer') as HTMLElement;
-  //     if (this.element) {
-  //       console.log('Element found:', this.element);
-
-  //       // Ensure the element is visible
-  //       if (this.element.offsetWidth > 0 && this.element.offsetHeight > 0) {
-  //         // Listen for mouse enter and leave events
-  //         this.renderer.listen(this.element, 'mouseenter', () => {
-  //           console.log('Mouse entered the element.');
-  //           this.isMouseOverElement = true;
-  //         });
-
-  //         this.renderer.listen(this.element, 'mouseleave', () => {
-  //           console.log('Mouse left the element.');
-  //           this.isMouseOverElement = false;
-  //           this.activateClose();
-  //         });
-  //       } else {
-  //         console.log('Element is not visible or not yet rendered');
-  //       }
-  //     }
-  // }
-
-
-  ngAfterViewInit(){
-    console.log('after view init!!');
-    setTimeout(() => {
-      // this.cdr.detectChanges();
-
-      this.element = document.querySelector('clark-context-menu-viewer') as HTMLElement;
-      if (this.element) {
-        console.log('Element found:', this.element);
-
-        // const rect = this.element.getBoundingClientRect();
-        // if (rect.width > 0 && rect.height > 0 && rect.top >= 0 && rect.left >= 0) {
-        //   // Element is visible and rendered
-        //   console.log('Element is visible and rendered');
-        // } else {
-        //   console.log('Element is not visible or not yet rendered');
-        // }
-
-        // // Ensure the element is visible
-        // if (this.element.offsetWidth > 0 && this.element.offsetHeight > 0) {
-        //   // Listen for mouse enter and leave events
-        //   this.renderer.listen(this.element, 'mouseenter', () => {
-        //     console.log('Mouse entered the element.');
-        //     this.isMouseOverElement = true;
-        //   });
-
-        //   this.renderer.listen(this.element, 'mouseleave', () => {
-        //     console.log('Mouse left the element.');
-        //     this.isMouseOverElement = false;
-        //     this.activateClose();
-        //   });
-        // } else {
-        //   console.log('Element is not visible or not yet rendered');
-        // }
-
-
-        this.element.onmouseout = function() {
- console.log('mouse over the element');
-};
-        // this.checkIfMouseIsOver(this.element);
-        // this.element.onscroll = function() { console.log("scrolling inside the pop up (hopefully)") };
-      }
-    }, 100);
-  }
-
-  detectMouseLeave(e: HTMLElement){
-    e.onmouseleave = function() {
-      console.log('mouse left the element');
-    };
-  }
-
 
   @HostListener('window:mousemove', ['$event'])
   onMouseMove(event){
-    this.mouseX = event.clientX;
-    this.mouseY = event.clientY;
-    // console.log("mouse moving...");
+    // get location of mouse pointer
+    const e = document.elementFromPoint(event.clientX, event.clientY);
+    // check if mouse is over the dropdown list options
+    if(e.tagName.includes('LI')){
+      this.isMouseOverElement = true;
+    }
+    if(!e.tagName.includes('LI') && this.isMouseOverElement === true){
+      this.activateClose();
+    }
   }
 
   @HostListener('window:scroll', ['$event'])
-  onScroll(event){
-
-    // if (this.element) {
-    //   const rect = this.element.getBoundingClientRect();
-
-    //   // Check if the rect is not zero, indicating the element is positioned correctly
-    //   if (rect.width > 0 && rect.height > 0) {
-    //     const isMouseOver = (
-    //       this.mouseX >= rect.left &&
-    //       this.mouseX <= rect.right &&
-    //       this.mouseY >= rect.top &&
-    //       this.mouseY <= rect.bottom
-    //     );
-
-    //     if (isMouseOver && !this.isMouseOverElement) {
-    //       console.log('Mouse entered the element');
-    //       this.isMouseOverElement = true;
-    //     } else if (!isMouseOver && this.isMouseOverElement) {
-    //       console.log('Mouse left the element');
-    //       this.isMouseOverElement = false;
-    //       this.activateClose();
-    //     }
-    //   } else {
-    //     console.log('Element is not yet rendered properly');
-    //   }
-    // }
-
-    console.log('scrolling');
-  }
-
-  checkIfMouseIsOver(element: HTMLElement) {
-    console.log('checking...');
-    this.renderer.listen(element, 'mouseenter', () => {
-      console.log('Mouse entered the element.');
-    });
-
-    this.renderer.listen(element, 'mouseleave', () => {
-      console.log('Mouse left the element.');
-      this.activateClose();
-    });
+  onScroll(){
+    // close context menu when user scrolls past all the options
+    this.activateClose();
   }
 
   /**
