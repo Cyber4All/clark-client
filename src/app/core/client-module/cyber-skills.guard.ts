26526dc330b1592a1ed9603ea20a46ee6564542d
Andreas <andreas.papacharalampous@secured.team>
2025-03-10 15:39:25 -0400

    add cyberskills2work guard
    Signed-off-by: Andreas <andreas.papacharalampous@secured.team>

---
+++ src/app/core/client-module/cyber-skills.guard.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { AuthService } from '../auth-module/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CyberSkillsGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private cookies: CookieService,
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ): Promise<boolean | UrlTree> | boolean {
+    const c = this.cookies.get('presence');
+
+    if (c) {
+      return this.auth.validateToken().then(
+        (val) => {
+          return (
+            this.auth.accessGroups.includes('admin') ||
+            this.auth.accessGroups.includes('editor') ||
+            this.auth.accessGroups.includes('curator')
+          );
+        },
+        (error) => {
+          return this.router.navigate(['/home']);
+        },
+      );
+    } else {
+      return this.router.navigate(['/home']);
+    }
+  }
+}
