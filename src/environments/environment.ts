226db3f17f8f014d1701e1582964a76c0d9cbde8
Paige <pz0342@gmail.com>
2025-03-11 21:38:15 -0400

    :zombie: Bringing back the filters my dude

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   experimental: false,
-  apiURL: 'http://localhost:3000',
+  apiURL: 'https://api.clark.center',
   s3Bucket: 'clark-dev-file-uploads',
   s3BucketRegion: 'us-east-1',
   cognitoRegion: 'us-east-1',
