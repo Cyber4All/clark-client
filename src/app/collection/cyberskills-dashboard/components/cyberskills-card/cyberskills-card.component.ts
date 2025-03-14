60b214f9cc7434cbf21ac6f4b9005ee14611f096
Romerico <romedavid2@outlook.com>
2025-03-14 12:56:05 -0400

    linting

--- src/app/collection/cyberskills-dashboard/components/cyberskills-card/cyberskills-card.component.ts
+++ src/app/collection/cyberskills-dashboard/components/cyberskills-card/cyberskills-card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CyberskillsCardComponent implements OnInit {
   @Input() learningObject: any;
-  @Input() statusDescription: string = ''; 
+  @Input() statusDescription = '';
 
   constructor() { }
 
