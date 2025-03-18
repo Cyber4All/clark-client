f641add15c7fe3663b20e4e58d9f2609d43c0143
Paige <pz0342@gmail.com>
2025-03-18 13:45:21 -0400

    :sparkles: Add specifications for deprecated query

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   experimental: false,
-  apiURL: 'https://api.clark.center',
+  apiURL: 'http://localhost:3000',
   s3Bucket: 'clark-dev-file-uploads',
   s3BucketRegion: 'us-east-1',
   cognitoRegion: 'us-east-1',
