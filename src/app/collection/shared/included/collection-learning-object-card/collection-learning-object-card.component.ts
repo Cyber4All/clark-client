9f6fcb4aa31df84ff3bb43e2346c83e953bfb8af
Diego Soto <diegosoto9744@gmail.com>
2025-03-24 13:54:18 -0400

    innerHtml for learning obj description

--- src/app/collection/shared/included/collection-learning-object-card/collection-learning-object-card.component.ts
+++ src/app/collection/shared/included/collection-learning-object-card/collection-learning-object-card.component.ts
@@ -7,14 +7,32 @@ import { Component, HostListener, Input, OnInit } from '@angular/core';
 })
 export class CollectionLearningObjectCardComponent implements OnInit {
   @Input() learnObj = new Input();
+  shortenedDesc: string;
   constructor() { }
 
   mobile = false;
 
   ngOnInit(): void {
-    if (window.screen.width < 450) { // 768px portrait
+    if (window.screen.width < 450) {
       this.mobile = true;
     }
+
+    if(this.learnObj.description){
+      this.shortenedDesc = this.truncateHtml(this.learnObj.description)
+    }
+  }
+
+  truncateHtml(html: string): string {
+    if (!html) return html;
+
+    const doc = new DOMParser().parseFromString(html, 'text/html');
+    let text = doc.body.textContent || "";
+    
+    if (text.length > 70) {
+      text = text.substring(0, 70) + '...';
+    }
+    
+    return text;
   }
 
   @HostListener('window:resize', ['$event'])
