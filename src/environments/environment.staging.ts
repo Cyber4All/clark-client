663920b91800bca7d505bdc0ec97c94767af8103
Diego Soto <diegosoto9744@gmail.com>
2025-06-30 12:23:51 -0400

    apiURL now clark-gateway.clark.center for staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   experimental: false,
-  apiURL: 'https://api.yeetbot.click',
+  apiURL: 'https://clark-gateway.clark.center',
   s3Bucket: 'clark-staging-file-uploads',
   s3BucketRegion: 'us-east-1',
   cognitoRegion: 'us-east-1',
   cognitoIdentityPoolId: 'us-east-1:3388292f-c48a-4257-aa55-d1816617b38f',
   cognitoAdminIdentityPoolId: 'us-east-1:a265148e-7418-4a40-aee2-78f5ae7cbf43',
-  cardUrl: 'https://api.clark.center',
+  cardUrl: 'https://clark-gateway.clark.center/',
   downtimeUrl: 'https://oyrmr4e2d5nwt3l33ifk4ezgcq0mhnax.lambda-url.us-east-1.on.aws',
 };
 
