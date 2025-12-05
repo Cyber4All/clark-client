8936b28690d709dd2a81391a53090ee7ce23307d
luis <luis.gcodes@gmail.com>
2025-12-05 14:26:38 -0500

    add ngx-markdown to chat

--- src/app/shared/modules/chatbot/chatbot.module.ts
+++ src/app/shared/modules/chatbot/chatbot.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { ChatbotLauncherComponent } from './chatbot-launcher/chatbot-launcher.component';
 import { ChatbotWindowComponent } from './chatbot-window/chatbot-window.component';
 import { ChatbotContainerComponent } from './chatbot-container/chatbot-container.component';
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ChatbotContainerComponent } from './chatbot-container/chatbot-container
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    MarkdownModule.forRoot()
   ],
   exports: [
     ChatbotContainerComponent
