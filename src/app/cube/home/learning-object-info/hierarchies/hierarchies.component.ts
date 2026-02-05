3d4a3efeeedcbd17f436e32df94a80d7c8aa100a
Diego Soto <diegosoto9744@gmail.com>
2026-02-05 14:56:10 -0500

    removing previously commented code for homepage collections component

--- src/app/cube/home/learning-object-info/hierarchies/hierarchies.component.ts
+++ src/app/cube/home/learning-object-info/hierarchies/hierarchies.component.ts
@@ -11,18 +11,7 @@ export class HierarchiesComponent implements OnInit {
 
   constructor() { }
 
-  hierarchy = [];
-  //Below uses a sample hierarchy from the Intro to Cybersecurity course
-  ngOnInit(): void {
-    this.hierarchy.push({name: 'Intro to Cybersecurity', length: 'course'});
-    this.hierarchy.push({name: 'Cyber Foundations', length: 'micromodule'});
-    this.hierarchy.push({name: 'Cybersecurity Case Studies', length: 'nanomodule'});
-    this.hierarchy.push({name: 'Cyber War', length: 'micromodule'});
-    this.hierarchy.push({name: 'Cyberwarfare Exercise', length: 'nanomodule'});
-    this.hierarchy.push({name: 'Network Security', length: 'micromodule'});
-    this.hierarchy.push({name: 'Network Security Lab', length: 'nanomodule'});
-    this.hierarchy.push({name: 'Secure Software', length: 'micromodule'});
-  }
+  ngOnInit(): void {}
 
   getHierarchyTooltip(hierarchyLevel: string): string {
     const tooltipMap = {
--- src/app/cube/home/learning-object-info/sticky-menu/sticky-menu.component.ts
+++ src/app/cube/home/learning-object-info/sticky-menu/sticky-menu.component.ts
@@ -32,7 +32,6 @@ export class StickyMenuComponent implements OnInit {
     learningObjects: true, // LearningObjects starts highlighted
     learningOutcomes: false,
     hierarchies: false,
-    // collections: false,
   };
   isMobileView = false;
   responsiveThreshold = 825;
@@ -95,7 +94,6 @@ export class StickyMenuComponent implements OnInit {
     this.isHighlighted.learningObjects = LEARNING_OBJECT_INFO_STATES.LEARNING_OBJECT === selection;
     this.isHighlighted.learningOutcomes = LEARNING_OBJECT_INFO_STATES.LEARNING_OUTCOMES === selection;
     this.isHighlighted.hierarchies = LEARNING_OBJECT_INFO_STATES.HIERARCHIES === selection;
-    // this.isHighlighted.collections = LEARNING_OBJECT_INFO_STATES.COLLECTIONS === selection;
   }
 
   /**
